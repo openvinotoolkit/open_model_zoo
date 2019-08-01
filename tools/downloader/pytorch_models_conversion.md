045b7024ff5aa4ce99ef2c2f5389a985f56ad6ea
ezamalie <eduard.zamaliev@intel.com>
2019-08-01 18:52:41 +0300

    NEW: Conversionscript can process any (i hope) model

--- tools/downloader/pytorch_models_conversion.md
+++ tools/downloader/pytorch_models_conversion.md
@@ -1,20 +1,13 @@
 # Conversion pretrained models from PyTorch to ONNX
 
 Models from PyTorch framework not supported by Model Optimizer now. Before conversion to IR, they
-must be converted to ONNX using script `pytorch_to_innx.py`. The script enables conversion of models 
+must be converted to ONNX using script `pytorch_to_onnx.py`. The script enables conversion of models
 from torchvision and for public models, which description and pre-trained weights are
 available for cloning or downloading from the internet resources.
 
 ## Supported models
 
-The models, supported in the current version of the script, are:
-
-* Torchvision models:
-    * ResNet-50-v1
-    * Inception-v3
-
-* Public pre-trained models
-    * MobileNetV2 (<https://github.com/tonylins/pytorch-mobilenet-v2>)
+Any model may be converted by this script. To do this weights and model's source code is needed.
 
 ## Setup
 
@@ -41,8 +34,13 @@ source <directory_for_environment>/bin/activate
 ```
 Install requirements:
 ```bash
-pip3 install torch==1.0.1 torchvision>=0.2.2 onnx==1.4.1 numpy<1.15
+pip3 install torch==1.0.1 onnx==1.4.1 numpy<1.15
 ```
+Additionally install TorchVision\* if necessary:
+```bash
+pip3 install torchvision>=0.2.2
+```
+
 Now you can work with the script.
 
 To deactivate virtual environment after finishing work, use the following command:
@@ -53,35 +51,40 @@ deactivate
 
 ## Usage
 
-The script takes the following input arguments:
+```
+Conversion of pretrained models from PyTorch to ONNX
 
-* `--model-name` - name of the PyTorch model to convert. Currently available model names are:
-    * resnet-v1-50
-    * inception-v3
-    * mobilenet-v2
-* `--weights` - path to a .pth or .pth.tar file with downloaded pre-trained PyTorch weights
-* `--input-shape` - input blob shape, given by comma-separated positive integer values for `batch size`,
-  `number of channels`, `height` and `width` in the order, defined for the chosen model
-* `--output-file` - path to the output .onnx file with the converted model.
-
-Optional arguments, taken by the script:
-
-* `--model-path` - path to a directory with python file(s), containing description of PyTorch model, chosen for
-  conversion. This parameter should be provided for public models, that are not a part of torchvision package.
-* `--input-names` - space-separated (if several) names of input layers. The input layers' names would be presented by
-  these values in ONNX model, or indexes of layers would be used instead, if this argument was not provided.
-* `--output-names` - space-separated (if several) names of output layers. The output layers' names would be presented
-  by these values in ONNX model, or indexes of layers would be used instead, if this argument was not provided.
-
-You may also refer to `-h, --help` option for getting full list of script arguments.
+optional arguments:
+  -h, --help            show this help message and exit
+  --model-name MODEL_NAME
+                        Model to convert. May be class name or name of
+                        constructor function
+  --weights WEIGHTS     Path to the weights in PyTorch's format
+  --input-shape INPUT_DIM
+                        Shape of the input blob
+  --output-file OUTPUT_FILE
+                        Path to the output ONNX model
+  --from-torchvision    Sets model's origin as Torchvision*
+  --model-path MODEL_PATH
+                        Path to PyTorch model's source code if model is not
+                        from Torchvision*
+  --import-module IMPORT_MODULE
+                        Name of module, which contains model's
+                        constructor.Requires if model not from Torchvision
+  --input-names INPUT_NAMES [INPUT_NAMES ...]
+                        Space separated names of the input layers
+  --output-names OUTPUT_NAMES [OUTPUT_NAMES ...]
+                        Space separated names of the output layers
+```
 
 For example, to convert ResNet-50-v1 model from torchvision, the following command may be used:
 
 ```bash
 python3 pytorch_to_onnx.py \
-    --model-name resnet-v1-50 \
+    --model-name resnet50 \
+    --from-torchvision \
     --weights  <path_to_downloaded_pretrained_weights>/resnet50-19c8e357.pth \
-    --input-shape 1 3 224 224 \
+    --input-shape 1,3,224,224 \
     --output-file <path_to_save_converted_model>/resnet-v1-50.onnx \
     --input-names data \
     --output-names prob
--- tools/downloader/pytorch_to_onnx.py
+++ tools/downloader/pytorch_to_onnx.py
@@ -2,19 +2,9 @@ import argparse
 from pathlib import Path
 import sys
 
+import onnx
+import torch
 import torch.onnx
-
-
-TORCHVISION_MODELS = [
-    'resnet-v1-50',
-    'inception-v3'
-]
-
-PUBLIC_PYTORCH_MODELS = [
-    'mobilenet-v2'
-]
-
-SUPPORTED_MODELS = TORCHVISION_MODELS + PUBLIC_PYTORCH_MODELS
 
 
 def positive_int_arg(values):
@@ -35,73 +25,83 @@ def positive_int_arg(values):
 def parse_args():
     """Parse input arguments"""
 
-    parser = argparse.ArgumentParser(description='Conversion script for pretrained models from PyTorch to ONNX')
+    parser = argparse.ArgumentParser(description='Conversion of pretrained models from PyTorch to ONNX')
+
     parser.add_argument('--model-name', type=str, required=True,
-                        help='Model name to convert.'
-                             ' One of the ' + ', '.join(SUPPORTED_MODELS) + ' is supported')
+                        help='Model to convert. May be class name or name of constructor function')
     parser.add_argument('--weights', type=str, required=True,
-                        help='Path to the PyTorch pretrained weights')
+                        help='Path to the weights in PyTorch\'s format')
     parser.add_argument('--input-shape', metavar='INPUT_DIM', type=positive_int_arg, required=True,
                         help='Shape of the input blob')
     parser.add_argument('--output-file', type=Path, required=True,
                         help='Path to the output ONNX model')
-
+    parser.add_argument('--from-torchvision', action='store_true',
+                        help='Sets model\'s origin as Torchvision*')
     parser.add_argument('--model-path', type=str,
-                        help='Path to python model description if model is not a standard torchvision model')
+                        help='Path to PyTorch model\'s source code if model is not from Torchvision*')
+    parser.add_argument('--import-module', type=str, default='',
+                        help='Name of module, which contains model\'s constructor.'
+                        'Requires if model not from Torchvision')
     parser.add_argument('--input-names', type=str, nargs='+',
                         help='Space separated names of the input layers')
     parser.add_argument('--output-names', type=str, nargs='+',
                         help='Space separated names of the output layers')
+
     return parser.parse_args()
 
 
-def load_model(model_name, weights, model_path=None):
+def load_model(model_name, weights, from_torchvision=True, model_path=None, module_name=None):
     """Import model and load pretrained weights"""
 
-    if model_name not in SUPPORTED_MODELS:
-        sys.exit('Only ' + ', '.join(SUPPORTED_MODELS) + ' are available for conversion')
-
-    if model_name in TORCHVISION_MODELS:
+    if from_torchvision:
         try:
-            import torchvision
-            if model_name == 'resnet-v1-50':
-                model = torchvision.models.resnet50()
-            elif model_name == 'inception-v3':
-                model = torchvision.models.inception_v3()
-        except ImportError as exc:
-            print(exc)
+            import torchvision.models
+            creator = getattr(torchvision.models, model_name)
+            model = creator()
+        except ImportError as err:
+            print(err)
             sys.exit('The torchvision package was not found.'
                      'Please install it to default location or '
                      'update PYTHONPATH environment variable '
                      'with the path to the installed torchvision package.')
+        except AttributeError as err:
+            print('ERROR: Model {} doesn\'t exist in torchvision!'.format(model_name))
+            sys.exit(err)
     else:
         sys.path.append(model_path)
         try:
-            from MobileNetV2 import MobileNetV2
-            model = MobileNetV2(n_class=1000)
-        except ImportError as exc:
-            print(exc)
-            sys.exit('MobileNetV2 can not be imported. ' +
-                     'Please provide valid path to python model description with use of --model-path argument')
+            module = __import__(module_name)
+            creator = getattr(module, model_name)
+            model = creator()
+        except ImportError as err:
+            print('Module {} in {} doesn\'t exist. Check import path and name'.format(model_name, model_path))
+            sys.exit(err)
+        except AttributeError as err:
+            print('ERROR: Module {} contains no class or function with name {}!'
+                  .format(module_name, model_name))
+            sys.exit(err)
 
-    model.load_state_dict(torch.load(weights, map_location='cpu'))
+    try:
+        model.load_state_dict(torch.load(weights, map_location='cpu'))
+    except RuntimeError as err:
+        print('ERROR: Weights from \n{}\n cannot be loaded for model {}! Check matching between model and weights')
+        sys.exit(err)
     return model
 
 
-def convert_to_onnx(pytorch_model, input_shape, output_file, input_names, output_names):
+def convert_to_onnx(model, input_shape, output_file, input_names, output_names):
     """Convert PyTorch model to ONNX and check the resulting onnx model"""
 
     output_file.parent.mkdir(parents=True, exist_ok=True)
-    pytorch_model.eval()
-    onnx_input_shape = torch.randn(input_shape)
-    torch.onnx.export(pytorch_model, onnx_input_shape, str(output_file),
+    model.eval()
+    dummy_input = torch.randn(input_shape)
+    torch.onnx.export(model, dummy_input, str(output_file),
                       verbose=False, input_names=input_names, output_names=output_names)
 
     # Model check after conversion
-    import onnx
-    model_from_onnx = onnx.load(str(output_file))
+    model = onnx.load(str(output_file))
     try:
-        onnx.checker.check_model(model_from_onnx)
+        onnx.checker.check_model(model)
         print('ONNX check passed successfully.')
     except onnx.onnx_cpp2py_export.checker.ValidationError as exc:
         sys.exit('ONNX check failed with error: ' + str(exc))
@@ -109,7 +109,7 @@ def convert_to_onnx(pytorch_model, input_shape, output_file, input_names, output
 
 def main():
     args = parse_args()
-    model = load_model(args.model_name, args.weights, args.model_path)
+    model = load_model(args.model_name, args.weights, args.from_torchvision, args.model_path, args.import_module)
     convert_to_onnx(model, args.input_shape, args.output_file, args.input_names, args.output_names)
 
 
