9d141b952f371f803a991bb6b5e3577a60a8438a
Ekaterina Aidova <ekaterina.aidova@intel.com>
2019-08-08 12:01:58 +0300

    fix unicode reading, handle index error in adapter

--- tools/accuracy_checker/accuracy_checker/adapters/nlp.py
+++ tools/accuracy_checker/accuracy_checker/adapters/nlp.py
@@ -43,7 +43,7 @@ class MachineTranslationAdapter(Adapter):
     def configure(self):
         vocab_file = self.get_value_from_config('vocabulary_file')
         self.encoding_vocab = {
-            idx: word for idx, word in enumerate(read_txt(vocab_file))
+            idx: word for idx, word in enumerate(read_txt(vocab_file, encoding='utf-8'))
         }
         self.eos_index = self.get_value_from_config('eos_index')
         self.subword_option = vocab_file.name.split('.')[1] if len(vocab_file.name.split('.')) > 1 else None
@@ -59,7 +59,15 @@ class MachineTranslationAdapter(Adapter):
                 if self.eos_index:
                     end_of_string = np.argwhere(best_sequence == self.eos_index)[0]
                     best_sequence = best_sequence[:end_of_string[0]]
-                encoded_words = [self.encoding_vocab[idx] for idx in best_sequence]
-                results.append(MachineTranslationPrediction(identifier, _clean(encoded_words, self.subword_option)))
+            encoded_words = []
+            for seq_id, idx in enumerate(best_sequence):
+                word = self.encoding_vocab.get(int(idx))
+                if word is None:
+                    raise ValueError(
+                        'Unrecognized index in position {}. '.format(seq_id) +
+                        'Model vocab does not contain word for {} index'.format(idx)
+                    )
+                encoded_words.append(word)
+            results.append(MachineTranslationPrediction(identifier, _clean(encoded_words, self.subword_option)))
 
         return results
