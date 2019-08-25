3ce64dfb14778acf6b355adf934f397282e7c78d
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2019-08-25 12:45:26 +0300

    Base Topology class

--- tools/downloader/topologies.py
+++ tools/downloader/topologies.py
@@ -13,6 +13,118 @@ _precision_marker = '$precision'
 _cache = Path(os.getenv('OPENCV_OPEN_MODEL_ZOO_CACHE_DIR', Path('.').resolve() / 'models'))
 
 
+class Topology:
+    def __init__(self, name, task_type, download_dir, config, model, framework,
+                 mo_args, precision):
+        self.name = name
+        self.task_type = task_type
+        self.mo_args = mo_args
+        self.download_dir = download_dir
+        self.config = config.replace(_precision_marker, precision)
+        self.model = model.replace(_precision_marker, precision)
+        self.framework = framework
+
+        if self.config and not os.path.exists(self.config) or \
+           self.model and not os.path.exists(self.model):
+            sys.argv = ['', '--name=' + self.name, '--precisions=' + precision,
+                        '--output_dir=' + str(_cache).replace('\\', '\\\\')]
+            from downloader import main
+            main()
+
+
+    def getIR(self, precision='FP32'):
+        '''
+        Creates OpenVINO Intermediate Representation (IR) network format.
+
+        Returns
+        -------
+            xmlPath
+                Path to generated or downloaded .xml file
+            binPath
+                Path to generated or downloaded .bin file
+        '''
+        if self.framework == 'dldt':
+            return self.config, self.model
+
+        prefix = Path(self.download_dir) / precision / self.name
+        xmlPath = str(prefix) + '.xml'
+        binPath = str(prefix) + '.bin'
+        if os.path.exists(xmlPath) and os.path.exists(binPath):
+            return xmlPath, binPath
+
+        sys.argv = ['', '--name=' + self.name, '--precisions=' + precision,
+                    '--download_dir=' + str(_cache).replace('\\', '\\\\')]
+        from converter import main
+        main()
+
+        return xmlPath, binPath
+
+
+    def getOCVModel(self, useIR=True):
+        '''
+        Creates OpenCV's cv::dnn::Model from origin network. Depends on network type,
+        returns cv.dnn_DetectionModel, cv.dnn_ClassificationModel, cv.dnn_SegmentationModel
+        or cv.dnn_Model if not specified.
+
+        Preprocessing parameters are set.
+        '''
+        import cv2 as cv
+
+        if useIR:
+            model, config = self.getIR()
+        else:
+            model, config = self.model, self.config
+
+        if self.task_type == 'detection':
+            m = cv.dnn_DetectionModel(model, config)
+        elif self.task_type == 'classification':
+            m = cv.dnn_ClassificationModel(model, config)
+        elif self.task_type == 'semantic_segmentation':
+            m = cv.dnn_SegmentationModel(model, config)
+        else:
+            m = cv.dnn_Model(model, config)
+
+        if useIR:
+            return m
+
+        if '--mean_values' in self.mo_args:
+            mean = self.mo_args['--mean_values']
+            mean = mean[mean.find('[') + 1:mean.find(']')].split(',')
+            mean = [float(val) for val in mean]
+            m.setInputMean(mean)
+
+        if '--scale_values' in self.mo_args:
+            scale = self.mo_args['--scale_values']
+            scale = 1.0 / float(scale[scale.find('[') + 1:scale.find(']')])
+            m.setInputScale(scale)
+
+        if '--input_shape' in self.mo_args:
+            shape = self.mo_args['--input_shape']
+            shape = shape[shape.find('[') + 1:shape.find(']')].split(',')
+            shape = [int(val) for val in shape]
+            if len(shape) == 4:
+                if self.framework == 'tf':  # NHWC
+                    w, h = shape[2], shape[1]
+                else:  # NCHW
+                    w, h = shape[3], shape[2]
+            m.setInputSize(w, h)
+
+        if '--reverse_input_channels' in self.mo_args:
+            m.setInputSwapRB(True)
+
+        return m
+
+
+    def getIENetwork(self):
+        '''
+        Creates openvino.inference_engine.IENetwork instance.
+        Model Optimizer is lauched to create OpenVINO Intermediate Representation (IR).
+        '''
+        from openvino.inference_engine import IENetwork
+        xmlPath, binPath = self.getIR()
+        return IENetwork(xmlPath, binPath)
+
+
 for _topology in common.load_models(argparse.Namespace(config=None)):
     _name = _topology.name.replace('-', '_').replace('.', '_')
     _download_dir = _cache / _topology.subdirectory
@@ -52,7 +164,7 @@ for _topology in common.load_models(argparse.Namespace(config=None)):
 
 
     exec("""
-class {className}:
+class {className}(Topology):
     '''
     {description}
 
@@ -69,119 +181,16 @@ class {className}:
     '''
 
     def __init__(self, precision='FP32'):
-        self.config = '{config}'.replace('{precisionMarker}', precision)
-        self.model = '{model}'.replace('{precisionMarker}', precision)
-        self.framework = '{framework}'
-
-        if not os.path.exists(self.config) or not os.path.exists(self.model):
-            sys.argv = ['', '--name={name}', '--output_dir={outdir}',
-                        '--precisions=' + precision]
-            from downloader import main
-            main()
-
-
-    def getIR(self, precision='FP32'):
-        '''
-        Creates OpenVINO Intermediate Representation (IR) network format.
-
-        Returns
-        -------
-            xmlPath
-                Path to generated or downloaded .xml file
-            binPath
-                Path to generated or downloaded .bin file
-        '''
-        if self.framework == 'dldt':
-            return self.config, self.model
-
-        prefix = os.path.join('{subdir}', precision, '{name}')
-        xmlPath = prefix + '.xml'
-        binPath = prefix + '.bin'
-        if os.path.exists(xmlPath) and os.path.exists(binPath):
-            return xmlPath, binPath
-
-        sys.argv = ['', '--name={name}', '--precisions=' + precision, '--download_dir={outdir}']
-        from converter import main
-        main()
-
-        return xmlPath, binPath
-
-
-    def getOCVModel(self, useIR=True):
-        '''
-        Creates OpenCV's cv::dnn::Model from origin network. Depends on network type,
-        returns cv.dnn_DetectionModel, cv.dnn_ClassificationModel, cv.dnn_SegmentationModel
-        or cv.dnn_Model if not specified.
-
-        Preprocessing parameters are set.
-        '''
-        import cv2 as cv
-
-        task_type = '{task_type}'
-        mo_args = {mo_args}
-
-        if useIR:
-            model, config = self.getIR()
-        else:
-            model, config = self.model, self.config
-
-        if task_type == 'detection':
-            m = cv.dnn_DetectionModel(model, config)
-        elif task_type == 'classification':
-            m = cv.dnn_ClassificationModel(model, config)
-        elif task_type == 'semantic_segmentation':
-            m = cv.dnn_SegmentationModel(model, config)
-        else:
-            m = cv.dnn_Model(model, config)
-
-        if useIR:
-            return m
-
-        if '--mean_values' in mo_args:
-            mean = mo_args['--mean_values']
-            mean = mean[mean.find('[') + 1:mean.find(']')].split(',')
-            mean = [float(val) for val in mean]
-            m.setInputMean(mean)
-
-        if '--scale_values' in mo_args:
-            scale = mo_args['--scale_values']
-            scale = 1.0 / float(scale[scale.find('[') + 1:scale.find(']')])
-            m.setInputScale(scale)
-
-        if '--input_shape' in mo_args:
-            shape = mo_args['--input_shape']
-            shape = shape[shape.find('[') + 1:shape.find(']')].split(',')
-            shape = [int(val) for val in shape]
-            if len(shape) == 4:
-                if self.framework == 'tf':  # NHWC
-                    w, h = shape[2], shape[1]
-                else:  # NCHW
-                    w, h = shape[3], shape[2]
-            m.setInputSize(w, h)
-
-        if '--reverse_input_channels' in mo_args:
-            m.setInputSwapRB(True)
-
-        return m
-
-
-    def getIENetwork(self):
-        '''
-        Creates openvino.inference_engine.IENetwork instance.
-        Model Optimizer is lauched to create OpenVINO Intermediate Representation (IR).
-        '''
-        from openvino.inference_engine import IENetwork
-        xmlPath, binPath = self.getIR()
-        return IENetwork(xmlPath, binPath)
+        Topology.__init__(self, '{name}', '{task_type}', '{download_dir}',
+                          '{config}', '{model}', '{framework}', {mo_args},
+                          precision)
 
             """.format(className=_name, description=_topology.description,
                        license=_topology.license_url,
                        name=_topology.name,
-                       outdir=str(_cache).replace('\\', '\\\\'),
-                       subdir=str(_download_dir),
+                       download_dir=str(_download_dir),
                        config=str(_config_path).replace('\\', '\\\\'),
                        model=str(_model_path).replace('\\', '\\\\'),
                        framework=_topology.framework,
-                       precisionMarker=_precision_marker,
                        task_type=_topology.task_type,
                        mo_args=_mo_args))
