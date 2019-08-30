cde3d4ad9651a75795a6e3ee76dab40c51cf7d88
Zlobin Vladimir <vladimir.zlobin@ointel.com>
2019-08-30 14:50:32 +0300

    demos/tests: add smart_classroom

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -183,7 +183,18 @@ NATIVE_DEMOS = [
         ],
     )),
 
-    # TODO: smart_classroom_demo
+    NativeDemo(name='smart_classroom_demo', test_cases=combine_cases(
+        TestCase(options={'-no_show': None,
+            '-i': ImagePatternArg('500x375x3'),
+            '-m_fd': ModelArg('face-detection-adas-0001')}),
+        device_cases('-d_act', '-d_fd', '-d_lm', '-d_reid'),
+        single_option_cases('-m_act', ModelArg('person-detection-action-recognition-0005'),
+                                      ModelArg('person-detection-action-recognition-0006'),
+                                      ModelArg('person-detection-raisinghand-recognition-0001'),
+                                      ModelArg('person-detection-action-recognition-teacher-0002')),
+        single_option_cases('-m_lm', None, ModelArg('landmarks-regression-retail-0009')),
+        single_option_cases('-m_reid', None, ModelArg('face-reidentification-retail-0095')),
+    )),
 
     NativeDemo(name='super_resolution_demo', test_cases=combine_cases(
         TestCase(options={'-i': ImageDirectoryArg('single-image-super-resolution')}),
--- demos/tests/image_sequences.py
+++ demos/tests/image_sequences.py
@@ -135,4 +135,17 @@ IMAGE_SEQUENCES = {
         image_net_arg('00037128'),
         image_net_arg('00048316'),
     ],
+
+    '500x375x3': [
+        image_net_arg('00000001'),
+        image_net_arg('00000002'),
+        image_net_arg('00000003'),
+        image_net_arg('00000004'),
+        image_net_arg('00000008'),
+        image_net_arg('00000010'),
+        image_net_arg('00000017'),
+        image_net_arg('00000019'),
+        image_net_arg('00000020'),
+        image_net_arg('00000024'),
+    ],
 }
