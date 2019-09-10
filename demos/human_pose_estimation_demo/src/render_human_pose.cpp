ceb660dc393787705f9c56902173e8a6bf4fb8ee
playboy007gy <50794318+playboy007gy@users.noreply.github.com>
2019-09-10 17:06:42 +0800

    Update render_human_pose.cpp

--- demos/human_pose_estimation_demo/src/render_human_pose.cpp
+++ demos/human_pose_estimation_demo/src/render_human_pose.cpp
@@ -10,14 +10,9 @@
 #include "human_pose_estimator.hpp"
 #include "render_human_pose.hpp"
 
-#define COCO
-//#define MPI
-//#define BODY_25
-//#define FACE
-//#define HAND
 
 namespace human_pose_estimation {
-void renderHumanPose(const std::vector<HumanPose>& poses, cv::Mat& image) {
+void renderHumanPose(const std::vector<HumanPose>& poses, cv::Mat& image, const std::string& modeltype) {
     CV_Assert(image.type() == CV_8UC3);
 
     const std::vector<cv::Scalar> colors = {
@@ -33,8 +28,6 @@ void renderHumanPose(const std::vector<HumanPose>& poses, cv::Mat& image) {
 		cv::Scalar(128, 255, 255), cv::Scalar(128, 170, 255), cv::Scalar(128, 85, 255),
 		cv::Scalar(128, 0, 255), cv::Scalar(85, 128, 255), cv::Scalar(170, 128, 255),
 		cv::Scalar(255, 128, 255), cv::Scalar(255, 128, 170), cv::Scalar(255, 128, 85)
-
-		#ifdef FACE
 		,cv::Scalar(255, 0, 0), cv::Scalar(255, 85, 0), cv::Scalar(255, 170, 0),
 		cv::Scalar(255, 255, 0), cv::Scalar(170, 255, 0), cv::Scalar(85, 255, 0),
 		cv::Scalar(0, 255, 0), cv::Scalar(0, 255, 85), cv::Scalar(0, 255, 170),
@@ -46,62 +39,87 @@ void renderHumanPose(const std::vector<HumanPose>& poses, cv::Mat& image) {
 		cv::Scalar(128, 255, 0), cv::Scalar(128, 255, 85), cv::Scalar(128, 255, 170),
 		cv::Scalar(128, 255, 255), cv::Scalar(128, 170, 255), cv::Scalar(128, 85, 255),
 		cv::Scalar(128, 0, 255), cv::Scalar(85, 128, 255)
-		#endif
     };
-    const std::vector<std::pair<int, int> > limbKeypointsIds = {
-	#ifdef COCO
-        {1, 2},  {1, 5},   {2, 3},
-        {3, 4},  {5, 6},   {6, 7},
-        {1, 8},  {8, 9},   {9, 10},
-        {1, 11}, {11, 12}, {12, 13},
-        {1, 0},  {0, 14},  {14, 16},
-        {0, 15}, {15, 17}
-	#endif
 
-	#ifdef MPI
-		{0,1}, {1,2}, {2,3},
-		{3,4}, {1,5}, {5,6},
-		{6,7}, {1,14}, {14,8}, {8,9},
-		{9,10}, {14,11}, {11,12}, {12,13}
-	#endif
+	const std::vector<std::pair<int, int> > limbKeypointsIds;
 
-	#ifdef BODY_25
-		{1,8}, {1,2}, {1,5}, {2,3}, {3,4},
-		{5,6}, {6,7}, {8,9}, {9,10}, {10,11},
-		{8,12}, {12,13}, {13,14}, {1,0}, {0,15},
-		{15,17}, {0,16}, {16,18}, {14,19}, {19,20},
-		{14,21}, {11,22}, {22,23}, {11,24}
-	#endif
+	if (modeltype == "COCO")
+	{
+		std::vector<std::pair<int, int> > *ptr = (std::vector<std::pair<int, int> >*)&limbKeypointsIds;
+		*ptr = {
+			{1, 2},  {1, 5},   {2, 3},
+			{3, 4},  {5, 6},   {6, 7},
+			{1, 8},  {8, 9},   {9, 10},
+			{1, 11}, {11, 12}, {12, 13},
+			{1, 0},  {0, 14},  {14, 16},
+			{0, 15}, {15, 17}
+		};
+	}
 
-	#ifdef FACE
-		{0,1}, {1,2}, {1,3}, {3,4}, {4,5},
-		{5,6}, {6,7}, {7,8}, {8,9}, {9,10},
-		{10,11}, {11,12}, {12,13}, {13,14}, {14,15},
-		{15,16}, {17,18}, {18,19}, {19,20}, {20,21},
-		{22,23}, {23,24}, {24,25}, {25,26}, {27,28},
-		{28,29}, {29,30}, {31,32}, {32,33}, {33,34},
-		{34,35}, {36,37}, {37,38}, {38,39}, {39,40},
-		{40,41}, {41,36}, {42,43}, {43,44}, {44,45},
-		{45,46}, {46,47}, {47,42}, {48,49}, {49,50},
-		{50,51}, {51,52}, {52,53}, {53,54}, {54,55},
-		{55,56}, {56,57}, {57,58}, {58,59}, {59,48},
-		{60,61}, {61,62}, {62,63}, {63,64}, {64,65},
-		{65,66}, {66,67}, {67,60}, {37,68}, {44,69}
-	#endif
+	else if (modeltype == "MPI")
+	{
+		std::vector<std::pair<int, int> > *ptr = (std::vector<std::pair<int, int> >*)&limbKeypointsIds;
+		*ptr = {
+			{0, 1}, { 1,2 }, { 2,3 },
+			{ 3,4 }, { 1,5 }, { 5,6 },
+			{ 6,7 }, { 1,14 }, { 14,8 }, { 8,9 },
+			{ 9,10 }, { 14,11 }, { 11,12 }, { 12,13 }
+		};
+	}
 
-	#ifdef HAND
-		{0,1}, {1,2}, {2,3}, {3,4}, {0,5},
-		{5,6}, {6,7}, {7,8}, {0,9}, {9,10},
-		{10,11}, {11,12}, {0,13}, {13,14}, {14,15},
-		{15,16}, {0,17}, {17,18}, {18,19}, {19,20}
-	#endif
-    };
+	else if (modeltype == "BODY_25")
+	{
+		std::vector<std::pair<int, int> > *ptr = (std::vector<std::pair<int, int> >*)&limbKeypointsIds;
+		*ptr = {
+			{1, 8}, { 1,2 }, { 1,5 }, { 2,3 }, { 3,4 },
+			{ 5,6 }, { 6,7 }, { 8,9 }, { 9,10 }, { 10,11 },
+			{ 8,12 }, { 12,13 }, { 13,14 }, { 1,0 }, { 0,15 },
+			{ 15,17 }, { 0,16 }, { 16,18 }, { 14,19 }, { 19,20 },
+			{ 14,21 }, { 11,22 }, { 22,23 }, { 11,24 }
+		};
+	}
+
+	else if (modeltype == "FACE")
+	{
+		std::vector<std::pair<int, int> > *ptr = (std::vector<std::pair<int, int> >*)&limbKeypointsIds;
+		*ptr = {
+			{0, 1}, { 1,2 }, { 1,3 }, { 3,4 }, { 4,5 },
+			{ 5,6 }, { 6,7 }, { 7,8 }, { 8,9 }, { 9,10 },
+			{ 10,11 }, { 11,12 }, { 12,13 }, { 13,14 }, { 14,15 },
+			{ 15,16 }, { 17,18 }, { 18,19 }, { 19,20 }, { 20,21 },
+			{ 22,23 }, { 23,24 }, { 24,25 }, { 25,26 }, { 27,28 },
+			{ 28,29 }, { 29,30 }, { 31,32 }, { 32,33 }, { 33,34 },
+			{ 34,35 }, { 36,37 }, { 37,38 }, { 38,39 }, { 39,40 },
+			{ 40,41 }, { 41,36 }, { 42,43 }, { 43,44 }, { 44,45 },
+			{ 45,46 }, { 46,47 }, { 47,42 }, { 48,49 }, { 49,50 },
+			{ 50,51 }, { 51,52 }, { 52,53 }, { 53,54 }, { 54,55 },
+			{ 55,56 }, { 56,57 }, { 57,58 }, { 58,59 }, { 59,48 },
+			{ 60,61 }, { 61,62 }, { 62,63 }, { 63,64 }, { 64,65 },
+			{ 65,66 }, { 66,67 }, { 67,60 }, { 37,68 }, { 44,69 }
+		};
+	}
+
+	else if (modeltype == "HAND")
+	{
+		std::vector<std::pair<int, int> > *ptr = (std::vector<std::pair<int, int> >*)&limbKeypointsIds;
+		*ptr = {
+			{0, 1}, { 1,2 }, { 2,3 }, { 3,4 }, { 0,5 },
+			{ 5,6 }, { 6,7 }, { 7,8 }, { 0,9 }, { 9,10 },
+			{ 10,11 }, { 11,12 }, { 0,13 }, { 13,14 }, { 14,15 },
+			{ 15,16 }, { 0,17 }, { 17,18 }, { 18,19 }, { 19,20 }
+		};
+	}
+
 
     const int stickWidth = 4;
     const cv::Point2f absentKeypoint(-1.0f, -1.0f);
     for (const auto& pose : poses) {
-        CV_Assert(pose.keypoints.size() == HumanPoseEstimator::keypointsNumber);
-
+        CV_Assert( (pose.keypoints.size() == HumanPoseEstimator::ModelType::COCO) || 
+				   (pose.keypoints.size() == HumanPoseEstimator::ModelType::MPI) ||
+			       (pose.keypoints.size() == HumanPoseEstimator::ModelType::BODY_25) || 
+			       (pose.keypoints.size() == HumanPoseEstimator::ModelType::FACE) || 
+			       (pose.keypoints.size() == HumanPoseEstimator::ModelType::HAND) );
+	
         for (size_t keypointIdx = 0; keypointIdx < pose.keypoints.size(); keypointIdx++) {
             if (pose.keypoints[keypointIdx] != absentKeypoint) {
                 cv::circle(image, pose.keypoints[keypointIdx], 4, colors[keypointIdx], -1);
