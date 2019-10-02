418467e5e3f55dcbf9139221a0454377b6498f1e
Zlobin Vladimir <vladimir.zlobin@ointel.com>
2019-10-02 17:50:52 +0300

    2 imgs

--- demos/tests/image_sequences.py
+++ demos/tests/image_sequences.py
@@ -137,12 +137,7 @@ IMAGE_SEQUENCES = {
     ],
 
     'smart-classroom-demo': [
-        image_net_arg('00000074'),
-        image_net_arg('00000164'),
         image_net_arg('00000181'),
-        image_net_arg('00000164'),
-        image_net_arg('00000181'),
-        image_net_arg('00000001'),
         image_net_arg('00000074'),
     ],
 }
