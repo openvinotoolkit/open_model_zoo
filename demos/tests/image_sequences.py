d2cb191185754a110163cc1990f0cb67f267fdeb
Zlobin Vladimir <vladimir.zlobin@ointel.com>
2019-10-02 15:22:56 +0300

    remove 1 image

--- demos/tests/image_sequences.py
+++ demos/tests/image_sequences.py
@@ -141,7 +141,7 @@ IMAGE_SEQUENCES = {
         image_net_arg('00000164'),
         image_net_arg('00000181'),
         image_net_arg('00000164'),
-        image_net_arg('00000181'),
+        # image_net_arg('00000181'),
         # image_net_arg('00000001'),
         # image_net_arg('00000074'),
     ],
