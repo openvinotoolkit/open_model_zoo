99a1a99a5a16d4a2176ca4cd7699fbfafa5fce97
FengYen-Chang <a60207john@gmail.com>
2019-10-08 18:08:54 +0800

    compute ASR performance by WER

---
+++ tools/accuracy_checker/accuracy_checker/metrics/speech_recognition.py
@@ -0,0 +1,29 @@
+######################################
+#
+#   John Feng, john.feng@intel.com
+#
+#   using word error rate to evaluate
+#   deep speech 
+#
+######################################
+
+from ..representation import CharacterRecognitionAnnotation, CharacterRecognitionPrediction
+from .metric import PerImageEvaluationMetric
+from .word_error_meter import WordErrorMeter
+
+
+class SpeechRecognitionAccuracy(PerImageEvaluationMetric):
+    __provider__ = 'speech_recognition_accuracy'
+
+    annotation_types = (CharacterRecognitionAnnotation, )
+    prediction_types = (CharacterRecognitionPrediction, )
+
+    def configure(self):
+        self.threshold = self.get_value_from_config('threshold')
+        self.accuracy = WordErrorMeter(self.threshold)
+
+    def update(self, annotation, prediction):
+        self.accuracy.update(annotation.label, prediction.label)
+
+    def evaluate(self, annotations, predictions):
+        return self.accuracy.evaluate()
