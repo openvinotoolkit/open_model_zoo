1e7d28f460da5e974a3ca302f727d5ed6c62e652
Ekaterina Aidova <ekaterina.aidova@intel.com>
2019-10-11 10:20:10 +0300

    try overload commandline parameters if they already merged with config

--- tools/accuracy_checker/accuracy_checker/config/config_reader.py
+++ tools/accuracy_checker/accuracy_checker/config/config_reader.py
@@ -266,7 +266,6 @@ class ConfigReader:
                     continue
 
                 if not args[argument].is_dir():
-                    print(config_path)
                     raise ConfigError('argument: {} should be a directory'.format(argument))
                 value[field] = args[argument] / config_path
 
@@ -372,17 +371,19 @@ class ConfigReader:
             if 'bitstream' not in launcher_entry and 'bitstreams' in arguments and arguments.bitstreams:
                 if not arguments.bitstreams.is_dir():
                     launcher_entry['bitstream'] = arguments.bitstreams
+                    arguments.bitstreams = None
 
             if 'cpu_extensions' not in launcher_entry and 'extensions' in arguments and arguments.extensions:
                 extensions = arguments.extensions
                 if not extensions.is_dir() or extensions.name == 'AUTO':
-                    print('extensions.name == AUTO')
                     launcher_entry['cpu_extensions'] = arguments.extensions
+                    arguments.extensions = None
 
             if 'affinity_map' not in launcher_entry and 'affinity_map' in arguments and arguments.affinity_map:
                 am = arguments.affinity_map
                 if not am.is_dir():
                     launcher_entry['affinity_map'] = arguments.affinity_map
+                    arguments.affinity_map = None
 
             return launcher_entry
 
