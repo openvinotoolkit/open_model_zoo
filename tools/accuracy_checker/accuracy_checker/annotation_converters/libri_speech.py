cfa19896876b22e681c715c61019daa58955e3de
FengYen-Chang <a60207john@gmail.com>
2019-10-15 15:09:07 +0800

    to R3

--- tools/accuracy_checker/accuracy_checker/annotation_converters/libri_speech.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/libri_speech.py
@@ -6,13 +6,11 @@
 ###############################################
 
 from ..config import PathField
-# from ..representation import SpeechRecognitionAnnotation
 from ..representation import CharacterRecognitionAnnotation
-from ..utils import read_txt
+from ..utils import read_txt, check_file_existence
+from .format_converter import FileBasedAnnotationConverter, ConverterReturn
 
-from .format_converter import BaseFormatConverter
-
-class LibriSpeechFormatConverter(BaseFormatConverter):
+class LibriSpeechFormatConverter(FileBasedAnnotationConverter):
     __provider__ = 'libri_speech'
 
     annotation_types = (CharacterRecognitionAnnotation, )
@@ -31,15 +29,33 @@ class LibriSpeechFormatConverter(BaseFormatConverter):
     def configure(self):
         self.data_dir = self.get_value_from_config('data_dir')
 
-    def convert(self):
-        annotation = []
-        for file_in_dir in self.data_dir.iterdir():
-            str_file_in_dir = str(file_in_dir)
+    def convert(self, check_content=False, progress_callback=None, progress_interval=100, **kwargs):
+        annotations = []
+        content_errors = None
+        if check_content:
+            content_errors = []
+            self.images_dir = self.images_dir or self.annotation_file.parent
+
+        label_files = []
+
+        for files_in_dir in self.data_dir.iterdir():
+            str_file_in_dir = str(files_in_dir)
             if '.txt' in str_file_in_dir:
-                label = read_txt(str_file_in_dir)[0].lower()
-                audio_name = str_file_in_dir[:-3] + 'wav'
-                annotation.append(CharacterRecognitionAnnotation(audio_name, label))
+                label_files.append(str_file_in_dir)
+
+        num_iterations = len(label_files)
+
+        for _id, _file in enumerate(label_files):
+            label = read_txt(_file)[0].lower()
+            audio_name = _file[:-3] + 'wav'
+            annotations.append(CharacterRecognitionAnnotation(audio_name, label))
+            if check_content:
+                if not check_file_existence(self.images_dir / audio_name):
+                    content_errors.append('{}: does not exist'.format(audio_name))
+            if progress_callback is not None and _id % progress_interval:
+                progress_callback(_id / num_iterations * 100)
 
         label_map = {ind: str(key) for ind, key in enumerate(self.supported_symbols)}
+        meta = {'label_map': label_map, 'blank_label': len(label_map)}
 
-        return annotation, {'label_map': label_map, 'blank_label': len(label_map)}
+        return ConverterReturn(annotations, meta, content_errors)
