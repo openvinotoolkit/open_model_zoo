b70984de04d8eb506356bf45f511535b8f515cfd
Leonid Beynenson <Leonid.Beynenson@gmail.com>
2019-10-29 16:13:17 +0300

    Add command line parameter to turn on greedy face reid matching in smart_classroom_demo

--- demos/smart_classroom_demo/main.cpp
+++ demos/smart_classroom_demo/main.cpp
@@ -608,10 +608,9 @@ int main(int argc, char* argv[]) {
         VectorCNN landmarks_detector(landmarks_config);
 
         // Create face gallery
-        bool use_greedy_matcher = false;
         EmbeddingsGallery face_gallery(FLAGS_fg, FLAGS_t_reid, FLAGS_min_size_fr, FLAGS_crop_gallery,
                                        face_detector_for_registration, landmarks_detector, face_reid,
-                                       use_greedy_matcher);
+                                       FLAGS_greedy_reid_matching);
 
         if (!reid_config.enabled) {
             slog::warn << "Face recognition models are disabled!"  << slog::endl;
