2b03937981da710d06b98d716bfad4a73683f195
FengYen-Chang <a60207john@gmail.com>
2019-11-01 10:49:54 +0800

    patch initializer

--- demos/multi_channel/object_detection_demo_yolov3/main.cpp
+++ demos/multi_channel/object_detection_demo_yolov3/main.cpp
@@ -119,14 +119,13 @@ struct DetectionObject {
     int xmin, ymin, xmax, ymax, class_id;
     float confidence;
 
-    DetectionObject(double x, double y, double h, double w, int class_id, float confidence, float h_scale, float w_scale) {
-        this->xmin = static_cast<int>((x - w / 2) * w_scale);
-        this->ymin = static_cast<int>((y - h / 2) * h_scale);
-        this->xmax = static_cast<int>(this->xmin + w * w_scale);
-        this->ymax = static_cast<int>(this->ymin + h * h_scale);
-        this->class_id = class_id;
-        this->confidence = confidence;
-    }
+    DetectionObject(double x, double y, double h, double w, int class_id, float confidence, float h_scale, float w_scale) :
+        xmin{static_cast<int>((x - w / 2) * w_scale)},
+        ymin{static_cast<int>((y - h / 2) * h_scale)},
+        xmax{static_cast<int>(this->xmin + w * w_scale)},
+        ymax{static_cast<int>(this->ymin + h * h_scale)},
+        class_id{class_id},
+        confidence{confidence} {}
 
     bool operator <(const DetectionObject &s2) const {
         return this->confidence < s2.confidence;
