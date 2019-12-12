74183583bf3e65140c3997a8693cab62865013da
ukhpacha <juangonzalesz@hotmail.com>
2019-12-12 20:47:08 +0100

    Update demos/human_pose_estimation_demo/src/human_pose_estimator.cpp
    Co-Authored-By: Zlobin Vladimir <vladimir.zlobin@intel.com>

--- demos/human_pose_estimation_demo/src/human_pose_estimator.cpp
+++ demos/human_pose_estimation_demo/src/human_pose_estimator.cpp
@@ -158,7 +158,6 @@ void HumanPoseEstimator::swapRequest() {
 std::vector<HumanPose> HumanPoseEstimator::estimateCurr() {
     InferenceEngine::Blob::Ptr pafsBlob = request_curr->GetBlob(pafsBlobName);
     InferenceEngine::Blob::Ptr heatMapsBlob = request_curr->GetBlob(heatmapsBlobName);
-    CV_Assert(heatMapsBlob->getTensorDesc().getDims()[1] == keypointsNumber + 1);
     InferenceEngine::SizeVector heatMapDims = heatMapsBlob->getTensorDesc().getDims();
     std::vector<HumanPose> poses = postprocess(
             heatMapsBlob->buffer(),
