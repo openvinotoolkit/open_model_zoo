3e9f524f1bd504b18439a2fd4b0185b4023790a4
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-01-13 13:03:05 +0300

    MNLI dataset

--- tools/accuracy_checker/accuracy_checker/annotation_converters/text_classification.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/text_classification.py
@@ -17,6 +17,7 @@ from ._nlp_common import get_tokenizer, truncate_seq_pair, SEG_ID_A, SEG_ID_B, S
 InputExample = namedtuple('InputExample', ['guid', 'text_a', 'text_b', 'label'])
 labels = {
     'xnli': ["contradiction", "entailment", "neutral"],
+    'mnli': ["contradiction", "entailment", "neutral"],
     'imdb': ['neg', 'pos'],
     'mrpc': ['0', '1'],
     'cola': ['0', '1']
@@ -73,6 +74,118 @@ class XNLIDatasetConverter(BaseFormatConverter):
                 if self.language_filter and language not in self.language_filter:
                     continue
                 label = self.reversed_label_map[line[1]]
+                text_a = line[6]
+                text_b = line[7]
+                lines.append(InputExample(guid, text_a, text_b, label))
+
+        return lines
+
+    def convert_single_example(self, example):
+        identifier = [
+            'input_ids_{}'.format(example.guid),
+            'input_mask_{}'.format(example.guid),
+            'segment_ids_{}'.format(example.guid)
+        ]
+        tokens_a = self.tokenizer.tokenize(example.text_a)
+        tokens_b = None
+        if example.text_b:
+            tokens_b = self.tokenizer.tokenize(example.text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            truncate_seq_pair(tokens_a, tokens_b, self.max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > self.max_seq_length - 2:
+                tokens_a = tokens_a[:self.max_seq_length - 2]
+        tokens = []
+        segment_ids = []
+        tokens.append("[CLS]" if self.support_vocab else CLS_ID)
+        segment_ids.append(0)
+        for token in tokens_a:
+            tokens.append(token)
+            segment_ids.append(0)
+        tokens.append("[SEP]" if self.support_vocab else SEP_ID)
+        segment_ids.append(0)
+
+        if tokens_b:
+            for token in tokens_b:
+                tokens.append(token)
+                segment_ids.append(1)
+            tokens.append("[SEP]" if self.support_vocab else SEP_ID)
+            segment_ids.append(1)
+
+        input_ids = self.tokenizer.convert_tokens_to_ids(tokens) if self.support_vocab else tokens
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        input_mask = [1] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        padding_size = self.max_seq_length - len(input_ids)
+        if padding_size:
+            padding = [0] * padding_size
+            input_ids.extend(padding)
+            input_mask.extend(padding)
+            segment_ids.extend(padding)
+
+        return TextClassificationAnnotation(
+            identifier, example.label, np.array(input_ids), np.array(input_mask), np.array(segment_ids), tokens
+        )
+
+    def convert(self, check_content=False, progress_callback=None, progress_interval=100, **kwargs):
+        examples = self.read_tsv()
+        annotations = []
+        num_iter = len(examples)
+        for example_id, example in enumerate(examples):
+            annotations.append(self.convert_single_example(example))
+            if progress_callback and example_id % progress_interval == 0:
+                progress_callback(example_id * 100 / num_iter)
+
+        return ConverterReturn(annotations, {'label_map': self.label_map}, None)
+
+
+class MNLIDatasetConverter(BaseFormatConverter):
+    __provider__ = 'mnli'
+    annotation_types = (TextClassificationAnnotation, )
+
+    @classmethod
+    def parameters(cls):
+        params = super().parameters()
+        params.update({
+            'annotation_file': PathField(description='path to annotation file in json or tsv format'),
+            'vocab_file': PathField(description='Path to vocabulary file for word piece tokenizer', optional=True),
+            'sentence_piece_model_file': PathField(description='sentence piece model for tokenization', optional=True),
+            'max_seq_length': NumberField(
+                description='The maximum total input sequence length after tokenization.',
+                optional=True, default=128
+            ),
+            'lower_case': BoolField(optional=True, default=False, description='Switch tokens to lower case register')
+        })
+
+        return params
+
+    def configure(self):
+        self.annotation_file = self.get_value_from_config('annotation_file')
+        self.max_seq_length = self.get_value_from_config('max_seq_length')
+        self.lower_case = self.get_value_from_config('lower_case')
+        self.tokenizer = get_tokenizer(self.config, self.lower_case)
+        mnli_labels = labels['mnli']
+        self.label_map = dict(enumerate(mnli_labels))
+        self.reversed_label_map = {value: key for key, value in self.label_map.items()}
+        self.support_vocab = 'vocab_file' in self.config
+
+    def read_tsv(self):
+        lines = []
+        with self.annotation_file.open('r') as ann_file:
+            reader = csv.reader(ann_file, delimiter="\t", quotechar=None)
+            for idx, line in enumerate(reader):
+                if idx == 0:
+                    continue
+                guid = "dev-{}".format(idx)
+                label = self.reversed_label_map[line[-1]]
                 text_a = line[6]
                 text_b = line[7]
                 lines.append(InputExample(guid, text_a, text_b, label))
