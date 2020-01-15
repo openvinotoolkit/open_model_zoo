e7dd892397d652bbb026b5fec3d060a02cf35318
AnthonyQuantum <anton.vizgalov@intel.com>
2020-01-15 14:24:57 +0300

    Change one flag argument in cases.py, remove vscode files

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -123,7 +123,7 @@ NATIVE_DEMOS = [
         TestCase(options={
             '-no_show': None,
             '-time': '5',
-            '-i': DataPatternArg('imagenet-classification')}),
+            '-i': DataDirectoryArg('imagenet-classification')}),
         single_option_cases('-m',
             ModelArg('alexnet'),
             ModelArg('resnet-50'),
