54fc64446556718cc516298580eb006e69bc1c7d
David Hafner <david.hafner@intel.com>
2020-02-17 10:48:19 +0100

    update task_type

--- tools/downloader/common.py
+++ tools/downloader/common.py
@@ -54,7 +54,7 @@ KNOWN_TASK_TYPES = {
     'human_pose_estimation',
     'image_processing',
     'instance_segmentation',
-    'mono_depth',
+    'monocular_depth_estimation',
     'object_attributes',
     'optical_character_recognition',
     'semantic_segmentation',
