6de967947e51b8c9d813401481c99e1439442cde
AnthonyQuantum <anton.vizgalov@intel.com>
2020-03-04 13:49:46 +0300

    Remove DataOrigFileNamesArg class from args.py

--- demos/tests/args.py
+++ demos/tests/args.py
@@ -73,27 +73,6 @@ class DataPatternArg:
         return str(seq_dir / name_format)
 
 
-class DataOrigFileNamesArg:
-    def __init__(self, sequence_name):
-        self.sequence_name = sequence_name
-
-    def resolve(self, context):
-        seq_dir = context.data_sequence_dir / self.sequence_name
-        seq = [Path(data.resolve(context))
-            for data in context.data_sequences[self.sequence_name]]
-            
-        assert len(set(data.suffix for data in seq)) == 1, "all images in the sequence must have the same extension"
-        assert '%' not in seq[0].suffix
-
-        if not seq_dir.is_dir():
-            seq_dir.mkdir(parents=True)
-
-            for index, data in enumerate(context.data_sequences[self.sequence_name]):
-                shutil.copyfile(data.resolve(context), str(seq_dir / (seq[index].stem + seq[0].suffix)))
-                
-        return str(seq_dir)
-
-
 class DataDirectoryArg:
     def __init__(self, sequence_name):
         self.backend = DataPatternArg(sequence_name)
@@ -105,11 +84,20 @@ class DataDirectoryArg:
 
 class DataDirectoryOrigFileNamesArg:
     def __init__(self, sequence_name):
-        self.backend = DataOrigFileNamesArg(sequence_name)
+        self.sequence_name = sequence_name
 
     def resolve(self, context):
-        pattern = self.backend.resolve(context)
-        return str(Path(pattern))
+        seq_dir = context.data_sequence_dir / self.sequence_name
+        seq = [Path(data.resolve(context))
+            for data in context.data_sequences[self.sequence_name]]
+
+        if not seq_dir.is_dir():
+            seq_dir.mkdir(parents=True)
+
+            for index, data in enumerate(context.data_sequences[self.sequence_name]):
+                shutil.copyfile(seq[index], str(seq_dir / (seq[index].stem + seq[index].suffix)))
+                
+        return str(Path(seq_dir))
 
 
 class DemoFileArg:
