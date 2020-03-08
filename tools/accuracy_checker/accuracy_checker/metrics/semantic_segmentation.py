79dd3fe036097072bcddbc5dd3baf46edb59805e
Katya <ekaterina.aidova@intel.com>
2020-03-09 00:20:44 +0300

    fix

--- tools/accuracy_checker/accuracy_checker/metrics/semantic_segmentation.py
+++ tools/accuracy_checker/accuracy_checker/metrics/semantic_segmentation.py
@@ -3,7 +3,9 @@ Copyright (c) 2019 Intel Corporation
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
       http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,7 +100,7 @@ class SegmentationIOU(SegmentationMetric):
         union = cm.sum(axis=1) + cm.sum(axis=0) - diagonal
         iou = np.divide(diagonal, union, out=np.full_like(diagonal, np.nan), where=union != 0)
         if self.ignore_label is not None:
-            iou = np.delete(iou, self.ignore_index)
+            iou = np.delete(iou, self.ignore_label)
 
         return iou
 
