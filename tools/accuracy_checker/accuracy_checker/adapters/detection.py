5df8b9d0fd41d0c11377ae3ad0eb6680e05ada8b
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-03-23 10:37:31 +0300

    [WIP]: RFCN

--- tools/accuracy_checker/accuracy_checker/adapters/detection.py
+++ tools/accuracy_checker/accuracy_checker/adapters/detection.py
@@ -717,3 +717,53 @@ class ClassAgnosticDetectionAdapter(Adapter):
                 'If you need to use another layer, please specify it explicitly'
             )
         return filter_outputs[0]
+
+
+class RFCNCaffe(Adapter):
+    __provider__ = 'rfcn_loc_with_priors'
+
+    @classmethod
+    def parameters(cls):
+        params = super().parameters()
+        params.update(
+            {
+                'cls_out': StringField(description='bboxes predicted classes score out'),
+                'bbox_out': StringField(
+                    description='bboxes output  with shape [N, 8], where first 4 locs of boxes and last 4 for prior box'
+                )
+            }
+        )
+        return params
+
+    def configure(self):
+        self.cls_out = self.get_value_from_config('cls_out')
+        self.bbox_out = self.get_value_from_config('bbox_out')
+
+    def process(self, raw, identifiers=None, frame_meta=None):
+        raw_out = self._extract_predictions(raw, frame_meta)
+
+
+
+    def bbox_transform_inv(self, boxes, deltas):
+        if boxes.shape[0] == 0:
+            return np.zeros((0, deltas.shape[1]), dtype=deltas.dtype)
+        boxes = boxes.astype(deltas.dtype, copy=False)
+        widths = boxes[:, 2] - boxes[:, 0] + 1.0
+        heights = boxes[:, 3] - boxes[:, 1] + 1.0
+        ctr_x = boxes[:, 0] + 0.5 * widths
+        ctr_y = boxes[:, 1] + 0.5 * heights
+        dx = deltas[:, 0::4]
+        dy = deltas[:, 1::4]
+        dw = deltas[:, 2::4]
+        dh = deltas[:, 3::4]
+        pred_ctr_x = dx * widths[:, np.newaxis] + ctr_x[:, np.newaxis]
+        pred_ctr_y = dy * heights[:, np.newaxis] + ctr_y[:, np.newaxis]
+        pred_w = np.exp(dw) * widths[:, np.newaxis]
+        pred_h = np.exp(dh) * heights[:, np.newaxis]
+        pred_boxes = np.zeros(deltas.shape, dtype=deltas.dtype)
+        pred_boxes[:, 0::4] = pred_ctr_x - 0.5 * pred_w
+        pred_boxes[:, 1::4] = pred_ctr_y - 0.5 * pred_h
+        pred_boxes[:, 2::4] = pred_ctr_x + 0.5 * pred_w
+        pred_boxes[:, 3::4] = pred_ctr_y + 0.5 * pred_h
+
+        return pred_boxes
