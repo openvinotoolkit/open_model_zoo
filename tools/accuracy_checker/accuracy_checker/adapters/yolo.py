96bdc219c8962c0dfb3cc74f8466ef77e9da8154
eizamaliev <eduard.zamaliev@intel.com>
2020-05-21 12:23:50 +0300

    Fixed cells option processing

--- tools/accuracy_checker/accuracy_checker/adapters/yolo.py
+++ tools/accuracy_checker/accuracy_checker/adapters/yolo.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import warnings
 
 import numpy as np
 
@@ -331,8 +332,13 @@ class YoloV3Adapter(Adapter):
         self.do_reshape = self.get_value_from_config('do_reshape')
         self.cells = self.get_value_from_config('cells')
         if self.outputs and len(self.outputs) != len(self.cells):
-            raise ConfigError('Incorrect number of output layer ({}) or detection grid size ({})'
-                              'Must be equal with each other'.format(len(self.outputs), len(self.cells)))
+            if self.do_reshape:
+                raise ConfigError('Incorrect number of output layer ({}) or detection grid size ({}). '
+                                  'Must be equal with each other, check "cells" or "outputs" option'
+                                  .format(len(self.outputs), len(self.cells)))
+            warnings.warn('Number of output layers ({}) not equal to detection grid size ({}). '
+                          'Must be equal with each other, if output tensor resize is required'
+                          .format(len(self.outputs), len(self.cells)))
 
         self.raw_output = self.get_value_from_config('raw_output')
         self.output_format = self.get_value_from_config('output_format')
@@ -376,10 +382,15 @@ class YoloV3Adapter(Adapter):
             nchw_layout = input_shape[1] == 3
             self.processor.width_normalizer = input_shape[3 if nchw_layout else 2]
             self.processor.height_normalizer = input_shape[2 if nchw_layout else 1]
-            for layer_id, (cells, p) in enumerate(zip(self.cells, prediction)):
+            for layer_id, p in enumerate(prediction):
                 anchors = self.masked_anchors[layer_id] if self.masked_anchors else self.anchors
                 num = len(anchors) // 2 if self.masked_anchors else self.num
                 if self.do_reshape or len(p.shape) != 3:
+                    try:
+                        cells = self.cells[layer_id]
+                    except IndexError:
+                        raise ConfigError('Number of output layers ({}) is more than detection grid size ({}). '
+                                          'Check "cells" option.'.format(len(outputs), len(self.cells)))
                     if self.output_format == 'BHW':
                         new_shape = (num * box_size, cells, cells)
                     else:
