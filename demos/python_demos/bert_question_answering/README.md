98d02e2700d83cc94fb887293cac1d4afc3e9dd6
myshevts <maxim.y.shevtsov@intel.com>
2020-05-27 15:28:53 +0300

    brushed readme and comments

--- demos/python_demos/bert_question_answering/README.md
+++ demos/python_demos/bert_question_answering/README.md
@@ -4,7 +4,7 @@ This README describes the Question Answering demo application that uses Squad-tu
 
 ## How It Works
 
-Upon the start-up the demo application reads command line parameters and loads a network to Inference engine and 
+Upon the start-up the demo application reads command line parameters and loads a network to Inference engine and
 an url to the "context" text to search answers for user-provided questions.
 
 
@@ -46,24 +46,51 @@ Options:
 
 ```
 
-> **NOTE**: Before running the demo with a trained model, make sure the model is converted to the Inference Engine format (\*.xml + \*.bin) using the [Model Optimizer tool](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html).
-
-
-You can use the following command to start inference:
-```
-    python3 question_answering_demo.py --vocab=<path_to_model>/vocab.txt --model=<path_to_model>/bert_qa.xml --input_names=['input_ids','attention_mask','token_type_ids'] --output_names=['output_s','output_e'] --input=https://en.wikipedia.org/wiki/Speed_of_light
-```
+> **NOTE**: Before running the demo with a trained model, make sure to convert the model to the Inference Engine's
+> Intermediate Representation format (\*.xml + \*.bin)
+> using the [Model Optimizer tool](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html).
+> When using the pre-trained BERT from the model zoo (please see [Model Downloader](../../../tools/downloader/README.md)),
+> the model is already converted to the IR.
 
 ## Demo Input
 
-The application reads page by the given url and then iterativly answers questions from console
+The application reads text from the html page by the given url and then answers questions typed from the console.
+Notice that since order of inputs for the model does matter, the demo script checks that the inputs specified
+from the command-line match the actual network inputs.
 
 ## Demo Output
 
 The application outputs found answers to the same console.
+
+## Example Cmd-Line
+You can use the following command to try the demo (assuming fp32 model from the Open Model Zoo, downloaded with the
+[Model Downloader](../../../tools/downloader/README.md)):
+```
+    python3 question_answering_demo.py
+            --vocab=<path_to_model>/vocab.txt
+            --model=<path_to_model>/bert-large-uncased-whole-word-masking-squad-fp32-onnx-0001.xml
+            --input_names=['0','1','2']
+            --output_names=['3171','3172']
+            --input=https://en.wikipedia.org/wiki/Bert_(Sesame_Street)
+```
+The demo will use a wiki-page about the Bert character to answer your question like "who is Bert", "how old is Bert", etc.
+
+## Classifying Documents with Long Texts
+Notice that when the original "context" (text from the url) together with the question do not fit the model input
+(usually 384 tokens for the Bert-Large, or 128 for the Bert-Base), the demo splits the context into overlapping segments.
+Thus, for the long texts, the network is called multiple times. The results are then sorted by the probabilities.
+
+## Demo Performance
+Even though, the demo reports inference performance (by measuring wall-clock time for individual inference calls),
+it is rather baseline performance, as certain tricks likes batching,
+[throughput mode] (https://docs.openvinotoolkit.org/latest/_docs_IE_DG_Intro_to_Performance.html).
+Please refer to the full-blown [Benchmark C++ Sample](https://docs.openvinotoolkit.org/latest/_inference_engine_samples_benchmark_app_README.html)
+for any actual performance measurements.
 
 
 ## See Also
 * [Using Open Model Zoo demos](../../README.md)
 * [Model Optimizer](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html)
 * [Model Downloader](../../../tools/downloader/README.md)
+* [Benchmark C++ Sample](https://docs.openvinotoolkit.org/latest/_inference_engine_samples_benchmark_app_README.html)
+
--- demos/python_demos/bert_question_answering/question_answering_demo.py
+++ demos/python_demos/bert_question_answering/question_answering_demo.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python
-import sys
-import os
-from argparse import ArgumentParser, SUPPRESS
-import numpy as np
 import logging as log
+import os
+import string
+import sys
 import time
 import unicodedata
+from argparse import ArgumentParser, SUPPRESS
+
 import bs4
+import numpy as np
 import requests
-import string
 from openvino.inference_engine import IENetwork, IECore
+
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument("-v", "--vocab", help="Required. path to vocabulary file with tokens",
+    args.add_argument("-v", "--vocab", help="Required. path to the vocabulary file with tokens",
                       required=True, type=str)
     args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model",
                       required=True, type=str)
-    args.add_argument("--input_names", help="Required. Names for inputs in networks. For example ['input_ids','attention_mask','token_type_ids']",
+    args.add_argument("--input_names",
+                      help="Required. Input names for the network. For example ['input_ids','attention_mask','token_type_ids']",
                       required=True, type=str)
-    args.add_argument("--output_names", help="Required. Names for outputs in networks. For example ['output_s','output_e']",
+    args.add_argument("--output_names",
+                      help="Required. Output names for the network. For example ['output_s','output_e']",
                       required=True, type=str)
     args.add_argument("--model_squad_ver", help="Optional. SQUAD version used for model fine tuning",
-                      default="1.2",required=False, type=str)
+                      default="1.2", required=False, type=str)
     args.add_argument("-i", "--input", help="Required. Url to a page with context",
                       required=True, type=str)
     args.add_argument("-a", "--max_answer_token_num", help="Optional. Maximum number of tokens in answer",
                       default=15, required=False, type=int)
     args.add_argument("-d", "--device",
-                      help="Optional. Specify the target device to infer on."
+                      help="Optional. Target device to perform inference on."
                            "Default value is CPU",
                       default="CPU", type=str)
     return parser
 
+
 # split word by vocab items and get tok codes
-# iterativly return codes
-def encode_by_voc(w,vocab):
+# iteratively return codes
+def encode_by_voc(w, vocab):
     # remove mark and control chars
     def clean_word(w):
         # extract marks as separate chars to remove them later
@@ -62,10 +67,11 @@ def encode_by_voc(w,vocab):
     if s < len(w):
         yield vocab['[UNK]']
 
-#split big text into words by spaces
-#iterativly return words
+
+# split big text into words by spaces
+# iteratively return words
 def split_to_words(text):
-    prev_is_sep = True # mark initial prev as space to start word from 0 char
+    prev_is_sep = True  # mark initial prev as space to start word from 0 char
     for i, c in enumerate(text + " "):
         is_punc = (c in string.punctuation or unicodedata.category(c)[0] == "P")
         cur_is_sep = (c.isspace() or is_punc)
@@ -76,40 +82,43 @@ def split_to_words(text):
                 yield start, i
                 del start
         if is_punc:
-            yield i, i+1
+            yield i, i + 1
         prev_is_sep = cur_is_sep
 
-# get big text and return list of token id and stren-end position for each id in original texts
+
+# get the text and return list of token ids and start-end position for each id (in the original text)
 def text_to_tokens(text, vocab):
     tokens_id = []
     tokens_se = []
     for s, e in split_to_words(text):
         for tok in encode_by_voc(text[s:e], vocab):
-            tokens_id.append( tok )
-            tokens_se.append( (s, e) )
-
+            tokens_id.append(tok)
+            tokens_se.append((s, e))
+    log.info("Size: {} tokens".format(len(tokens_id)))
     return tokens_id, tokens_se
 
-# return whole sentence as start-end positions by given sub-sentence answer.
-def find_sentence_range(context,s,e):
-    #find start of sentence
-    for c_s in range(s,max(0, s-200),-1):
+
+# return entire sentence as start-end positions for a given answer (within the sentence).
+def find_sentence_range(context, s, e):
+    # find start of sentence
+    for c_s in range(s, max(-1, s - 200), -1):
         if context[c_s] in "\n\.":
             c_s += 1
             break
 
-    #find end of sentence
-    for c_e in range(max(0,e-1),min(len(context), s+200),+1):
+    # find end of sentence
+    for c_e in range(max(0, e - 1), min(len(context), s + 200), +1):
         if context[c_e] in "\n\.":
             break
 
-    return c_s,c_e
+    return c_s, c_e
+
 
 # return context as one big string by given input arguments
 def get_context(args):
     log.info("Get context from {}".format(args.input))
     response = requests.get(args.input)
-    if response is None:
+    if response.status_code == 404:
         raise Exception('URL is invalid')
     html = bs4.BeautifulSoup(response.text, 'html.parser')
     heads = html.select("#firstHeading")
@@ -118,6 +127,7 @@ def get_context(args):
     log.info("Article title: '{}'".format(title))
     context = '\n'.join([par.text for par in paragraphs])
     log.info("Size: {} chars".format(len(context)))
+    log.info("Context: \033[91m {} \033[0m".format(context))
     return context
 
 
@@ -132,14 +142,13 @@ def main():
     version_str = "{}.{}.{}".format(version.major, version.minor, version.build_number)
     log.info("Plugin version is {}".format(version_str))
 
-
-    # Read IR
+    # read IR
     model_xml = args.model
     model_bin = os.path.splitext(model_xml)[0] + ".bin"
     log.info("Loading network files:\n\t{}\n\t{}".format(model_xml, model_bin))
     ie_encoder = ie.read_network(model=model_xml, weights=model_bin)
 
-    #check input and output names
+    # check input and output names
     input_names_model = list(ie_encoder.inputs.keys())
     output_names_model = list(ie_encoder.outputs.keys())
     input_names = eval(args.input_names)
@@ -148,25 +157,24 @@ def main():
     if set(input_names_model) != set(input_names) or set(output_names_model) != set(output_names):
         log.error("Input or Output names don't match")
         log.error("     Network input->output names: {}->{}".format(input_names_model, output_names_model))
-        log.error("     Expected input->output names: {}->{}".format(input_names, output_names))
-        raise Exception("Unexpected nework input or output names")
+        log.error("     Expected (from the demo cmd-line) input->output names: {}->{}".format(input_names, output_names))
+        raise Exception("Unexpected network input or output names")
 
-
-    # Loading model to the device
+    # load model to the device
     log.info("Loading model to the {}".format(args.device))
     ie_encoder_exec = ie.load_network(network=ie_encoder, device_name=args.device)
 
-    #load vocabulary file for model
+    # load vocabulary file for model
     log.info("Loading vocab file:\t{}".format(args.vocab))
     with open(args.vocab, "r", encoding="utf-8") as r:
         vocab = dict((t.rstrip("\n"), i) for i, t in enumerate(r.readlines()))
     log.info("{} tokens loaded".format(len(vocab)))
 
-    #get context as string and then encode it into token id list
+    # get context as a string and encode that  into token id list
     context = get_context(args)
     c_tokens_id, c_tokens_se = text_to_tokens(context, vocab)
 
-    #loop on questions
+    # loop on user's questions
     while True:
         question = input('Type question (enter to exit):')
         if not question:
@@ -174,18 +182,17 @@ def main():
 
         q_tokens_id, _ = text_to_tokens(question, vocab)
 
-        #init some constant that defines token lists sizes
-        #maximum number of tokens processed by network
+        # maximum number of tokens that can be processed by network at once
         max_length = ie_encoder.inputs[input_names[0]].shape[1]
 
-        # calculate number of tokens for context in each request.
+        # calculate number of tokens for context in each inference request.
         # reserve 3 positions for special tokens
         # [CLS] q_tokens [SEP] c_tokens [SEP]
         c_wnd_len = max_length - (len(q_tokens_id) + 3)
 
-        # token num between two neighbours context windows
+        # token num between two neighbour context windows
         # 1/2 means that context windows are overlapped by half
-        c_stride = c_wnd_len//2
+        c_stride = c_wnd_len // 2
 
         t0 = time.time()
         t_count = 0
@@ -193,16 +200,16 @@ def main():
         # array of answers from each window
         answers = []
 
-        #init scan window
+        # init a window to iterate over context
         c_s, c_e = 0, min(c_wnd_len, len(c_tokens_id))
 
-        #iterate while context window is not empty
+        # iterate while context window is not empty
         while c_e > c_s:
-            #form the request
+            # form the request
             tok_cls = vocab['[CLS]']
             tok_sep = vocab['[SEP]']
-            input_ids = [tok_cls] + q_tokens_id  + [tok_sep] + c_tokens_id[c_s:c_e] + [tok_sep]
-            token_type_ids = [0] + [0]*len(q_tokens_id) + [0] + [1] * (c_e - c_s) + [0]
+            input_ids = [tok_cls] + q_tokens_id + [tok_sep] + c_tokens_id[c_s:c_e] + [tok_sep]
+            token_type_ids = [0] + [0] * len(q_tokens_id) + [0] + [1] * (c_e - c_s) + [0]
             attention_mask = [1] * len(input_ids)
 
             # pad the rest of the request
@@ -211,41 +218,43 @@ def main():
             token_type_ids += [0] * pad_len
             attention_mask += [0] * pad_len
 
-            #create numpy inputs for IE
+            # create numpy inputs for IE
             inputs = {
                 input_names[0]: np.array([input_ids], dtype=np.int32),
                 input_names[1]: np.array([attention_mask], dtype=np.int32),
                 input_names[2]: np.array([token_type_ids], dtype=np.int32),
             }
             t_start = time.time()
-            #infer by IE
+            # infer by IE
             res = ie_encoder_exec.infer(inputs=inputs)
             t_end = time.time()
             t_count += 1
             log.info("Sequence of length {} is processed with {:0.2f} sentence/sec ({:0.2} sec per request)".format(
                 max_length,
-                1/(t_end - t_start),
+                1 / (t_end - t_start),
                 t_end - t_start
             ))
-            #get start-end scores for context
+
+            # get start-end scores for context
             def get_score(name):
                 out = np.exp(res[name].reshape((max_length,)))
                 return out / out.sum(axis=-1)
+
             score_s = get_score(output_names[0])
             score_e = get_score(output_names[1])
 
-            #get non answer score (not valid if model has been fine-tuned on squad1.x)
-            if args.model_squad_ver.split('.')[0]=='1':
+            # get 'no-answer' score (not valid if model has been fine-tuned on squad1.x)
+            if args.model_squad_ver.split('.')[0] == '1':
                 score_na = 0
             else:
-                score_na = score_s[0]*score_e[0]
+                score_na = score_s[0] * score_e[0]
 
             # find product of all start-end combinations to find the best one
-            c_s_idx = len(q_tokens_id) + 2 # index of first context token in tensor
-            c_e_idx = max_length-(1+pad_len) # index of last+1 context token in tensor
+            c_s_idx = len(q_tokens_id) + 2  # index of first context token in tensor
+            c_e_idx = max_length - (1 + pad_len)  # index of last+1 context token in tensor
             score_mat = np.matmul(
-                score_s[c_s_idx:c_e_idx].reshape((c_e-c_s, 1)),
-                score_e[c_s_idx:c_e_idx].reshape((1, c_e-c_s))
+                score_s[c_s_idx:c_e_idx].reshape((c_e - c_s, 1)),
+                score_e[c_s_idx:c_e_idx].reshape((1, c_e - c_s))
             )
             # reset candidates with end before start
             score_mat = np.triu(score_mat)
@@ -253,25 +262,24 @@ def main():
             score_mat = np.tril(score_mat, args.max_answer_token_num - 1)
             # find the best start-end pair
             max_s, max_e = divmod(score_mat.flatten().argmax(), score_mat.shape[1])
-            max_score = score_mat[max_s, max_e] * (1-score_na)
+            max_score = score_mat[max_s, max_e] * (1 - score_na)
 
             # convert to context text start-end index
             max_s = c_tokens_se[c_s + max_s][0]
             max_e = c_tokens_se[c_s + max_e][1]
 
-            # check that answers list does not have found answer yet
-            # it could be because of context windows overlapping
-            same = [i for i,a in enumerate(answers) if a[1] == max_s and a[2] == max_e]
+            # check that answers list does not have duplicates (because of context windows overlapping)
+            same = [i for i, a in enumerate(answers) if a[1] == max_s and a[2] == max_e]
             if same:
                 assert len(same) == 1
-                #update exist answer record
+                # update exist answer record
                 a = answers[same[0]]
-                answers[same[0]] = (max(max_score, a[0]), max_s, max_e )
+                answers[same[0]] = (max(max_score, a[0]), max_s, max_e)
             else:
-                #add new record
+                # add new record
                 answers.append((max_score, max_s, max_e))
 
-            #check that context window reach the end
+            # check that context window reach the end
             if c_e == len(c_tokens_id):
                 break
 
@@ -283,16 +291,16 @@ def main():
         log.info("{} requests by {} length are processed by {:0.2f}sec ({:0.2}sec per request)".format(
             t_count,
             max_length,
-            t1-t0,
-            (t1 - t0)/t_count
+            t1 - t0,
+            (t1 - t0) / t_count
         ))
 
-        #print top 3 results
+        # print top 3 results
         answers = list(sorted(answers, key=lambda x: -x[0]))
-        for score,s,e in answers[:3]:
+        for score, s, e in answers[:3]:
             log.info("---answer: {:0.2f} {}".format(score, context[s:e]))
-            c_s,c_e = find_sentence_range(context,s,e)
-            log.info("   "+context[c_s:s] + "\033[91m" + context[s:e] + '\033[0m' + context[e:c_e])
+            c_s, c_e = find_sentence_range(context, s, e)
+            log.info("   " + context[c_s:s] + "\033[91m" + context[s:e] + '\033[0m' + context[e:c_e])
 
 
 if __name__ == '__main__':
