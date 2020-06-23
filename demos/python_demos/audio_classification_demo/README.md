19cf307d45278202ba55d775eb78583d2d95a487
eizamaliev <eduard.zamaliev@intel.com>
2020-06-23 11:10:59 +0300

    Add audio classification demo

---
+++ demos/python_demos/audio_classification_demo/README.md
@@ -0,0 +1,60 @@
+# Audio Classification Python\* Demo
+
+This Readme describes Audio Classification demo application that uses audio classification models. 
+
+## How It Works
+
+Upon the start-up the demo application reads command line parameters and loads a network to Inference engine. It uses only audiofiles in `wav` format. Audio could be converted to different framerate using `-fr/--framerate` option, if necessary for the model. After reading the audio, audio sliced into clips to fit model input (clips could overlap each other with `-ol/--overlap` option) and each clip is processed separately with its own prediction. 
+
+## Running
+
+Run the application with the `-h` option to see the usage message:
+```
+python3 audio_classification_demo.py -h
+```
+The command yields the following usage message:
+```
+usage: audio_classification_demo.py [-h] -i INPUT -m MODEL [-l CPU_EXTENSION]
+                                    [-d DEVICE] [--labels LABELS]
+                                    [-fr FRAMERATE] [-ol OVERLAP]
+
+Options:
+  -h, --help            Show this help message and exit.
+  -i INPUT, --input INPUT
+                        Required. Input to process
+  -m MODEL, --model MODEL
+                        Required. Path to an .xml file with a trained model.
+  -l CPU_EXTENSION, --cpu_extension CPU_EXTENSION
+                        Optional. Required for CPU custom layers. Absolute
+                        path to a shared library with the kernels
+                        implementations.
+  -d DEVICE, --device DEVICE
+                        Optional. Specify the target device to infer on; CPU,
+                        GPU, FPGA, HDDL or MYRIAD is acceptable. The sample
+                        will look for a suitable plugin for device specified.
+                        Default value is CPU
+  --labels LABELS       Optional. Labels mapping file
+  -fr FRAMERATE, --framerate FRAMERATE
+                        Optional. Set framerate for audio input
+  -ol OVERLAP, --overlap OVERLAP
+                        Optional. Set the overlapping between audio clip in
+                        samples or percent
+```
+Running the application with the empty list of options yields the usage message given above and an error message.
+You can use the following command to do inference on GPU with a pre-trained audio classification model and conversion input audio to framerate of 16000:
+```
+    python3 audio_classification_demo.py -i <path_to_wav>/input_audio.wav -m <path_to_model>/aclnet.xml -d GPU --framerate 16000
+```
+
+To run the demo, you can use public or pre-trained models. You can download the pre-trained models with the OpenVINO [Model Downloader](../../../tools/downloader/README.md) or from [https://download.01.org/opencv/](https://download.01.org/opencv/).
+
+> **NOTE**: Before running the demo with a trained model, make sure the model is converted to the Inference Engine format (\*.xml + \*.bin) using the [Model Optimizer tool](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html).
+
+## Demo Output
+
+The demo uses console to display the predictions. It shows classification of each clip with timing of it and total prediction of whole audio.
+
+## See Also
+* [Using Open Model Zoo demos](../../README.md)
+* [Model Optimizer](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html)
+* [Model Downloader](../../../tools/downloader/README.md)
---
+++ demos/python_demos/audio_classification_demo/audio_classification_demo.py
@@ -0,0 +1,197 @@
+from argparse import ArgumentParser, ArgumentError, SUPPRESS
+import logging
+import sys
+import wave
+
+import numpy as np
+from openvino.inference_engine import IECore
+
+
+def type_overlap(val):
+    if isinstance(val, str):
+        if val.endswith('%'):
+            try:
+                res = float(val[:-1]) / 100
+            except FloatingPointError:
+                raise ArgumentError("Wrong value for '--overlap' argument")
+        else:
+            try:
+                res = int(val)
+            except ValueError:
+                raise ArgumentError("Wrong value for '--overlap' argument")
+    else:
+        try:
+            res = int(val)
+        except ValueError:
+            raise ArgumentError("Wrong value for '--overlap' argument")
+    return res
+
+
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+
+    args.add_argument('-i', '--input', type=str, required=True,
+                      help="Required. Input to process")
+    args.add_argument('-m', "--model", type=str, required=True,
+                      help="Required. Path to an .xml file with a trained model.")
+    args.add_argument("-l", "--cpu_extension", type=str, default=None,
+                      help="Optional. Required for CPU custom layers. Absolute path to a shared library with "
+                           "the kernels implementations.")
+    args.add_argument("-d", "--device", type=str, default="CPU",
+                      help="Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is"
+                           " acceptable. The sample will look for a suitable plugin for device specified. "
+                           "Default value is CPU")
+    args.add_argument('--labels', type=str, default=None,
+                      help="Optional. Labels mapping file")
+    args.add_argument('-fr', '--framerate', type=int,
+                      help="Optional. Set framerate for audio input")
+    args.add_argument('-ol', '--overlap', type=type_overlap, default=0,
+                      help='Optional. Set the overlapping between audio clip in samples or percent')
+
+    return parser.parse_args()
+
+
+class AudioSource:
+    def __init__(self, source, channels=2, framerate=None):
+        self.source = source
+        self.framerate = framerate
+        self.channels = channels
+
+    def load(self):
+        framerate, audio = read_wav(self.source, as_float=True)
+        audio = audio.T
+        if audio.shape[0] != self.channels:
+            raise RuntimeError("Audio channels incorrect")
+        if self.framerate:
+            if self.framerate != framerate:
+                audio = resample(audio, framerate, self.framerate)
+        else:
+            self.framerate = framerate
+
+        self.audio = audio
+
+    def chunks(self, size, hop=None):
+        if not hop:
+            hop = size
+        pos = 0
+
+        while pos + size <= self.audio.shape[1]:
+            yield self.audio[:, pos: pos+size]
+            pos += hop
+
+
+def resample(audio, sample_rate, new_sample_rate):
+    duration = audio.shape[1] / float(sample_rate)
+    x_old = np.linspace(0, duration, audio.shape[1])
+    x_new = np.linspace(0, duration, int(duration*new_sample_rate))
+    data = np.array([np.interp(x_new, x_old, channel) for channel in audio])
+
+    return data
+
+
+def read_wav(file, as_float=False):
+    sampwidth_types = {
+        1: np.uint8,
+        2: np.int16,
+        3: np.int32,
+        4: np.int32
+    }
+    sampwidth_max = {
+        1: 255,
+        2: 2**15,
+        3: 2*23,
+        4: 2**31
+    }
+    with wave.open(file, "rb") as wav:
+        params = wav.getparams()
+        data = wav.readframes(params.nframes)
+        if sampwidth_types.get(params.sampwidth):
+            data = np.frombuffer(data, dtype=sampwidth_types[params.sampwidth])
+        else:
+            raise RuntimeError("Couldn't process file {}: unsupported sample width {}"
+                               .format(str(file), params.sampwidth))
+        data = np.reshape(data, (params.nframes, params.nchannels))
+        if as_float:
+            data = data / sampwidth_max[params.sampwidth]
+
+    return params.framerate, data
+
+
+def main():
+    args = build_argparser()
+
+    logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.INFO, stream=sys.stdout)
+    log = logging.getLogger()
+
+    log.info("Creating Inference Engine")
+    ie = IECore()
+
+    if args.device == "CPU" and args.cpu_extension:
+        ie.add_extension(args.cpu_extension, 'CPU')
+
+    log.info("Loading model {}".format(args.model))
+    model_path = args.model[:-4]
+    net = ie.read_network(model_path + ".xml", model_path + ".bin")
+
+    if args.device == "CPU":
+        supported_layers = ie.query_network(net, args.device)
+        not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
+        if len(not_supported_layers) > 0:
+            raise RuntimeError("Following layers are not supported by the {} plugin:\n {}"
+                               .format(args.device, ', '.join(not_supported_layers)))
+
+    if len(net.inputs) != 1:
+        log.error("Demo supports only models with 1 input layer")
+        sys.exit(1)
+    input_blob = next(iter(net.inputs))
+    input_shape = net.inputs[input_blob].shape
+    if len(net.outputs) != 1:
+        log.error("Demo supports only models with 1 output layer")
+        sys.exit(1)
+    output_blob = next(iter(net.outputs))
+
+    log.info("Loading model to the plugin")
+    exec_net = ie.load_network(network=net, device_name=args.device)
+
+    log.info("Preparing input")
+
+    labels = []
+    if args.labels:
+        with open(args.labels, "r") as file:
+            labels = [l.rstrip() for l in file.readlines()]
+
+    _, channels, _, length = input_shape
+
+    audio = AudioSource(args.input, channels=channels, framerate=args.framerate)
+    audio.load()
+
+    hop = length - args.overlap if isinstance(args.overlap, int) else int(length * (1.0 - args.overlap))
+    if hop < 0:
+        log.error("Wrong value for '--overlap' argument - overlapping more than clip length")
+        sys.exit(1)
+
+    log.info("Starting inference")
+    outputs = []
+    for id, chunk in enumerate(audio.chunks(length, hop)):
+        if len(chunk.shape) != len(input_shape):
+            chunk = np.reshape(chunk, newshape=input_shape)
+        output = exec_net.infer(inputs={input_blob: chunk})
+        output = output[output_blob]
+        start_time = id*hop / audio.framerate
+        end_time = (id*hop + length) / audio.framerate
+        outputs.append(output)
+        label = np.argmax(output)
+        log.info("[{:.2f}:{:.2f}] - {:s}: {:.2f}%".format(start_time, end_time,
+                                                          labels[label] if labels else "Class {}".format(label),
+                                                          output[0, label]*100))
+
+    total = np.mean(outputs, axis=0)
+    label = np.argmax(total)
+    log.info("Total over audio - {:s}: {:.2f}%".format(labels[label] if labels else "Class {}".format(label),
+                                                       total[0, label]*100))
+
+
+if __name__ == '__main__':
+    main()
