67899e05b507328dae88c51f23eee08de476705a
Anna Mironova <anna.mironova@intel.com>
2020-07-08 18:54:59 +0300

    Update dataset.py

--- tools/accuracy_checker/accuracy_checker/dataset.py
+++ tools/accuracy_checker/accuracy_checker/dataset.py
@@ -82,9 +82,11 @@ class Dataset:
             annotation = create_subset(annotation, subsample_size, subsample_seed, shuffle)
 
         if self._config.get('analyze_dataset', False):
-            meta['segmentation_masks_source'] = self._config.get('segmentation_masks_source')
+            if self._config.get('segmentation_masks_source'):
+                meta['segmentation_masks_source'] = self._config.get('segmentation_masks_source')
             meta = analyze_dataset(annotation, meta)
-            del meta['segmentation_masks_source']
+            if meta.get('segmentation_masks_source'):
+                del meta['segmentation_masks_source']
 
         if use_converted_annotation and contains_all(self._config, ['annotation', 'annotation_conversion']):
             annotation_name = self._config['annotation']
@@ -235,9 +237,11 @@ class Dataset:
             annotation = create_subset(annotation, subsample_size, subsample_seed)
 
         if self._config.get('analyze_dataset', False):
-            self.metadata['segmentation_masks_source'] = self._config.get('segmentation_masks_source')
+            if self._config.get('segmentation_masks_source'):
+                self.metadata['segmentation_masks_source'] = self._config.get('segmentation_masks_source')
             self.metadata = analyze_dataset(annotation, self.metadata)
-            del self.metadata['segmentation_masks_source']
+            if self.metadata.get('segmentation_masks_source'):
+                del self.metadata['segmentation_masks_source']
 
         self._annotation = annotation
         self.name = self._config.get('name')
