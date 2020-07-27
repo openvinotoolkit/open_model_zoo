956b2cb9de1afa7963dea6c33bdd4584ce74a1fc
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-07-27 11:05:42 +0300

    mel features

--- tools/accuracy_checker/accuracy_checker/preprocessor/audio_preprocessing.py
+++ tools/accuracy_checker/accuracy_checker/preprocessor/audio_preprocessing.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from scipy import signal
 import numpy as np
 
 from ..config import BoolField, BaseField, NumberField, ConfigError, StringField
