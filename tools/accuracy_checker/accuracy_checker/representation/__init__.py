ec0724014ab6612f7210a036d6892d01d78d2abf
Anna Mironova <anna.mironova@intel.com>
2020-08-03 09:40:04 +0300

    Aligned copyright for AC files

--- tools/accuracy_checker/accuracy_checker/representation/__init__.py
+++ tools/accuracy_checker/accuracy_checker/representation/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2019 Intel Corporation
+Copyright (c) 2018-2020 Intel Corporation
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
 
 from .base_representation import BaseRepresentation
 from .classification_representation import (
