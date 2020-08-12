fadb48da5c5ff9d0187501e126cb50be3593b642
VoronovaIntern <anastasia.voronova@intel.com>
2020-08-12 14:39:17 +0300

    final

--- demos/object_detection_demo_ssd_async/ssd.hpp
+++ demos/object_detection_demo_ssd_async/ssd.hpp
@@ -3,14 +3,15 @@
 
 class Ssd :public Model
 {
-private:
-     int maxProposalCount;
-     int objectSize;
-     std::string imageInfoInputName;
+
 public:
     Ssd(const InferenceEngine::Core &ie, std::string networkModel);
     void prepareInputBlobs(bool autoResize);
     void prepareOutputBlobs();
     void setConstInput(InferenceEngine::InferRequest::Ptr& inferReq);
     void processOutput(std::map< std::string, InferenceEngine::Blob::Ptr>& outputs, cv::Mat frame, bool printOutput, double threshold);
+private:
+    int maxProposalCount;
+    int objectSize;
+    std::string imageInfoInputName;
 };
