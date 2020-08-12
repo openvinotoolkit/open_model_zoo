6a4036d7eb91768aa91fcb4022ebe91bd797e23f
Anna Mironova <anna.mironova@intel.com>
2020-08-12 08:06:26 +0300

    Delete unnecessary import

--- tools/accuracy_checker/accuracy_checker/representation/super_resolution_representation.py
+++ tools/accuracy_checker/accuracy_checker/representation/super_resolution_representation.py
@@ -14,9 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import numpy as np
-
-from ..data_readers import BaseReader
 from .image_processing import ImageProcessingAnnotation, ImageProcessingPrediction
 
 
