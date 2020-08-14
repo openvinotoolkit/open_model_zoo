18441808b46ba08807b4b85de203ea5dae458433
Anna Mironova <anna.mironova@intel.com>
2020-08-14 15:48:44 +0300

    AC: Added data_analyzer for reid tasks (#1411)
    * Added data_analyzer for ReIdentificationClassificationAnnotation
    
    * Added data_analyzer for ReIdentificationAnnotation

--- tools/accuracy_checker/accuracy_checker/data_analyzer/__init__.py
+++ tools/accuracy_checker/accuracy_checker/data_analyzer/__init__.py
@@ -22,6 +22,7 @@ from .regression_analyzer import RegressionDataAnalyzer
 from .detection_analyzer import DetectionDataAnalyzer
 from .coco_instance_segmentation_analyzer import CoCoInstanceSegmentationDataAnalyzer
 from .segmentation_analyzer import SegmentationDataAnalyzer
+from .reidentification_analyzer import ReIdentificationDataAnalyzer, ReIdentificationClassificationDataAnalyzer
 
 __all__ = [
     'BaseDataAnalyzer',
@@ -31,5 +32,7 @@ __all__ = [
     'RegressionDataAnalyzer',
     'DetectionDataAnalyzer',
     'CoCoInstanceSegmentationDataAnalyzer',
-    'SegmentationDataAnalyzer'
+    'SegmentationDataAnalyzer',
+    'ReIdentificationDataAnalyzer',
+    'ReIdentificationClassificationDataAnalyzer'
     ]
