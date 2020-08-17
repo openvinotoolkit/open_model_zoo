5e2129cf531253e7396d39652631ef49a8ce4773
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-08-17 16:05:15 +0300

    AC: additional data source as segmentation mask source (#1421)

--- tools/accuracy_checker/accuracy_checker/representation/segmentation_representation.py
+++ tools/accuracy_checker/accuracy_checker/representation/segmentation_representation.py
@@ -90,7 +90,7 @@ class SegmentationAnnotation(SegmentationRepresentation):
     def _load_mask(self):
         if self._mask is None:
             loader_config = self.LOADERS.get(self._mask_loader)
-            data_source = self.metadata.get('segmentation_masks_source')
+            data_source = self.metadata.get('segmentation_masks_source') or self.metadata.get('additional_data_source')
             if data_source is None:
                 data_source = self.metadata['data_source']
             if isinstance(loader_config, str):
