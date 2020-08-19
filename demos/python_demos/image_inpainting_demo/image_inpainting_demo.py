b93ba8a2f469b06ebed2c297922662b0c1ce8709
fzhar <Fedor.Zharinov@intel.com>
2020-08-20 01:09:58 +0300

    Fixes

--- demos/python_demos/image_inpainting_demo/image_inpainting_demo.py
+++ demos/python_demos/image_inpainting_demo/image_inpainting_demo.py
@@ -12,7 +12,6 @@
  limitations under the License.
 """
 
-import sys
 from argparse import ArgumentParser, SUPPRESS
 
 import numpy as np
@@ -23,7 +22,7 @@ from inpainting_gui import InpaintingGUI
 from inpainting import ImageInpainting
 
 
-def build_argparser():
+def buildArgParser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
@@ -35,36 +34,32 @@ def build_argparser():
                            "acceptable. The demo will look for a suitable plugin for device specified. "
                            "Default value is CPU", default="CPU", type=str)
     args.add_argument("-r", "--rnd",
-                      help="Optional. Use random mask for inpainting (with parameters set by -p, -mbw, -mk and -mv).Skipped in GUI mode",
+                      help="Optional. Use random mask for inpainting (with parameters set by -p, -mbw, -mk and -mv)."
+                      " Ignored in GUI mode",
                       action='store_true')
-    args.add_argument("-p", "--parts", help="Optional. Number of parts to draw mask. Skipped in GUI mode", default=8, type=int)
-    args.add_argument("-mbw", "--max_brush_width", help="Optional. Max width of brush to draw mask. Skipped in GUI mode",
+    args.add_argument("-p", "--parts", help="Optional. Number of parts to draw mask. Ignored in GUI mode",
+                      default=8, type=int)
+    args.add_argument("-mbw", "--max_brush_width", help="Optional. Max width of brush to draw mask."
+                     " Ignored in GUI mode",
                       default=24, type=int)
-    args.add_argument("-ml", "--max_length", help="Optional. Max strokes length to draw mask. Skipped in GUI mode",
+    args.add_argument("-ml", "--max_length", help="Optional. Max strokes length to draw mask. Ignored in GUI mode",
                       default=100, type=int)
-    args.add_argument("-mv", "--max_vertex", help="Optional. Max number of vertex to draw mask. Skipped in GUI mode",
+    args.add_argument("-mv", "--max_vertex", help="Optional. Max number of vertex to draw mask. Ignored in GUI mode",
                       default=20, type=int)
     args.add_argument("-mc", "--mask_color",
                       help="Optional. Color to be treated as mask (provide 3 RGB components in range of 0...255)."
-                      " Default is 0 0 0. Skipped in GUI mode", default=[0, 0, 0], type=int, nargs="+")
-    args.add_argument("--no_show", help="Optional. Don't show output. Skipped in GUI mode", action='store_true')
-    args.add_argument("-o", "--output", help="Optional. Save output to the file with provided filename. Skipped in GUI mode",
-                      default="", type=str)
-    args.add_argument("-a", "--auto", help="Optional. Use automatic (non-interactive) mode instead of GUI", action='store_true')
+                      " Default is 0 0 0. Ignored in GUI mode", default=[0, 0, 0], type=int, nargs=3)
+    args.add_argument("--no_show", help="Optional. Don't show output. Ignored in GUI mode", action='store_true')
+    args.add_argument("-o", "--output", help="Optional. Save output to the file with provided filename."
+                     " Ignored in GUI mode", default="", type=str)
+    args.add_argument("-a", "--auto", help="Optional. Use automatic (non-interactive) mode instead of GUI",
+                      action='store_true')
 
     return parser
 
 
-def loadImage(imgPath):
-    img = cv2.imread(imgPath, cv2.IMREAD_COLOR)
-    if img is None:
-        print(f"Cannot load image {imgPath}")
-        exit()
-    return img
-
-
 def createRandomMask(parts, max_vertex, max_length, max_brush_width, h, w, max_angle=360):
-    mask = np.zeros((h,w,1), dtype=np.float32)
+    mask = np.zeros((h, w, 1), dtype=np.float32)
     for _ in range(parts):
         num_strokes = np.random.randint(max_vertex)
         start_y = np.random.randint(h)
@@ -87,50 +82,53 @@ def createRandomMask(parts, max_vertex, max_length, max_brush_width, h, w, max_a
     return mask
 
 
-def inpaintRandomHoles(args):
-    if len(args.mask_color) != 3:
-        print("Invalid mask_color is provided. Please provide 3 RGB components\n")
-        exit()
-    mask_color=args.mask_color[::-1] # argument comes in RGB mode, but we will use BGR notation below
+def inpaintRandomHoles(img,args):
+    mask_color = args.mask_color[::-1] # argument comes in RGB mode, but we will use BGR notation below
 
     ie = IECore()
-    img = loadImage(args.input)
 
     inpainting_processor = ImageInpainting(ie, args.model, args.device)
 
     #--- Resize to model input and generate random mask
-    img = cv2.resize(img, (inpainting_processor.input_width,inpainting_processor.input_height))
+    img = cv2.resize(img, (inpainting_processor.input_width, inpainting_processor.input_height))
 
     if args.rnd:
-        mask = createRandomMask(args.parts,args.max_vertex,args.max_length,args.max_brush_width,
-                                inpainting_processor.input_height,inpainting_processor.input_width)
+        mask = createRandomMask(args.parts, args.max_vertex, args.max_length, args.max_brush_width,
+                                inpainting_processor.input_height, inpainting_processor.input_width)
     else:
-        top = np.full(img.shape,mask_color,np.uint8)
-        mask = cv2.inRange(img,top,top) / 255
-        mask = np.expand_dims(mask,2)
+        top = np.full(img.shape, mask_color, np.uint8)
+        mask = cv2.inRange(img, top, top) / 255
+        mask = np.expand_dims(mask, 2)
 
     masked_image = (img * (1 - mask) + 255 * mask).astype(np.uint8)
 
     #--- Inpaint and show results
-    output_image = inpainting_processor.process(masked_image,mask)
+    output_image = inpainting_processor.process(masked_image, mask)
     concat_imgs = np.hstack((masked_image, output_image))
     cv2.putText(concat_imgs, 'summary: {:.1f} FPS'.format(float(1 / inpainting_processor.infer_time)), (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 200))
-    return concat_imgs,output_image
+    return concat_imgs, output_image
 
 def main():
-    args = build_argparser().parse_args()
+    args = buildArgParser().parse_args()
+
+    # Loading source image
+    img = cv2.imread(args.input, cv2.IMREAD_COLOR)
+    if img is None:
+        print("Cannot load image " + args.input)
+        return -1
 
     if args.auto:
         # Command-line inpaining for just one image
-        concat_image,result = inpaintRandomHoles(args)
+        concat_image, result = inpaintRandomHoles(img,args)
         if args.output != "":
-            cv2.imwrite(args.output,result)
+            cv2.imwrite(args.output, result)
         if not args.no_show:
             cv2.imshow('Image Inpainting Demo', concat_image)
             cv2.waitKey(0)
     else:
         # Inpainting with GUI
-        InpaintingGUI(loadImage(args.input),args.model, args.device).run()
+        InpaintingGUI(img, args.model, args.device).run()
+    return 0
 
 if __name__ == "__main__":
-    main()
+    exit(main())
--- demos/python_demos/image_inpainting_demo/inpainting_gui.py
+++ demos/python_demos/image_inpainting_demo/inpainting_gui.py
@@ -19,113 +19,109 @@ from openvino.inference_engine import IECore
 
 class InpaintingGUI(object):
     def __init__(self, srcImg, modelPath, device="CPU"):
-        self.wndName="Inpainting (press H for help)"
-        self.maskColor=(255,0,0)
-        self.radius=10
-        self.oldPoint=(-1,-1)
+        self.wndName = "Inpainting demo (press H for help)"
+        self.maskColor = (255, 0, 0)
+        self.radius = 10
+        self.oldPoint = None
 
-        self.inpainter = ImageInpainting(IECore(),modelPath,device)
-   
-        self.img = cv2.resize(srcImg,(self.inpainter.input_width,self.inpainter.input_height))
+        self.inpainter = ImageInpainting(IECore(), modelPath, device)
+
+        self.img = cv2.resize(srcImg, (self.inpainter.input_width, self.inpainter.input_height))
         self.originalImg = self.img.copy()
         self.label = ""
-        self.mask = np.zeros((self.inpainter.input_height,self.inpainter.input_width,1),dtype=np.float32)
+        self.mask = np.zeros((self.inpainter.input_height, self.inpainter.input_width, 1), dtype=np.float32)
 
-        cv2.namedWindow(self.wndName, cv2.WINDOW_AUTOSIZE )
-        cv2.setMouseCallback(self.wndName,self.onMouse)
-        cv2.createTrackbar("Brush size",self.wndName,self.radius,30,self.onTrackBar)
-        cv2.setTrackbarMin("Brush size",self.wndName,1)
+        cv2.namedWindow(self.wndName, cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback(self.wndName, self.onMouse)
+        cv2.createTrackbar("Brush size", self.wndName, self.radius, 30, self.onTrackBar)
+        cv2.setTrackbarMin("Brush size", self.wndName, 1)
 
-        self.isHelpShown=False
-        self.isOriginalShown=False
+        self.isHelpShown = False
+        self.isOriginalShown = False
 
 
-    def onMouse(self, event, x,y, flags, param):
-        if flags==cv2.EVENT_FLAG_LBUTTON and not self.isOriginalShown:
-            if self.oldPoint[0]!=-1:
-                cv2.line(self.mask,self.oldPoint,(x,y),1,self.radius*2)
-            cv2.circle(self.mask,(x,y),self.radius,1,cv2.FILLED)
-            self.oldPoint=(x,y)
+    def onMouse(self, _event, x, y, flags, _param):
+        if flags == cv2.EVENT_FLAG_LBUTTON and not self.isOriginalShown:
+            if self.oldPoint is not None:
+                cv2.line(self.mask, self.oldPoint, (x, y), 1, self.radius*2)
+            cv2.circle(self.mask, (x, y), self.radius, 1, cv2.FILLED)
+            self.oldPoint = (x, y)
 
             self.updateWindow()
         else:
-            self.oldPoint=(-1,-1)
+            self.oldPoint = None
 
 
     def run(self):
         self.updateWindow()
 
-        if self.img is None:
-            print("Cannot load source image\n")
-            return
-
-        key= cv2.waitKey(1)
-        while key not in (27,ord('q'),ord('Q')):
-            if key in (ord(" "),ord("\r")):
+        key = ""
+        while key not in (27, ord('q'), ord('Q')):
+            if key in (ord(" "), ord("\r")):
                 self.isOriginalShown = False
                 self.showInfo("Processing...")
 
-                self.img[:,:,0:1][self.mask>0]=0
-                self.img[:,:,1:2][self.mask>0]=0
-                self.img[:,:,2:3][self.mask>0]=0
-                self.img=self.inpainter.process(self.img,self.mask)
+                self.img[:, :, 0:1][self.mask > 0] = 0
+                self.img[:, :, 1:2][self.mask > 0] = 0
+                self.img[:, :, 2:3][self.mask > 0] = 0
+                self.img = self.inpainter.process(self.img, self.mask)
 
                 self.showInfo("")
-                self.mask[:,:,:]=0
+                self.mask[:, :, :] = 0
                 self.updateWindow()
-            elif key in (8,ord('c'),ord('C')): # Backspace or c
+            elif key in (8, ord('c'), ord('C')): # Backspace or c
                 self.isOriginalShown = False
-                self.mask[:,:,:]=0
+                self.mask[:, :, :] = 0
                 self.updateWindow()
             elif key == ord('\t'):
                 self.isOriginalShown = not self.isOriginalShown
                 self.updateWindow()
-            elif key in (ord('h'),ord('H')):
+            elif key in (ord('h'), ord('H')):
                 if not self.isHelpShown:
                     self.showInfo("Use mouse with LMB to paint\nBksp or C to clear\nSpace or Enter to inpaint\nTab to show original image\nEsc or Q to quit")
-                    self.isHelpShown=True
+                    self.isHelpShown = True
                 else:
                     self.showInfo("")
-                    self.isHelpShown=False
+                    self.isHelpShown = False
 
             key = cv2.waitKey(1)
 
 
-    def onTrackBar(self,x):
-        self.radius=x
+    def onTrackBar(self, x):
+        self.radius = x
 
 
-    def showInfo(self,text):
-        self.label=text
+    def showInfo(self, text):
+        self.label = text
         self.updateWindow()
         cv2.waitKey(1) # This is required to actually paint window contents right away
-        self.isHelpShown=False # Any other label removes help from the screen
+        self.isHelpShown = False # Any other label removes help from the screen
 
 
     def updateWindow(self):
-        pad=10
-        margin=10
+        pad = 10
+        margin = 10
         if self.isOriginalShown:
-            backbuffer=self.originalImg.copy()
-            sz = cv2.getTextSize("Original", cv2.FONT_HERSHEY_COMPLEX, 0.75,1)[0]
+            backbuffer = self.originalImg.copy()
+            sz = cv2.getTextSize("Original", cv2.FONT_HERSHEY_COMPLEX, 0.75, 1)[0]
             imgWidth = backbuffer.shape[1]
-            labelArea = backbuffer[margin:sz[1]+pad*2+margin,imgWidth-margin-(sz[0]+pad*2):imgWidth-margin]
-            labelArea//=2
-            cv2.putText(backbuffer,"Original",(imgWidth-margin-sz[0]-pad,margin+sz[1]+pad),cv2.FONT_HERSHEY_COMPLEX, 0.75,(128,255,128))
+            labelArea = backbuffer[margin:sz[1]+pad*2+margin, imgWidth-margin-(sz[0]+pad*2):imgWidth-margin]
+            labelArea //= 2
+            cv2.putText(backbuffer, "Original", (imgWidth-margin-sz[0]-pad, margin+sz[1]+pad), cv2.FONT_HERSHEY_COMPLEX, 0.75, (128, 255, 128))
         else:
             backbuffer = self.img.copy()
-            backbuffer[:,:,0:1][self.mask>0]=self.maskColor[0]
-            backbuffer[:,:,1:2][self.mask>0]=self.maskColor[1]
-            backbuffer[:,:,2:3][self.mask>0]=self.maskColor[2]
+            backbuffer[:, :, 0:1][self.mask > 0] = self.maskColor[0]
+            backbuffer[:, :, 1:2][self.mask > 0] = self.maskColor[1]
+            backbuffer[:, :, 2:3][self.mask > 0] = self.maskColor[2]
 
-        if self.label is not None and self.label!="":
+        if self.label is not None and self.label != "":
             lines = self.label.split("\n")
             count = len(lines)
-            w = max(cv2.getTextSize(line, cv2.FONT_HERSHEY_COMPLEX, 0.75,1)[0][0] for line in lines)+pad*2
-            lineH = cv2.getTextSize(lines[0], cv2.FONT_HERSHEY_COMPLEX, 0.75,1)[0][1]+pad
-            labelArea=backbuffer[margin:lineH*count+pad*2+margin,margin:w+margin]
-            labelArea//=2
-            for i,line in enumerate(lines):
-                cv2.putText(backbuffer,line,(pad+margin,margin+(i+1)*lineH),cv2.FONT_HERSHEY_COMPLEX, 0.75,(192,192,192))
+            w = max(cv2.getTextSize(line, cv2.FONT_HERSHEY_COMPLEX, 0.75, 1)[0][0] for line in lines) + pad*2
+            lineH = cv2.getTextSize(lines[0], cv2.FONT_HERSHEY_COMPLEX, 0.75, 1)[0][1] + pad
+            labelArea = backbuffer[margin:lineH*count+pad*2+margin, margin:w+margin]
+            labelArea //= 2
+            for i, line in enumerate(lines):
+                cv2.putText(backbuffer, line, (pad+margin, margin+(i+1)*lineH), cv2.FONT_HERSHEY_COMPLEX, 0.75, (192, 192, 192))
 
         cv2.imshow(self.wndName, backbuffer)
