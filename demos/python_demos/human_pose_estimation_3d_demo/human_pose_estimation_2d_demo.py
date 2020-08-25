51976be583fbf59255b161853110d943fcc177d7
playboy007gy <50794318+playboy007gy@users.noreply.github.com>
2020-08-25 16:15:19 +0800

    Update human_pose_estimation_2d_demo.py

--- demos/python_demos/human_pose_estimation_3d_demo/human_pose_estimation_2d_demo.py
+++ demos/python_demos/human_pose_estimation_3d_demo/human_pose_estimation_2d_demo.py
@@ -24,19 +24,8 @@ from modules.input_reader import InputReader
 from modules.draw import Plotter3d, draw_poses
 from modules.parse_poses import parse_poses
 
-
-def rotate_poses(poses_3d, R, t):
-    R_inv = np.linalg.inv(R)
-    for pose_id in range(poses_3d.shape[0]):
-        pose_3d = poses_3d[pose_id].reshape((-1, 4)).transpose()
-        pose_3d[0:3] = np.dot(R_inv, pose_3d[0:3] - t)
-        poses_3d[pose_id] = pose_3d.transpose().reshape(-1)
-
-    return poses_3d
-
-
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Lightweight 3D human pose estimation demo. '
+    parser = ArgumentParser(description='Lightweight 2D human pose estimation demo. '
                                         'Press esc to exit, "p" to (un)pause video or process next image.',
                             add_help=False)
     args = parser.add_argument_group('Options')
@@ -66,20 +55,12 @@ if __name__ == '__main__':
 
     stride = 8
     inference_engine = InferenceEngine(args.model, args.device, stride)
-    canvas_3d = np.zeros((720, 1280, 3), dtype=np.uint8)
-    plotter = Plotter3d(canvas_3d.shape[:2])
-    canvas_3d_window_name = 'Canvas 3D'
+    canvas_2d = np.zeros((720, 1280, 3), dtype=np.uint8)
+    plotter = Plotter3d(canvas_2d.shape[:2])
+    canvas_2d_window_name = 'Canvas 2D'
     if not args.no_show:
-        cv2.namedWindow(canvas_3d_window_name)
-        cv2.setMouseCallback(canvas_3d_window_name, Plotter3d.mouse_callback)
-
-    #file_path = args.extrinsics_path
-    #if file_path is None:
-    #    file_path = os.path.join(os.path.dirname(__file__), 'data', 'extrinsics.json')
-    #with open(file_path, 'r') as f:
-    #    extrinsics = json.load(f)
-    #R = np.array(extrinsics['R'], dtype=np.float32)
-    #t = np.array(extrinsics['t'], dtype=np.float32)
+        cv2.namedWindow(canvas_2d_window_name)
+        cv2.setMouseCallback(canvas_2d_window_name, Plotter3d.mouse_callback)
 
     frame_provider = InputReader(args.input)
     is_video = frame_provider.is_video
@@ -99,20 +80,8 @@ if __name__ == '__main__':
             fx = np.float32(0.8 * frame.shape[1])
 
         inference_result = inference_engine.infer(scaled_img)
-        #poses_3d, 
+
         poses_2d = parse_poses(inference_result, input_scale, stride, fx, is_video)
-        #edges = []
-        #if len(poses_3d) > 0:
-        #    poses_3d = rotate_poses(poses_3d, R, t)
-        #    poses_3d_copy = poses_3d.copy()
-        #    x = poses_3d_copy[:, 0::4]
-        #    y = poses_3d_copy[:, 1::4]
-        #    z = poses_3d_copy[:, 2::4]
-        #    poses_3d[:, 0::4], poses_3d[:, 1::4], poses_3d[:, 2::4] = -z, x, -y
-        #
-        #    poses_3d = poses_3d.reshape(poses_3d.shape[0], 19, -1)[:, :, 0:3]
-        #    edges = (Plotter3d.SKELETON_EDGES + 19 * np.arange(poses_3d.shape[0]).reshape((-1, 1, 1))).reshape((-1, 2))
-        #plotter.plot(canvas_3d, poses_3d, edges)
 
         draw_poses(frame, poses_2d)
         current_time = (cv2.getTickCount() - current_time) / cv2.getTickFrequency()
@@ -124,8 +93,7 @@ if __name__ == '__main__':
                     (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
         if args.no_show:
             continue
-        #cv2.imshow(canvas_3d_window_name, canvas_3d)
-        cv2.imshow('3D Human Pose Estimation', frame)
+        cv2.imshow('2D Human Pose Estimation', frame)
 
         key = cv2.waitKey(delay)
         if key == esc_code:
@@ -135,13 +103,12 @@ if __name__ == '__main__':
                 delay = 0
             else:
                 delay = 1
-        if delay == 0 or not is_video:  # allow to rotate 3D canvas while on pause
+        if delay == 0 or not is_video:  
             key = 0
             while (key != p_code
                    and key != esc_code
                    and key != space_code):
-                #plotter.plot(canvas_3d, poses_3d, edges)
-                #cv2.imshow(canvas_3d_window_name, canvas_3d)
+
                 key = cv2.waitKey(33)
             if key == esc_code:
                 break
