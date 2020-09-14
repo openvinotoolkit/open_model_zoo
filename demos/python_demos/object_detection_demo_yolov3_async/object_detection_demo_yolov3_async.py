17738472134a81e889c4c9ecc72e01c5cd5f1784
AnthonyQuantum <anton.vizgalov@intel.com>
2020-09-14 16:20:45 +0300

    [WIP] Turn off all drawing over frame

--- demos/python_demos/object_detection_demo_yolov3_async/object_detection_demo_yolov3_async.py
+++ demos/python_demos/object_detection_demo_yolov3_async/object_detection_demo_yolov3_async.py
@@ -436,17 +436,17 @@ def main():
                 #             "#" + det_label + ' ' + str(round(obj['confidence'] * 100, 1)) + ' %',
                 #             (obj['xmin'], obj['ymin'] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
 
-            helpers.put_highlighted_text(frame, "{} mode".format(mode.current.name), (10, int(origin_im_size[0] - 20)),
-                                         cv2.FONT_HERSHEY_COMPLEX, 0.75, (10, 10, 200), 2)
+            # helpers.put_highlighted_text(frame, "{} mode".format(mode.current.name), (10, int(origin_im_size[0] - 20)),
+            #                              cv2.FONT_HERSHEY_COMPLEX, 0.75, (10, 10, 200), 2)
             
             if is_same_mode and prev_mode_active_request_count == 0:
                 mode_metrics[mode.current].update(start_time, frame)
             else:
                 mode_metrics[mode.get_other()].update(start_time, frame)
                 prev_mode_active_request_count -= 1
-                helpers.put_highlighted_text(frame, "Switching modes, please wait...",
-                                             (10, int(origin_im_size[0] - 50)), cv2.FONT_HERSHEY_COMPLEX, 0.75,
-                                             (10, 200, 10), 2)
+                # helpers.put_highlighted_text(frame, "Switching modes, please wait...",
+                #                              (10, int(origin_im_size[0] - 50)), cv2.FONT_HERSHEY_COMPLEX, 0.75,
+                #                              (10, 200, 10), 2)
 
             if args.output:
                 out_video.write(frame)
