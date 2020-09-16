ab050d8cd9e2a318823f4a7fffb5e9570a603a77
Alexey, Khorkin <alexeykhorkin12@gmail.com>
2020-09-16 17:19:11 +0300

    update README replace libuser ext to support reshape operation fix code style to fit to pep8

--- demos/python_demos/facies_classification_demo/README.md
+++ demos/python_demos/facies_classification_demo/README.md
@@ -24,16 +24,10 @@ python3 facies_classification_demo.py -h
 ```
 The command yields the following usage message:
 ```
-usage: facies_classification_demo.py [-h] -m MODEL -i DATA_PATH
+usage: facies_classification_demo.py [-h] -m MODEL -i DATA_PATH -l
+                                     CPU_EXTENSION
                                      [-t {inline,crossline,timeline}]
-                                     [-s SLICE_INDEX] [-l CPU_EXTENSION]
-                                     [-d "<device>"]
-
-optional arguments:
-  -l CPU_EXTENSION, --cpu_extension CPU_EXTENSION
-                        Optional. Required for CPU custom layers. Absolute
-                        MKLDNN (CPU)-targeted custom layers. Absolute path to
-                        a shared library with the kernels implementations
+                                     [-s SLICE_INDEX] [-d "<device>"]
 
 Options:
   -h, --help            Show this help message and exit.
@@ -41,6 +35,11 @@ Options:
                         Required. Path to an .xml file with a trained model.
   -i DATA_PATH, --data_path DATA_PATH
                         Required. Path to seismic datafile.
+  -l CPU_EXTENSION, --cpu_extension CPU_EXTENSION
+                        Required. Required for CPU custom layers. (MaxUnpool
+                        layers in this case) Absolute MKLDNN (CPU)-targeted
+                        custom layers. Absolute path to a shared library with
+                        the kernels implementations
   -t {inline,crossline,timeline}, --slice_type {inline,crossline,timeline}
                         Type of slice .
   -s SLICE_INDEX, --slice_index SLICE_INDEX
@@ -57,7 +56,7 @@ To run the demo, you can use public or pre-trained models. You can download the 
 
 ## Demo Output
 
-The application uses matplotlib to display resulting instance segmentation masks, saving the picture to the current directory.
+The application uses matplotlib to display resulting instance classification masks, saving the picture to the current directory.
 
 ![](./facies_classification_demo.png)
 
--- demos/python_demos/facies_classification_demo/facies_classification_demo.py
+++ demos/python_demos/facies_classification_demo/facies_classification_demo.py
@@ -18,11 +18,12 @@
 import logging
 import os
 import sys
+from argparse import ArgumentParser, SUPPRESS
 
-import numpy as np
 import matplotlib.pyplot as plt
 
-from argparse import ArgumentParser, SUPPRESS
+import numpy as np
+
 from openvino.inference_engine import IECore
 
 
@@ -32,10 +33,18 @@ def build_argparser():
     args.add_argument('-h', '--help', action='help', default=SUPPRESS,
                       help='Show this help message and exit.')
     args.add_argument('-m', '--model',
-                      help='Required. Path to an .xml file with a trained model.',
+                      help='Required. \
+                      Path to an .xml file with a trained model.',
                       required=True, type=str)
     args.add_argument('-i', '--data_path',
                       help='Required. Path to seismic datafile.',
+                      required=True, type=str)
+    args.add_argument("-l", "--cpu_extension",
+                      help="Required. Required for CPU custom layers. \
+                      (MaxUnpool layers in this case) \
+                      Absolute MKLDNN (CPU)-targeted custom layers. "
+                      "Absolute path to a shared library \
+                      with the kernels implementations",
                       required=True, type=str)
     args.add_argument('-t', '--slice_type',
                       help='Type of slice .',
@@ -44,39 +53,49 @@ def build_argparser():
     args.add_argument('-s', '--slice_index',
                       help='Index of slice .',
                       type=int, default=0)
-    parser.add_argument("-l", "--cpu_extension", 
-                      help="Optional. Required for CPU custom layers. Absolute MKLDNN (CPU)-targeted custom layers. "
-                      "Absolute path to a shared library with the kernels implementations", type=str, default="libuser_cpu_extension_for_max_unpool.so")
     args.add_argument('-d', '--device',
-                      help='Optional. Specify the target device to infer on: CPU, GPU, FPGA, HDDL or MYRIAD. '
-                           'The demo will look for a suitable plugin for device specified '
-                           '(by default, it is CPU).',
+                      help='Optional. Specify the target device to infer on: \
+                      CPU, GPU, FPGA, HDDL or MYRIAD. '
+                      'The demo will look for a suitable plugin \
+                      for device specified '
+                      '(by default, it is CPU).',
                       default='CPU', type=str, metavar='"<device>"')
     return parser
+
 
 def get_logger():
     ''' Setting up Logging and return logger.'''
     INFO, INFER = 5, 6
-
     logging.addLevelName(INFO, 'INFO')
+
     def info(self, message, *args, **kws):
-        self.log(INFO, message, *args, **kws) 
+        self.log(INFO, message, *args, **kws)
     logging.Logger.info = info
 
     logging.addLevelName(INFER, 'INFER')
+
     def infer(self, message, *args, **kws):
-        self.log(INFER, message, *args, **kws) 
+        self.log(INFER, message, *args, **kws)
     logging.Logger.infer = infer
 
-    logging.basicConfig(format='[%(levelname)s] %(message)s', stream=sys.stdout)
+    logging.basicConfig(format='[%(levelname)s] %(message)s',
+                        stream=sys.stdout)
     logger = logging.getLogger('conv_deconvnet_demo')
     logger.setLevel(INFO)
-
     return logger
+
+
+def normalize(data, mu=0, std=1):
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+    data = (data - data.flatten().mean())/data.flatten().std()
+    return data * std + mu
+
 
 def load_data(data_path, slice_index, slice_type):
     data_format = data_path.split('.')[1]
-    assert not (data_path.split('.')[0] == '' or data_format == ''), f'Invalid path to data file: {data_path}'
+    assert not (data_path.split('.')[0] == '' or data_format == ''), \
+        f'Invalid path to data file: {data_path}'
     if data_format == 'npy':
         data = np.load(data_path)
     elif data_format == 'dat':
@@ -85,25 +104,30 @@ def load_data(data_path, slice_index, slice_type):
         import segyio
         data = segyio.tools.cube(data_path)
         data = np.moveaxis(data, -1, 0)
-        data = np.ascontiguousarray(data,'float32')
+        data = np.ascontiguousarray(data, 'float32')
     else:
-        assert False, f'Unsupported data format: {data_format}' 
-    
+        assert False, f'Unsupported data format: {data_format}'
 
-    assert slice_index >= 0, 'Invalid slice index argument, slice index must not be negative'
+    data = normalize(data, mu=1e-8, std=0.2097654)
+    print(f"[INFO] Dataset has been loaded, shape is {data.shape}")
+    print(f"[INFO] Dataset mean is {data.flatten().mean():.5f}, std {data.flatten().std():.5f}")
+
+    assert slice_index >= 0, \
+        'Invalid slice index argument, slice index must not be negative'
     x, y, z = data.shape
-    if slice_type=='inline':
-        assert slice_index < x , f'Invalid slice index'
+    if slice_type == 'inline':
+        assert slice_index < x, f'Invalid slice index, must be in {[0, x - 1]}'
         x_clice = data[slice_index, :, :]
-    elif slice_type=='crossline':
-        assert slice_index < y , f'Invalid slice index'
+    elif slice_type == 'crossline':
+        assert slice_index < y, f'Invalid slice index, must be in {[0, y - 1]}'
         x_clice = data[:, slice_index, :]
-    elif slice_type=='timeline':
-        assert slice_index < z , f'Invalid slice index'
+    elif slice_type == 'timeline':
+        assert slice_index < z, f'Invalid slice index, must be in {[0, z - 1]}'
         x_clice = data[:, :, slice_index]
     return x_clice.T
-    
-def decode_segmap(label_mask, plot=False):
+
+
+def decode_segmap(label_mask):
     """Decode segmentation class labels into a color image
     Args:
         label_mask (np.ndarray): an (M,N) array of integer values denoting
@@ -113,8 +137,9 @@ def decode_segmap(label_mask, plot=False):
     Returns:
         (np.ndarray, optional): the resulting decoded color image.
     """
-    label_colours = np.asarray([ [69,117,180], [145,191,219], [224,243,248], 
-                                    [254,224,144], [252,141,89], [215,48,39]])
+    label_colours = np.asarray([[69, 117, 180], [145, 191, 219],
+                                [224, 243, 248], [254, 224, 144],
+                                [252, 141, 89], [215, 48, 39]])
     r = label_mask.copy()
     g = label_mask.copy()
     b = label_mask.copy()
@@ -126,13 +151,8 @@ def decode_segmap(label_mask, plot=False):
     rgb[:, :, 0] = r / 255.0
     rgb[:, :, 1] = g / 255.0
     rgb[:, :, 2] = b / 255.0
-    if plot:
-        h, w = rgb.shape[:-1]
-        plt.figure(figsize=(10, int(10*w/h)))
-        plt.imshow(rgb)
-        plt.show()
-    else:
-        return rgb
+    return rgb
+
 
 def show_interpretation(input_to_net, out_from_net):
     from matplotlib.colors import LinearSegmentedColormap
@@ -140,22 +160,25 @@ def show_interpretation(input_to_net, out_from_net):
     res = np.argmax(out_from_net, axis=1).squeeze()
     res_image = decode_segmap(res)
 
-    color_list = np.asarray([ [69,117,180], [145,191,219], [224,243,248], 
-                                    [254,224,144], [252,141,89], [215,48,39]])/255
+    color_list = np.asarray([[69, 117, 180], [145, 191, 219],
+                            [224, 243, 248], [254, 224, 144],
+                            [252, 141, 89], [215, 48, 39]]) / 255
     cm = LinearSegmentedColormap.from_list('custom_cmap', color_list, N=6)
 
-    fig, axs = plt.subplots(1, 2, figsize=(13,5))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
     fig.suptitle("Facies classification results", fontsize=22)
     axs[0].imshow(input_to_net, cmap='gray')
     axs[0].set_title('Input slice')
-    
+
     im = axs[1].imshow(res_image, cmap=cm)
     axs[1].set_title('Interpretation of the slice')
 
-    cbaxes = fig.add_axes([0.91, 0.2, 0.01, 0.6]) 
-    cb = fig.colorbar(im, ax=axs[1], ticks=[0.33, 0.45, 0.57, 0.69, 0.82, 0.93], cax = cbaxes) 
+    cbaxes = fig.add_axes([0.91, 0.2, 0.01, 0.6])
+    cb = fig.colorbar(im, ax=axs[1], cax=cbaxes,
+                      ticks=[0.33, 0.45, 0.57, 0.69, 0.82, 0.93])
     cb.ax.set_yticklabels(['upper_ns', 'middle_ns', 'lower_ns',
-                   'rijnland_chalk', 'scruff', 'zechstein'], fontsize=9, ha="left")
+                          'rijnland_chalk', 'scruff', 'zechstein'],
+                          fontsize=9, ha="left")
     plt.savefig('interpretation.png')
 
 
@@ -164,24 +187,31 @@ def main(args, logger):
     ie = IECore()
 
     if args.cpu_extension and "CPU" in args.device:
-        logger.info(f'Using custom user CPU extention: {args.cpu_extension}')    
+        logger.info(f'Using custom user CPU extention: {args.cpu_extension}')
         ie.add_extension(args.cpu_extension, "CPU")
 
     logger.info("Loading network")
     net = ie.read_network(args.model, os.path.splitext(args.model)[0] + ".bin")
 
+    logger.info("Loading dataset")
+    inp = load_data(args.data_path, args.slice_index, args.slice_type)
+
+    input_net_info = net.input_info
+    input_name = next(iter(input_net_info))
+    input_net_shape = input_net_info[input_name].input_data.shape
+
+    if inp.shape != input_net_shape[-2:]:
+        logger.info(f"Reshaping model to fit for given slice shape: {inp.shape}")
+        net.reshape({input_name: [1, 1, *inp.shape]})
+
     logger.info("loading model to the plugin")
     exec_net = ie.load_network(network=net, device_name=args.device)
 
-    logger.info("Data preparation")
-    inp = load_data(args.data_path, args.slice_index, args.slice_type)
-
     logger.infer("Starting inference")
-    out = exec_net.infer(inputs={'input':inp})['output']
+    out = exec_net.infer(inputs={'input': inp})['output']
 
     logger.info("Showing results")
     show_interpretation(inp, out)
-
 
 
 if __name__ == '__main__':
