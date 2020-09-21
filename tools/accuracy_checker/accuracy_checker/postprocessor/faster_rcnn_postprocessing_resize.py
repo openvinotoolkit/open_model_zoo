7bea428fdb2f803675922527aca848bba3b0b30f
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-09-21 13:05:20 +0300

    support rescale in frcnn postprocessing

--- tools/accuracy_checker/accuracy_checker/postprocessor/faster_rcnn_postprocessing_resize.py
+++ tools/accuracy_checker/accuracy_checker/postprocessor/faster_rcnn_postprocessing_resize.py
@@ -17,7 +17,7 @@ limitations under the License.
 from ..representation import DetectionPrediction, DetectionAnnotation
 from ..postprocessor.postprocessor import Postprocessor
 from ..utils import get_size_from_config
-from ..config import NumberField
+from ..config import NumberField, BoolField
 
 
 class FRCNNPostprocessingBboxResize(Postprocessor):
@@ -36,15 +36,17 @@ class FRCNNPostprocessingBboxResize(Postprocessor):
         parameters.update({
             'dst_width': NumberField(value_type=int, optional=True, min_value=1, description="Destination width."),
             'dst_height': NumberField(value_type=int, optional=True, min_value=1, description="Destination height."),
-            'size': NumberField(value_type=int, optional=True, min_value=1, description="Destination size.")
+            'size': NumberField(value_type=int, optional=True, min_value=1, description="Destination size."),
+            'rescale': BoolField(optional=True, default=False)
         })
         return parameters
 
     def configure(self):
         self.dst_height, self.dst_width = get_size_from_config(self.config, allow_none=True)
+        self.rescale = self.get_value_from_config('rescale')
 
     @staticmethod
-    def get_coeff_x_y_from_metadata(image_metadata):
+    def get_coeff_x_y_from_metadata(image_metadata, rescale=False):
         assert image_metadata and 'geometric_operations' in image_metadata
         geometric_operations = image_metadata['geometric_operations'] # should be list of GeometricOperationMetadata
 
@@ -54,9 +56,8 @@ class FRCNNPostprocessingBboxResize(Postprocessor):
 
             coeff_x = op_props['original_width']
             coeff_y = op_props['original_height']
-            return coeff_x, coeff_y
 
-        if len(geometric_operations) == 2:
+        elif len(geometric_operations) == 2:
             op_name1, op_props1 = geometric_operations[0]
             op_name2, op_props2 = geometric_operations[1]
             assert op_name1 == "resize", (
@@ -72,11 +73,18 @@ class FRCNNPostprocessingBboxResize(Postprocessor):
 
             coeff_x = pad_pref_width / resize_scale_x
             coeff_y = pad_pref_height / resize_scale_y
-            return coeff_x, coeff_y
 
-        raise RuntimeError("Unknown case: "
-                           "len(image_metadata['geometric_operations']) = {}".format(
+        else:
+            raise RuntimeError("Unknown case: ""len(image_metadata['geometric_operations']) = {}".format(
                                len(geometric_operations)))
+        if rescale:
+            input_shape = [value for value in image_metadata['input_shape'].values() if len(value) == 4]
+            assert len(input_shape) == 1, 'suitable input shape not found or multi inputs detected'
+            input_shape = input_shape[0]
+            input_h, input_w = input_shape[2:] if input_shape[1] in [1, 3] else input_shape[1:3]
+            coeff_x /= input_w
+            coeff_y /= input_h
+        return coeff_x, coeff_y
 
     def process_image(self, annotations, predictions):
         raise RuntimeError("Since `process_image_with_metadata` is overriden, this method MUST NOT be called")
@@ -85,7 +93,7 @@ class FRCNNPostprocessingBboxResize(Postprocessor):
         assert image_metadata and 'geometric_operations' in image_metadata, (
             "Postprocessing step `faster_rcnn_postprocessing_resize` cannot work without "
             "metadata with `geometric_operations` field")
-        coeff_x, coeff_y = self.get_coeff_x_y_from_metadata(image_metadata)
+        coeff_x, coeff_y = self.get_coeff_x_y_from_metadata(image_metadata, self.rescale)
 
         for prediction in predictions:
             prediction.x_mins *= coeff_x
