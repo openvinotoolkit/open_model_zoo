c18fdcdf15ba288b18a9510d031eea65d2fbb6ea
eizamaliev <eduard.zamaliev@intel.com>
2020-09-28 18:43:33 +0300

    Add retinaface

--- demos/python_demos/object_detection_demo_async/models/__init__.py
+++ demos/python_demos/object_detection_demo_async/models/__init__.py
@@ -2,10 +2,12 @@ from .ssd import SSD
 from .yolo import YOLO
 from .faceboxes import FaceBoxes
 from .centernet import CenterNet
+from .retinaface import RetinaFace
 
 __all__ = [
     'SSD',
     'YOLO',
     'FaceBoxes',
     'CenterNet',
+    'RetinaFace'
 ]
