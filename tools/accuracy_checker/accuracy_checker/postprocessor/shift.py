3d5c779be4835288a3e356a38f1a7b0eb958e5b0
jkamelin <julia.kamelina@intel.com>
2020-11-02 17:38:36 +0300

    support efficientdet

--- tools/accuracy_checker/accuracy_checker/postprocessor/shift.py
+++ tools/accuracy_checker/accuracy_checker/postprocessor/shift.py
@@ -17,7 +17,7 @@ limitations under the License.
 import numpy as np
 from ..config import NumberField
 from .postprocessor import PostprocessorWithSpecificTargets
-from ..representation import SegmentationAnnotation, SegmentationPrediction
+from ..representation import SegmentationAnnotation, SegmentationPrediction, DetectionPrediction, DetectionAnnotation
 
 
 class Shift(PostprocessorWithSpecificTargets):
@@ -53,5 +53,36 @@ class Shift(PostprocessorWithSpecificTargets):
             mask = prediction_.mask
             update_mask = mask + self.shift_value
             prediction_.mask = update_mask.astype(np.int16)
+
+        return annotation, prediction
+
+
+class ShiftLabels(PostprocessorWithSpecificTargets):
+    """
+    Shift predicted detection labels or annotation detection labels.
+    """
+
+    __provider__ = 'shift_labels'
+
+    prediction_types = (DetectionPrediction, )
+    annotation_types = (DetectionAnnotation, )
+
+    @classmethod
+    def parameters(cls):
+        parameters = super().parameters()
+        parameters.update({
+            'offset': NumberField(value_type=int, optional=True, default=0, description="Value for shift.")
+        })
+        return parameters
+
+    def configure(self):
+        self.offset = self.get_value_from_config('offset')
+
+    def process_image(self, annotation, prediction):
+        for prediction_ in prediction:
+            prediction_.labels += self.offset
+
+        for annotation_ in annotation:
+            annotation_.labels += self.offset
 
         return annotation, prediction
