18187bfa00960954f86de5767cfdea8bc382c7fb
Vladimir Dudnik <vladimir.dudnik@intel.com>
2020-11-03 17:47:15 +0300

    Update images_capture.cpp

--- demos/common/src/images_capture.cpp
+++ demos/common/src/images_capture.cpp
@@ -123,7 +123,7 @@ public:
                 return;
             }
         }
-        catch(const std::invalid_argument&) {} // If stoi conversion failed, let's try another way to open capture device
+        catch (const std::invalid_argument&) {} // If stoi conversion failed, let's try another way to open capture device
         catch (const std::out_of_range&) {}
 
         if (cap.open(input)) {
