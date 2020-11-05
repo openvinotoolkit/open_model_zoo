aa76d7cc116a8e0a2a9aac89d492b338d3823eaf
Pavel Druzhkov <druzhkov.paul@gmail.com>
2020-11-05 13:48:27 +0300

    add initial AC support and config

--- tools/accuracy_checker/accuracy_checker/adapters/__init__.py
+++ tools/accuracy_checker/accuracy_checker/adapters/__init__.py
@@ -63,7 +63,13 @@ from .retinanet import RetinaNetAdapter, MultiOutRetinaNet
 from .yolo import TinyYOLOv1Adapter, YoloV2Adapter, YoloV3Adapter, YoloV3ONNX
 from .classification import ClassificationAdapter
 from .segmentation import SegmentationAdapter, BrainTumorSegmentationAdapter, DUCSegmentationAdapter
-from .pose_estimation import HumanPoseAdapter, SingleHumanPoseAdapter, StackedHourGlassNetworkAdapter
+from .pose_estimation import (
+    AssociativeEmbeddingAdapter,
+    HumanPoseAdapter,
+    SingleHumanPoseAdapter,
+    StackedHourGlassNetworkAdapter
+)
+
 from .pose_estimation_3d import HumanPose3dAdapter
 
 from .hit_ratio import HitRatioAdapter
@@ -159,6 +165,7 @@ __all__ = [
     'LPRAdapter',
     'CTCGreedySearchDecoder',
 
+    'AssociativeEmbeddingAdapter',
     'HumanPoseAdapter',
     'HumanPose3dAdapter',
     'SingleHumanPoseAdapter',
