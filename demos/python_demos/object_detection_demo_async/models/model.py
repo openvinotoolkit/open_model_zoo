5fed565d2a0f74c1c0b5edd5cecda59079567248
eizamaliev <eduard.zamaliev@intel.com>
2020-11-05 11:51:06 +0300

    Small fixes

--- demos/python_demos/object_detection_demo_async/models/model.py
+++ demos/python_demos/object_detection_demo_async/models/model.py
@@ -1,18 +1,16 @@
-from collections import deque
-import threading
 from time import perf_counter
 
 
 class Model:
     def __init__(self, ie, model_path, logger=None):
         self.logger = logger
-        if logger:
-            logger.info('Reading network from IR...')
+        if self.logger:
+            self.logger.info('Reading network from IR...')
         loading_time = perf_counter()
         self.net = ie.read_network(model_path, model_path[:-4] + '.bin')
         loading_time = (perf_counter() - loading_time)
-        if logger:
-            logger.info('Read in {:.3f} seconds'.format(loading_time))
+        if self.logger:
+            self.logger.info('Read in {:.3f} seconds'.format(loading_time))
 
     def unify_inputs(self, inputs) -> dict:
         raise NotImplementedError
