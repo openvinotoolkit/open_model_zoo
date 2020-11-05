98c7ca73a6bf38f0dc41793fbb537940245434ff
eizamaliev <eduard.zamaliev@intel.com>
2020-11-05 11:51:32 +0300

    Yolo model fix

--- demos/python_demos/object_detection_demo_async/models/yolo.py
+++ demos/python_demos/object_detection_demo_async/models/yolo.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import ngraph
 
 from .model import Model
 from .utils import Detection, resize_image, resize_image_letterbox
@@ -9,20 +10,20 @@ from .utils import Detection, resize_image, resize_image_letterbox
 class YOLO(Model):
     class Params:
         # Magic numbers are copied from yolo samples
-        def __init__(self, param, side):
-            self.num = 3 if 'num' not in param else int(param['num'])
-            self.coords = 4 if 'coords' not in param else int(param['coords'])
-            self.classes = 80 if 'classes' not in param else int(param['classes'])
-            self.side = side
-            self.anchors = [10.0, 13.0, 16.0, 30.0, 33.0, 23.0,
-                            30.0, 61.0, 62.0, 45.0, 59.0, 119.0,
-                            116.0, 90.0, 156.0, 198.0, 373.0, 326.0] \
-                if 'anchors' not in param else [float(a) for a in param['anchors'].split(',')]
+        def __init__(self, param, sides):
+            self.num = param.get('num', 3)
+            self.coords = param.get('coord', 4)
+            self.classes = param.get('classes', 80)
+            self.sides = sides
+            self.anchors = param.get('anchors',
+                                     [10.0, 13.0, 16.0, 30.0, 33.0, 23.0,
+                                      30.0, 61.0, 62.0, 45.0, 59.0, 119.0,
+                                      116.0, 90.0, 156.0, 198.0, 373.0, 326.0])
 
             self.isYoloV3 = False
 
-            if param.get('mask'):
-                mask = [int(idx) for idx in param['mask'].split(',')]
+            if 'mask' in param:
+                mask = [int(idx) for idx in param['mask']]
                 self.num = len(mask)
 
                 masked_anchors = []
@@ -37,8 +38,9 @@ class YOLO(Model):
 
         self.threshold = 0.5
         self.iou_threshold = 0.5
+
         self.keep_aspect_ratio = keep_aspect_ratio
-        self.resize = resize_image_letterbox if self.keep_aspect_ratio else resize_image
+        self.resize_image = resize_image_letterbox if self.keep_aspect_ratio else resize_image
 
         assert len(self.net.input_info) == 1, "Sample supports only YOLO V* based single input topologies"
         self.image_blob_name = next(iter(self.net.input_info))
@@ -50,32 +52,21 @@ class YOLO(Model):
             self.nchw_shape = False
         assert self.n == 1, 'Only batch size == 1 is supported.'
 
-        self.yolo_layer_params = {}
-        for layer_name, out_blob in self.net.outputs.items():
-            shape = out_blob.shape if len(out_blob.shape) == 4 else \
-                self.net.layers[self.net.layers[layer_name].parents[0]].out_data[0].shape
-            self.yolo_layer_params.update({layer_name: self.Params(self.net.layers[layer_name].params, shape[2])})
+        self.yolo_layer_params = self._get_output_info()
 
-
-    @staticmethod
-    def _resize_image(frame, size, keep_aspect_ratio=False):
-        if not keep_aspect_ratio:
-            resized_frame = cv2.resize(frame, size)
-        else:
-            # h, w = frame.shape[:2]
-            # scale = min(size[1] / h, size[0] / w)
-            # resized_frame = cv2.resize(frame, None, fx=scale, fy=scale)
-            iw, ih = frame.shape[0:2][::-1]
-            w, h = size
-            scale = min(w / iw, h / ih)
-            nw = int(iw * scale)
-            nh = int(ih * scale)
-            resized_frame = cv2.resize(frame, (nw, nh))
-            # resized_frame = np.full((size[1], size[0], 3), 128, dtype=np.uint8)
-            # dx = (w - nw) // 2
-            # dy = (h - nh) // 2
-            # resized_frame[dy:dy + nh, dx:dx + nw, :] = frame
-        return resized_frame
+    def _get_output_info(self):
+        def get_parent(node):
+            return node.inputs()[0].get_source_output().get_node()
+        ng_func = ngraph.function_from_cnn(self.net)
+        output_info = {}
+        for node in ng_func.get_ordered_ops():
+            layer_name = node.get_friendly_name()
+            if layer_name not in self.net.outputs:
+                continue
+            shape = list(get_parent(node).shape)
+            yolo_params = self.Params(node._get_attributes(), shape[2:4])
+            output_info[layer_name] = (shape, yolo_params)
+        return output_info
 
     def unify_inputs(self, inputs) -> dict:
         if not isinstance(inputs, dict):
@@ -89,13 +80,6 @@ class YOLO(Model):
         h, w = img.shape[:2]
         meta = {'original_shape': inputs[self.image_blob_name].shape,
                 'resized_shape': img.shape}
-        if h != self.h or w != self.w:
-            resized_frame = np.full((self.h, self.w, 3), 128, dtype=np.uint8)
-            dx = (self.w - w) // 2
-            dy = (self.h - h) // 2
-            resized_frame[dy:dy + h, dx:dx + w, :] = img
-            img = resized_frame
-            meta.update({'scales': (w / self.w, h / self.h)})
         if self.nchw_shape:
             img = img.transpose((2, 0, 1))  # Change data layout from HWC to CHW
         img = img.reshape((self.n, self.c, self.h, self.w))
@@ -103,27 +87,22 @@ class YOLO(Model):
         return inputs, meta
 
     @staticmethod
-    def _parse_yolo_region(predictions, input_size, params, threshold):
-        _, _, out_blob_h, out_blob_w = predictions.shape
-        assert out_blob_w == out_blob_h, "Invalid size of output blob. It sould be in NCHW layout and height should " \
-                                         "be equal to width. Current height = {}, current width = {}" \
-                                         "".format(out_blob_h, out_blob_w)
-
+    def _parse_yolo_region(predictions, input_size, params, threshold, multiple_labels=True):
         # ------------------------------------------ Extracting layer parameters ---------------------------------------
         objects = list()
-        size_normalizer = input_size if params.isYoloV3 else (params.side, params.side)
+        size_normalizer = input_size if params.isYoloV3 else params.sides
         bbox_size = params.coords + 1 + params.classes
         # ------------------------------------------- Parsing YOLO Region output ---------------------------------------
-        for row, col, n in np.ndindex(params.side, params.side, params.num):
-            # Getting raw values for each detection bounding box
+        for row, col, n in np.ndindex(params.sides[0], params.sides[1], params.num):
+            # Getting raw values for each detection bounding bFox
             bbox = predictions[0, n * bbox_size:(n + 1) * bbox_size, row, col]
             x, y, width, height, object_probability = bbox[:5]
             class_probabilities = bbox[5:]
             if object_probability < threshold:
                 continue
             # Process raw value
-            x = (col + x) / params.side
-            y = (row + y) / params.side
+            x = (col + x) / params.sides[1]
+            y = (row + y) / params.sides[0]
             # Value for exp is very big number in some cases so following construction is using here
             try:
                 width = np.exp(width)
@@ -134,17 +113,23 @@ class YOLO(Model):
             width = width * params.anchors[2 * n] / size_normalizer[0]
             height = height * params.anchors[2 * n + 1] / size_normalizer[1]
 
-            class_id = np.argmax(class_probabilities)
-            confidence = class_probabilities[class_id] * object_probability
-            if confidence < threshold:
-                continue
-            detection = Detection(x - width / 2, y - height / 2, x + width / 2, y + height / 2,
-                                  confidence.item(), class_id.item())
-            objects.append(detection)
+            if multiple_labels:
+                for class_id, class_probability in enumerate(class_probabilities):
+                    confidence = object_probability * class_probability
+                    if confidence > threshold:
+                        objects.append(Detection(x - width / 2, y - height / 2, x + width / 2, y + height / 2,
+                                                 confidence, class_id))
+            else:
+                class_id = np.argmax(class_probabilities)
+                confidence = class_probabilities[class_id] * object_probability
+                if confidence < threshold:
+                    continue
+                objects.append(Detection(x - width / 2, y - height / 2, x + width / 2, y + height / 2,
+                                         confidence.item(), class_id.item()))
         return objects
 
     @staticmethod
-    def _filter(self, detections, iou_threshold):
+    def _filter(detections, iou_threshold):
         def iou(box_1, box_2):
             width_of_overlap_area = min(box_1.xmax, box_2.xmax) - max(box_1.xmin, box_2.xmin)
             height_of_overlap_area = min(box_1.ymax, box_2.ymax) - max(box_1.ymin, box_2.ymin)
@@ -174,39 +159,40 @@ class YOLO(Model):
         return [det for det in detections if det.score > 0]
 
     @staticmethod
-    def _resize_detections(detections, input_shape, original_image_shape, resized_image_shape, aspect_ratio_kept=False):
-        if aspect_ratio_kept:
-            scales = (resized_image_shape[0]/input_shape[0], resized_image_shape[1]/input_shape[1])
-            detections = YOLO._resize_detections_letterbox(detections, scales)
-        scales = (original_image_shape[0] , original_image_shape[1] )
+    def _resize_detections(detections, original_shape):
         for detection in detections:
-            detection.xmin *= scales[0]
-            detection.xmax *= scales[0]
-            detection.ymin *= scales[1]
-            detection.ymax *= scales[1]
+            detection.xmin *= original_shape[0]
+            detection.xmax *= original_shape[0]
+            detection.ymin *= original_shape[1]
+            detection.ymax *= original_shape[1]
         return detections
 
     @staticmethod
-    def _resize_detections_letterbox(detections, scales):
+    def _resize_detections_letterbox(detections, original_shape, resized_shape):
+        scales = [x / y for x, y in zip(resized_shape, original_shape)]
+        scale = min(scales)
+        scales = (scale / scales[0], scale / scales[1])
         offset = [0.5 * (1 - x) for x in scales]
         for detection in detections:
-            detection.xmin = (detection.xmin - offset[0]) / scales[0]
-            detection.xmax = (detection.xmax - offset[0]) / scales[0]
-            detection.ymin = (detection.ymin - offset[1]) / scales[1]
-            detection.ymax = (detection.ymax - offset[1]) / scales[1]
+            detection.xmin = ((detection.xmin - offset[0]) / scales[0]) * original_shape[0]
+            detection.xmax = ((detection.xmax - offset[0]) / scales[0]) * original_shape[0]
+            detection.ymin = ((detection.ymin - offset[1]) / scales[1]) * original_shape[1]
+            detection.ymax = ((detection.ymax - offset[1]) / scales[1]) * original_shape[1]
         return detections
 
     def postprocess(self, outputs, meta):
         detections = list()
 
         for layer_name, out_blob in outputs.items():
-            out_blob = out_blob.reshape(self.net.layers[self.net.layers[layer_name].parents[0]].out_data[0].shape)
             layer_params = self.yolo_layer_params[layer_name]
-            detections += self._parse_yolo_region(out_blob, meta['resized_shape'], layer_params, self.threshold)
+            out_blob.shape = layer_params[0]
+            detections += self._parse_yolo_region(out_blob, meta['resized_shape'], layer_params[1], self.threshold)
 
         detections = self._filter(detections, self.iou_threshold)
-        detections = self._resize_detections(detections, (self.w, self.h), meta['original_shape'][1::-1],
-                                             meta['resized_shape'][1::-1], self.keep_aspect_ratio)
+        if self.keep_aspect_ratio:
+            detections = self._resize_detections_letterbox(detections, meta['original_shape'][1::-1],
+                                                           meta['resized_shape'][1::-1])
+        else:
+            detections = self._resize_detections(detections, meta['original_shape'][1::-1])
 
         return detections
-
