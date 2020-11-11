3b4344091f784a687ca7aa5eb776c06f2872d1aa
eizamaliev <eduard.zamaliev@intel.com>
2020-11-11 18:39:20 +0300

    fix

--- demos/python_demos/common/models/__init__.py
+++ demos/python_demos/common/models/__init__.py
@@ -3,11 +3,3 @@ from .yolo import YOLO
 from .faceboxes import FaceBoxes
 from .centernet import CenterNet
 from .retinaface import RetinaFace
-
-__all__ = [
-    'SSD',
-    'YOLO',
-    'FaceBoxes',
-    'CenterNet',
-    'RetinaFace'
-]
