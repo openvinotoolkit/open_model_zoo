901af53b29cb3d6662013783d1a7c54f9851952d
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-12-01 00:51:52 +0300

    handle reading errors (#20)

--- demos/python_demos/image_translation_demo/image_translation_demo.py
+++ demos/python_demos/image_translation_demo/image_translation_demo.py
@@ -104,11 +104,21 @@ def main():
         samples = [number_of_objects * [''], input_semantics, reference_images, reference_semantics]
     for input_img, input_sem, ref_img, ref_sem in zip(*samples):
         if use_seg:
-            input_sem = get_mask_from_image(cv2.imread(input_img), seg_model)
-            ref_sem = get_mask_from_image(cv2.imread(ref_img), seg_model)
+            in_img = cv2.imread(input_img)
+            if in_img is None:
+                raise IOError('Image {} cannot be read'.format(input_img)
+            input_sem = get_mask_from_image(in_img, seg_model)
+            r_img = cv2.imread(ref_img)
+            if r_img is None:
+                raise IOError('Image {} cannot be read'.format(ref_img)
+            ref_sem = get_mask_from_image(r_img, seg_model)
         else:
             input_sem = cv2.imread(input_sem, cv2.IMREAD_GRAYSCALE)
+            if input_sem is None:
+                raise IOError('Image {} cannot be read'.format(input_sem)
             ref_sem = cv2.imread(ref_sem, cv2.IMREAD_GRAYSCALE)
+            if ref_sem is None:
+                raise IOError('Image {} cannot be read'.format(ref_sem)
         input_sem = preprocess_semantics(input_sem, input_size=gan_model.input_semantic_size)
         ref_img = preprocess_image(cv2.imread(ref_img), input_size=gan_model.input_image_size)
         ref_sem = preprocess_semantics(ref_sem, input_size=gan_model.input_semantic_size)
