3b72c766fdbbf0ca97965f7b9c381c0bbb32f7e3
Anzhella Pankratova <anzhella.pankratova@intel.com>
2020-12-07 16:13:34 +0300

    Change main.py in segmentation demo

--- demos/python_demos/segmentation_demo/segmentation_demo.py
+++ demos/python_demos/segmentation_demo/segmentation_demo.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """
  Copyright (C) 2018-2020 Intel Corporation
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
       http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,199 +12,247 @@
  limitations under the License.
 """
 
+import logging
+import random
 import sys
-import os
 from argparse import ArgumentParser, SUPPRESS
+from pathlib import Path
+from time import perf_counter
+import os
+
 import cv2
 import numpy as np
-import logging as log
 from openvino.inference_engine import IECore
 
-classes_color_map = [
-    (150, 150, 150),
-    (58, 55, 169),
-    (211, 51, 17),
-    (157, 80, 44),
-    (23, 95, 189),
-    (210, 133, 34),
-    (76, 226, 202),
-    (101, 138, 127),
-    (223, 91, 182),
-    (80, 128, 113),
-    (235, 155, 55),
-    (44, 151, 243),
-    (159, 80, 170),
-    (239, 208, 44),
-    (128, 50, 51),
-    (82, 141, 193),
-    (9, 107, 10),
-    (223, 90, 142),
-    (50, 248, 83),
-    (178, 101, 130),
-    (71, 30, 204)
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'common'))
+
+from models import *
+import monitors
+from pipelines import AsyncPipeline
+from performance_metrics import PerformanceMetrics
+
+logging.basicConfig(format='[ %(levelname)s ] %(message)s', level=logging.INFO, stream=sys.stdout)
+log = logging.getLogger()
+
+
+CITYSCAPES_COLORS = [
+    (128, 64, 128),
+    (232, 35, 244),
+    (70, 70, 70),
+    (156, 102, 102),
+    (153, 153, 190),
+    (153, 153, 153),
+    (30, 170, 250),
+    (0, 220, 220),
+    (35, 142, 107),
+    (152, 251, 152),
+    (180, 130, 70),
+    (60, 20, 220),
+    (0, 0, 255),
+    (142, 0, 0),
+    (70, 0, 0),
+    (100, 60, 0),
+    (90, 0, 0),
+    (230, 0, 0),
+    (32, 11, 119),
+    (0, 74, 111),
+    (81, 0, 81)
 ]
 
-np.random.seed(10)
+
+def apply_color_map(input):
+    ### Initializing colors array if needed
+    colors = []
+    rng = random.Random(0xACE)
+    if not colors:
+        colors = np.zeros(size=(256, 1), dtype=np.uint8)
+        for i in range(len(CITYSCAPES_COLORS)):
+            colors[i, 0] = (CITYSCAPES_COLORS[i][0], CITYSCAPES_COLORS[i][1], CITYSCAPES_COLORS[i][2])
+        for i in range(len(colors)):
+            colors[i, 0] = (rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255))
+
+    # Converting class to color
+    out = cv2.applyColorMap(input, colors)
+    return out
+
+
+def render_segmentation_data(frame, objects):
+    # Visualizing result data over source image
+    return frame / 2 + apply_color_map(objects) / 2
 
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.",
-                      required=True, type=str)
-    args.add_argument("-i", "--input", help="Required. Path to a folder with images or path to an image files.",
-                      required=True, type=str, nargs="+")
-    args.add_argument("-lab", "--labels", help="Optional. Path to a text file containing class labels.",
-                      type=str)
-    args.add_argument("-c", "--colors", help="Optional. Path to a text file containing colors for classes.",
-                      type=str)
-    args.add_argument("-lw", "--legend_width", help="Optional. Width of legend.", default=300, type=int)
-    args.add_argument("-o", "--output_dir", help="Optional. Path to a folder where output files will be saved.",
-                      default="results", type=str)
-    args.add_argument("-l", "--cpu_extension",
-                      help="Optional. Required for CPU custom layers. "
-                           "Absolute MKLDNN (CPU)-targeted custom layers. Absolute path to a shared library with the "
-                           "kernels implementations.", type=str, default=None)
-    args.add_argument("-d", "--device",
-                      help="Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is "
-                           "acceptable. Sample will look for a suitable plugin for device specified. Default value is CPU.",
-                      default="CPU", type=str)
+    args.add_argument('-m', '--model', help='Required. Path to an .xml file with a trained model.',
+                      required=True, type=Path)
+    args.add_argument('-i', '--input', required=True, type=str,
+                      help='Required. Path to an image, folder with images, video file or a numeric camera ID.')
+    args.add_argument('-d', '--device', default='CPU', type=str,
+                      help='Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is '
+                           'acceptable. The sample will look for a suitable plugin for device specified. '
+                           'Default value is CPU.')
+
+    common_model_args = parser.add_argument_group('Common model options')
+    common_model_args.add_argument('--labels', help='Optional. Labels mapping file.', default=None, type=str)
+    common_model_args.add_argument('-t', '--prob_threshold', default=0.5, type=float,
+                                   help='Optional. Probability threshold for detections filtering.')
+    common_model_args.add_argument('--keep_aspect_ratio', action='store_true', default=False,
+                                   help='Optional. Keeps aspect ratio on resize.')
+
+    infer_args = parser.add_argument_group('Inference options')
+    infer_args.add_argument('-nireq', '--num_infer_requests', help='Optional. Number of infer requests',
+                            default=1, type=int)
+    infer_args.add_argument('-nstreams', '--num_streams',
+                            help='Optional. Number of streams to use for inference on the CPU or/and GPU in throughput '
+                                 'mode (for HETERO and MULTI device cases use format '
+                                 '<device1>:<nstreams1>,<device2>:<nstreams2> or just <nstreams>).',
+                            default='', type=str)
+    infer_args.add_argument('-nthreads', '--num_threads', default=None, type=int,
+                            help='Optional. Number of threads to use for inference on CPU (including HETERO cases).')
+
+    io_args = parser.add_argument_group('Input/output options')
+    io_args.add_argument('-loop', '--loop', help='Optional. Loops input data.', action='store_true', default=False)
+    io_args.add_argument('-no_show', '--no_show', help="Optional. Don't show output.", action='store_true')
+    io_args.add_argument('-u', '--utilization_monitors', default='', type=str,
+                         help='Optional. List of monitors to show initially.')
+
+    debug_args = parser.add_argument_group('Debug options')
+    debug_args.add_argument('-r', '--raw_output_message', help='Optional. Output inference results raw values showing.',
+                            default=False, action='store_true')
     return parser
 
+def get_plugin_configs(device, num_streams, num_threads):
+    config_user_specified = {}
 
-def get_files(input_paths):
-    inputs = []
-    for path in input_paths:
-        if not os.path.exists(path):
-            raise AttributeError("Path to input data: '{}' does not exist".format(path))
-        if os.path.isdir(path):
-            file_paths = [os.path.join(path, file) for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-            inputs.extend(file_paths)
-        else:
-            inputs.append(path)
-    return inputs
+    devices_nstreams = {}
+    if num_streams:
+        devices_nstreams = {device: num_streams for device in ['CPU', 'GPU'] if device in device} \
+            if num_streams.isdigit() \
+            else dict(device.split(':', 1) for device in num_streams.split(','))
 
+    if 'CPU' in device:
+        if num_threads is not None:
+            config_user_specified['CPU_THREADS_NUM'] = str(num_threads)
+        if 'CPU' in devices_nstreams:
+            config_user_specified['CPU_THROUGHPUT_STREAMS'] = devices_nstreams['CPU'] \
+                if int(devices_nstreams['CPU']) > 0 \
+                else 'CPU_THROUGHPUT_AUTO'
 
-def get_info_from_file(file):
-    if file is None:
-        return None
-    info = []
-    with open(file, 'r') as file:
-        for line in file.readlines():
-            info.append(line.strip())
-    return info
+    if 'GPU' in device:
+        if 'GPU' in devices_nstreams:
+            config_user_specified['GPU_THROUGHPUT_STREAMS'] = devices_nstreams['GPU'] \
+                if int(devices_nstreams['GPU']) > 0 \
+                else 'GPU_THROUGHPUT_AUTO'
 
-
-def get_legend(size, classes, colors):
-    height, width = size
-    legend = np.full((height, width, 3), 255, dtype="uint8")
-    colors = np.unique(colors)
-    class_height = height // len(colors)
-
-    for i in range(len(colors)):
-        _, font_base = cv2.getTextSize(classes[colors[i]].split(',')[0], cv2.FONT_HERSHEY_SIMPLEX, 1, 1)[0]
-        cv2.putText(legend, classes[colors[i]].split(',')[0], (110, int((i + 0.625) * class_height)),
-                    cv2.FONT_HERSHEY_SIMPLEX, min(1, 0.5*class_height/font_base), (0, 0, 0), 1)
-        color = classes_color_map[colors[i]]
-        cv2.rectangle(legend, (5, int((i + 0.25) * class_height)), (100, int((i + 0.75) * class_height)),
-                      (int(color[0]), int(color[1]), int(color[2])), -1)
-
-    return legend
+    return config_user_specified
 
 
 def main():
-    log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+    metrics = PerformanceMetrics()
     args = build_argparser().parse_args()
 
-    log.info("Creating Inference Engine")
+    log.info('Initializing Inference Engine...')
     ie = IECore()
-    if args.cpu_extension and 'CPU' in args.device:
-        ie.add_extension(args.cpu_extension, "CPU")
-    # Read IR
-    log.info("Loading network")
-    net = ie.read_network(args.model, os.path.splitext(args.model)[0] + ".bin")
 
-    assert len(net.input_info) == 1, "Sample supports only single input topologies"
-    assert len(net.outputs) == 1, "Sample supports only single output topologies"
+    plugin_config = get_plugin_configs(args.device, args.num_streams, args.num_threads)
 
-    log.info("Preparing input blobs")
-    input_blob = next(iter(net.input_info))
-    out_blob = next(iter(net.outputs))
-    inputs = get_files(args.input)
-    net.batch_size = len(inputs)
+    log.info('Loading network...')
 
-    # NB: This is required to load the image as uint8 np.array
-    #     Without this step the input blob is loaded in FP32 precision,
-    #     this requires additional operation and more memory.
-    net.input_info[input_blob].precision = "U8"
+    model = SegmentationModel(ie, args.model)
 
-    # Read and pre-process input images
-    n, c, h, w = net.input_info[input_blob].input_data.shape
-    images = np.ndarray(shape=(n, c, h, w))
-    for i in range(n):
-        image = cv2.imread(inputs[i])
-        assert image.dtype == np.uint8
-        if image.shape[:-1] != (h, w):
-            log.warning("Image {} is resized from {} to {}".format(inputs[i], image.shape[:-1], (h, w)))
-            image = cv2.resize(image, (w, h))
-        image = image.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-        images[i] = image
-    log.info("Batch size is {}".format(n))
+    pipeline = AsyncPipeline(ie, model, plugin_config, device=args.device, max_num_requests=args.num_infer_requests)
 
-    # Loading model to the plugin
-    log.info("Loading model to the plugin")
-    exec_net = ie.load_network(network=net, device_name=args.device)
+    try:
+        input_stream = int(args.input)
+    except ValueError:
+        input_stream = args.input
+    cap = cv2.VideoCapture(input_stream)
+    if not cap.isOpened():
+        log.error('OpenCV: Failed to open capture: ' + str(input_stream))
+        sys.exit(1)
 
-    # Start sync inference
-    log.info("Starting inference")
-    res = exec_net.infer(inputs={input_blob: images})
+    next_frame_id = 0
+    next_frame_id_to_show = 0
 
-    # Processing output blob
-    log.info("Processing output blob")
-    res = res[out_blob]
-    if len(res.shape) == 3:
-        res = np.expand_dims(res, axis=1)
-    if len(res.shape) == 4:
-        _, _, out_h, out_w = res.shape
-    else:
-        raise Exception("Unexpected output blob shape {}. Only 4D and 3D output blobs are supported".format(res.shape))
+    log.info('Starting inference...')
+    print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
 
-    classes = get_info_from_file(args.labels)
-    own_color_map = get_info_from_file(args.colors)
-    global classes_color_map
-    if own_color_map:
-        classes_color_map = [eval(color) for color in own_color_map]
+    presenter = monitors.Presenter(args.utilization_monitors, 55,
+                                   (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH) / 4),
+                                    round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) / 8)))
 
-    # Create folder to save results
-    os.makedirs(args.output_dir, exist_ok=True)
+    while cap.isOpened():
+        if pipeline.callback_exceptions:
+            raise pipeline.callback_exceptions[0]
+        # Process all completed requests
+        results = pipeline.get_result(next_frame_id_to_show)
+        if results:
+            objects, frame_meta = results
+            frame = frame_meta['frame']
+            start_time = frame_meta['start_time']
 
-    for batch, data in enumerate(res):
-        if classes:
-            classes_map = np.zeros(shape=(out_h, out_w + args.legend_width, 3), dtype=np.int)
-        else:
-            classes_map = np.zeros(shape=(out_h, out_w, 3), dtype=np.int)
-        colors = []
-        for i in range(out_h):
-            for j in range(out_w):
-                if len(data[:, i, j]) == 1:
-                    pixel_class = int(data[:, i, j])
+            presenter.drawGraphs(frame)
+            #frame = render_segmentation_data(frame, objects)
+            metrics.update(start_time, frame)
+            if not args.no_show:
+                cv2.imshow('Detection Results', frame)
+                key = cv2.waitKey(1)
+
+                ESC_KEY = 27
+                # Quit.
+                if key in {ord('q'), ord('Q'), ESC_KEY}:
+                    break
+                presenter.handleKey(key)
+            next_frame_id_to_show += 1
+            continue
+
+        if pipeline.is_ready():
+            # Get new image/frame
+            start_time = perf_counter()
+            ret, frame = cap.read()
+            if not ret:
+                if args.loop:
+                    cap.open(input_stream)
                 else:
-                    pixel_class = np.argmax(data[:, i, j])
-                while pixel_class >= len(classes_color_map):
-                    new_color = np.random.randint(0, 255, size=3)
-                    classes_color_map.append(new_color)
-                colors.append(pixel_class)
-                classes_map[i, j, :] = classes_color_map[pixel_class]
-        if classes:
-            legend = get_legend((out_h, args.legend_width), classes, colors)
-            classes_map[:, -args.legend_width:, :] = legend
+                    cap.release()
+                continue
 
-        out_img = os.path.join(args.output_dir, "out_{}.bmp".format(batch))
-        cv2.imwrite(out_img, classes_map)
-        log.info("Result image was saved to {}".format(out_img))
-    log.info("This demo is an API example, for any performance measurements please use the dedicated benchmark_app tool "
-             "from the openVINO toolkit\n")
+            # Submit for inference
+            pipeline.submit_data(frame, next_frame_id, {'frame': frame, 'start_time': start_time})
+            next_frame_id += 1
+        else:
+            # Wait for empty request
+            pipeline.await_any()
+
+    pipeline.await_all()
+    # Process completed requests
+    while pipeline.has_completed_request():
+        results = pipeline.get_result(next_frame_id_to_show)
+        #print(results)
+        if results:
+            objects, frame_meta = results
+            frame = frame_meta['frame']
+            start_time = frame_meta['start_time']
+            presenter.drawGraphs(frame)
+            #frame = render_segmentation_data(frame, objects)
+            metrics.update(start_time, frame)
+            if not args.no_show:
+                cv2.imshow('Segmentation Results', frame)
+                key = cv2.waitKey(1)
+
+                ESC_KEY = 27
+                # Quit.
+                if key in {ord('q'), ord('Q'), ESC_KEY}:
+                    break
+                presenter.handleKey(key)
+            next_frame_id_to_show += 1
+        else:
+            break
+
+    metrics.print_total()
+    print(presenter.reportMeans())
 
 
 if __name__ == '__main__':
