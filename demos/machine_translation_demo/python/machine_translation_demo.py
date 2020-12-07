e978c7beac8edee08162ed5131ebdce2d82ac076
Roman Donchenko <roman.donchenko@intel.com>
2020-12-07 20:14:56 +0300

    machine_translation_demo: fix the default output name
    It should correspond to the output names of our machine translation models.

--- demos/machine_translation_demo/python/machine_translation_demo.py
+++ demos/machine_translation_demo/python/machine_translation_demo.py
@@ -192,7 +192,7 @@ def build_argparser():
                         help='Required. Path to the folder with tgt tokenizer that contains vocab.json and merges.txt.')
     parser.add_argument('-i', '--input', type=str, required=False, nargs='*',
                         help='Optional. Text for translation. Replaces console input.')
-    parser.add_argument('--output-name', type=str, default='pred/Squeeze',
+    parser.add_argument('--output-name', type=str, default='pred',
                         help='Optional. Name of the models output node.')
     return parser
 
