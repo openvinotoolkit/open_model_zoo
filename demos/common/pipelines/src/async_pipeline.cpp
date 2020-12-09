03f258401ababef078071987eadf3dcf05faad14
Vladimir <10669582+Wovchena@users.noreply.github.com>
2020-12-09 08:57:41 +0300

    demos/async_pipeline: shift indentation one level to the left

--- demos/common/pipelines/src/async_pipeline.cpp
+++ demos/common/pipelines/src/async_pipeline.cpp
@@ -65,13 +65,12 @@ AsyncPipeline::AsyncPipeline(std::unique_ptr<ModelBase>&& modelInstance, const C
     unsigned int nireq = cnnConfig.maxAsyncRequests;
     if (nireq == 0) {
         try {
-                // +1 to use it as a buffer of the pipeline
-                nireq = execNetwork.GetMetric(METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)).as<unsigned int>() + 1;
-            } catch (const details::InferenceEngineException& ex) {
-                throw std::runtime_error(std::string("Every device used with the demo should support "
-                    "OPTIMAL_NUMBER_OF_INFER_REQUESTS ExecutableNetwork metric. Failed to query the metric with error: ") + ex.what());
-            }
-
+            // +1 to use it as a buffer of the pipeline
+            nireq = execNetwork.GetMetric(METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)).as<unsigned int>() + 1;
+        } catch (const details::InferenceEngineException& ex) {
+            throw std::runtime_error(std::string("Every device used with the demo should support "
+                "OPTIMAL_NUMBER_OF_INFER_REQUESTS ExecutableNetwork metric. Failed to query the metric with error: ") + ex.what());
+        }
     }
     requestsPool.reset(new RequestsPool(execNetwork, nireq));
 
