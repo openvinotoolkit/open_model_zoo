bd99eade1f902c6c636efe0ede4d4092788b7314
aDanPin <danil.pinaev@intel.com>
2020-12-10 14:02:21 +0300

    Review response

--- demos/interactive_face_detection_demo_gapi/interactive_face_detection_gapi.hpp
+++ demos/interactive_face_detection_demo_gapi/interactive_face_detection_gapi.hpp
@@ -6,36 +6,35 @@
 
 #include <gflags/gflags.h>
 
-static const char help_message[] = "Print a usage message";
-static const char input_video_message[] = "Required. Path to a video file (specify \"cam\" to work with camera).";
-static const char output_video_message[] = "Optional. Path to an output video file.";
-static const char face_detection_model_message[] = "Required. Path to an .xml file with a trained Face Detection model.";
-static const char age_gender_model_message[] = "Optional. Path to an .xml file with a trained Age/Gender Recognition model.";
-static const char head_pose_model_message[] = "Optional. Path to an .xml file with a trained Head Pose Estimation model.";
-static const char emotions_model_message[] = "Optional. Path to an .xml file with a trained Emotions Recognition model.";
+static const char help_message[] =                   "Print a usage message";
+static const char input_video_message[] =            "Required. Path to a video file (specify \"cam\" to work with camera).";
+static const char output_video_message[] =           "Optional. Path to an output video file.";
+static const char face_detection_model_message[] =   "Required. Path to an .xml file with a trained Face Detection model.";
+static const char age_gender_model_message[] =       "Optional. Path to an .xml file with a trained Age/Gender Recognition model.";
+static const char head_pose_model_message[] =        "Optional. Path to an .xml file with a trained Head Pose Estimation model.";
+static const char emotions_model_message[] =         "Optional. Path to an .xml file with a trained Emotions Recognition model.";
 static const char facial_landmarks_model_message[] = "Optional. Path to an .xml file with a trained Facial Landmarks Estimation model.";
-static const char target_device_message[] = "Optional. Target device for Face Detection network (the list of available devices is shown below). " \
-"Default value is CPU. The demo will look for a suitable plugin for a specified device.";
-static const char target_device_message_ag[] = "Optional. Target device for Age/Gender Recognition network (the list of available devices is shown below). " \
-"Default value is CPU. The demo will look for a suitable plugin for a specified device.";
-static const char target_device_message_hp[] = "Optional. Target device for Head Pose Estimation network (the list of available devices is shown below). " \
-"Default value is CPU. The demo will look for a suitable plugin for a specified device.";
-static const char target_device_message_em[] = "Optional. Target device for Emotions Recognition network (the list of available devices is shown below). " \
-"Default value is CPU. The demo will look for a suitable plugin for a specified device.";
-static const char target_device_message_lm[] = "Optional. Target device for Facial Landmarks Estimation network " \
-"(the list of available devices is shown below). Default value is CPU. The demo will look for a suitable plugin for device specified.";
-static const char thresh_output_message[] = "Optional. Probability threshold for detections";
+static const char target_device_message[] =          "Optional. Target device for Face Detection network (the list of available devices is shown below). " \
+                                                     "Default value is CPU. The demo will look for a suitable plugin for a specified device.";
+static const char target_device_message_ag[] =       "Optional. Target device for Age/Gender Recognition network (the list of available devices is shown below). " \
+                                                     "Default value is CPU. The demo will look for a suitable plugin for a specified device.";
+static const char target_device_message_hp[] =       "Optional. Target device for Head Pose Estimation network (the list of available devices is shown below). " \
+                                                     "Default value is CPU. The demo will look for a suitable plugin for a specified device.";
+static const char target_device_message_em[] =       "Optional. Target device for Emotions Recognition network (the list of available devices is shown below). " \
+                                                     "Default value is CPU. The demo will look for a suitable plugin for a specified device.";
+static const char target_device_message_lm[] =       "Optional. Target device for Facial Landmarks Estimation network " \
+                                                     "(the list of available devices is shown below). Default value is CPU. The demo will look for a suitable plugin for device specified.";
+static const char thresh_output_message[] =          "Optional. Probability threshold for detections";
 static const char bb_enlarge_coef_output_message[] = "Optional. Coefficient to enlarge/reduce the size of the bounding box around the detected face";
-static const char raw_output_message[] = "Optional. Output inference results as raw values";
-static const char no_wait_for_keypress_message[] = "Optional. Do not wait for key press in the end.";
-static const char no_show_processed_video[] = "Optional. Do not show processed video.";
-static const char dx_coef_output_message[] = "Optional. Coefficient to shift the bounding box around the detected face along the Ox axis";
-static const char dy_coef_output_message[] = "Optional. Coefficient to shift the bounding box around the detected face along the Oy axis";
+static const char raw_output_message[] =             "Optional. Output inference results as raw values";
+static const char no_show_processed_video[] =        "Optional. Do not show processed video.";
+static const char dx_coef_output_message[] =         "Optional. Coefficient to shift the bounding box around the detected face along the Ox axis";
+static const char dy_coef_output_message[] =         "Optional. Coefficient to shift the bounding box around the detected face along the Oy axis";
 // TODO: Make this option valid for single image case
-static const char loop_video_output_message[] = "Optional. Enable playing video on a loop";
-static const char no_smooth_output_message[] = "Optional. Do not smooth person attributes";
-static const char no_show_emotion_bar_message[] = "Optional. Do not show emotion bar";
-static const char utilization_monitors_message[] = "Optional. List of monitors to show initially.";
+static const char loop_video_output_message[] =      "Optional. Enable playing video on a loop";
+static const char no_smooth_output_message[] =       "Optional. Do not smooth person attributes";
+static const char no_show_emotion_bar_message[] =    "Optional. Do not show emotion bar";
+static const char utilization_monitors_message[] =   "Optional. List of monitors to show initially.";
 
 // TODO: Support options:
 // Set number of maximum simultaneously processed faces for Age/Gender,
@@ -67,7 +66,6 @@ DEFINE_string(d_lm, "CPU", target_device_message_lm);
 DEFINE_bool(r, false, raw_output_message);
 DEFINE_double(t, 0.5, thresh_output_message);
 DEFINE_double(bb_enlarge_coef, 1.2, bb_enlarge_coef_output_message);
-DEFINE_bool(no_wait, false, no_wait_for_keypress_message);
 DEFINE_bool(no_show, false, no_show_processed_video);
 DEFINE_double(dx_coef, 1, dx_coef_output_message);
 DEFINE_double(dy_coef, 1, dy_coef_output_message);
@@ -98,7 +96,6 @@ static void showUsage() {
     std::cout << "    -d_hp \"<device>\"           " << target_device_message_hp << std::endl;
     std::cout << "    -d_em \"<device>\"           " << target_device_message_em << std::endl;
     std::cout << "    -d_lm \"<device>\"           " << target_device_message_lm << std::endl;
-    std::cout << "    -no_wait                   " << no_wait_for_keypress_message << std::endl;
     std::cout << "    -no_show                   " << no_show_processed_video << std::endl;
     std::cout << "    -r                         " << raw_output_message << std::endl;
     std::cout << "    -t                         " << thresh_output_message << std::endl;
