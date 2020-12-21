c559fedb18ac2f5341e9e3da24c5ed1b6d7a403d
Ekaterina Aidova <ekaterina.aidova@intel.com>
2020-12-21 14:27:05 +0300

    fix top k parameter in retinaface adapter

--- tools/accuracy_checker/accuracy_checker/adapters/retinaface.py
+++ tools/accuracy_checker/accuracy_checker/adapters/retinaface.py
@@ -46,7 +46,8 @@ class RetinaFaceAdapter(Adapter):
                     optional=True, default=False, description="Allows include boundaries for NMS"
                 ),
                 'keep_top_k': NumberField(
-                    min_value=0, optional=True, description="Maximal number of boxes which should be kept"
+                    min_value=1, optional=True, description="Maximal number of boxes which should be kept",
+                    value_type=int
                 ),
                 'nms_threshold': NumberField(
                     min_value=0, optional=True, default=0.5, description="Overlap threshold for NMS"
@@ -61,7 +62,7 @@ class RetinaFaceAdapter(Adapter):
         self.landmarks_output = self.get_value_from_config('landmarks_outputs') or []
         self.type_scores_output = self.get_value_from_config('type_scores_outputs') or []
         self.include_boundaries = self.get_value_from_config('include_boundaries')
-        self.keep_top_k = int(self.get_value_from_config('keep_top_k'))
+        self.keep_top_k = self.get_value_from_config('keep_top_k')
         self.nms_threshold = self.get_value_from_config('nms_threshold')
         _ratio = (1.,)
         self.anchor_cfg = {
@@ -99,10 +100,10 @@ class RetinaFaceAdapter(Adapter):
                     anchors = self._anchor_plane_cache[(height, width)][s]
                 else:
                     anchors = self.anchors_plane(height, width, int(s), anchors_fpn)
+                    anchors = anchors.reshape((height * width * anchor_num, 4))
                     if (height, width) not in self._anchor_plane_cache:
                         self._anchor_plane_cache[(height, width)] = {}
                     self._anchor_plane_cache[(height, width)][s] = anchors
-                anchors = anchors.reshape((height * width * anchor_num, 4))
                 proposals = self._get_proposals(bbox_deltas, anchor_num, anchors)
                 x_mins, y_mins, x_maxs, y_maxs = proposals.T
                 keep = NMS.nms(x_mins, y_mins, x_maxs, y_maxs, scores, self.nms_threshold,
