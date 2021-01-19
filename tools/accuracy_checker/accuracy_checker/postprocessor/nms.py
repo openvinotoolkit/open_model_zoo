f2988c98b8f07f893226e237ffbce47bd2612c76
playboy007gy <50794318+playboy007gy@users.noreply.github.com>
2021-01-20 04:44:12 +0800

    Update nms.py

--- tools/accuracy_checker/accuracy_checker/postprocessor/nms.py
+++ tools/accuracy_checker/accuracy_checker/postprocessor/nms.py
@@ -131,7 +131,6 @@ class NMS(Postprocessor):
 
         return keep
 
-
 class SoftNMS(Postprocessor):
     __provider__ = 'soft_nms'
 
@@ -231,94 +230,3 @@ class SoftNMS(Postprocessor):
             scores *= np.exp(np.negative(np.square(iou_values) / self.sigma))
 
         return np.array(out_ids, dtype=np.int32), np.array(out_scores, dtype=np.float32)
-
-#add DIOU-NMS support
-class DIOU_NMS(Postprocessor):
-    __provider__ = 'diou_nms'
-
-    prediction_types = (DetectionPrediction, ActionDetectionPrediction)
-    annotation_types = (DetectionAnnotation, ActionDetectionPrediction)
-
-    @classmethod
-    def parameters(cls):
-        parameters = super().parameters()
-        parameters.update({
-            'overlap': NumberField(
-                min_value=0, max_value=1, optional=True, default=0.5,
-                description="Overlap threshold for merging detections."
-            ),
-            'include_boundaries': BoolField(
-                optional=True, default=True, description="Shows if boundaries are included."
-            ),
-            'keep_top_k': NumberField(min_value=0, optional=True, description="Keep top K."),
-            'use_min_area': BoolField(
-                optional=True, default=False,
-                description="Use minimum area of two bounding boxes as base area to calculate overlap"
-            )
-        })
-        return parameters
-
-    def configure(self):
-        self.overlap = self.get_value_from_config('overlap')
-        self.include_boundaries = self.get_value_from_config('include_boundaries')
-        self.keep_top_k = self.get_value_from_config('keep_top_k')
-        self.use_min_area = self.get_value_from_config('use_min_area')
-
-    def process_image(self, annotations, predictions):
-        for prediction in predictions:
-            scores = get_scores(prediction)
-            keep = self.diou_nms(
-                prediction.x_mins, prediction.y_mins, prediction.x_maxs, prediction.y_maxs, scores,
-                self.overlap, self.include_boundaries, self.keep_top_k, self.use_min_area
-            )
-            prediction.remove([box for box in range(len(prediction.x_mins)) if box not in keep])
-
-        return annotations, predictions
-
-    @staticmethod
-    def diou_nms(x1, y1, x2, y2, scores, thresh, include_boundaries=True, keep_top_k=None, use_min_area=False):
-        """
-        Pure Python NMS baseline.
-        """
-        b = 1 if include_boundaries else 0
-
-        areas = (x2 - x1 + b) * (y2 - y1 + b)
-        order = scores.argsort()[::-1]
-
-        if keep_top_k:
-            order = order[:keep_top_k]
-
-        keep = []
-
-        while order.size > 0:
-            i = order[0]
-            keep.append(i)
-
-            xx1 = np.maximum(x1[i], x1[order[1:]])
-            yy1 = np.maximum(y1[i], y1[order[1:]])
-            xx2 = np.minimum(x2[i], x2[order[1:]])
-            yy2 = np.minimum(y2[i], y2[order[1:]])
-
-            w = np.maximum(0.0, xx2 - xx1 + b)
-            h = np.maximum(0.0, yy2 - yy1 + b)
-            intersection = w * h
-
-            cw = np.maximum(x2[i], x2[order[1:]]) - np.minimum(x1[i], x1[order[1:]])
-            ch = np.maximum(y2[i], y2[order[1:]]) - np.minimum(y1[i], y1[order[1:]])
-            c_area = cw**2+ch**2+1e-16
-            rh02 = ((x2[order[1:]]+x1[order[1:]])-(x2[i]+x1[i]))**2/4+((y2[order[1:]]+y1[order[1:]])-(y2[i]+y1[i]))**2/4
-
-            if use_min_area:
-                base_area = np.minimum(areas[i], areas[order[1:]])
-            else:
-                base_area = (areas[i] + areas[order[1:]] - intersection)
-
-            overlap = np.divide(
-                intersection,
-                base_area,
-                out=np.zeros_like(intersection, dtype=float),
-                where=base_area != 0
-            ) - pow(rh02/c_area,0.6)
-            order = order[np.where(overlap <= thresh)[0] + 1] # pylint: disable=W0143
-
-        return keep
