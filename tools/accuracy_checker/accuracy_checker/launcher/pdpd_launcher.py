e3a3ff0aa38ee498c52e988c5e660afc935d0241
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-02-25 19:13:55 +0300

    AC: support paddle paddle launcher

---
+++ tools/accuracy_checker/accuracy_checker/launcher/pdpd_launcher.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+import numpy as np
+from paddle.fluid.core import PaddleTensor, AnalysisConfig, create_paddle_predictor
+from .launcher import Launcher
+from ..config import PathField, StringField, ConfigError
+from ..logging import print_info
+
+DEVICE_REGEX = r'(?P<device>cpu$|gpu)'
+
+
+class PaddlePaddleLauncher(Launcher):
+    __provider__ = 'paddle_paddle'
+
+    def __init__(self, config_entry: dict, *args, **kwargs):
+        super().__init__(config_entry, *args, **kwargs)
+        self._delayed_model_loading = kwargs.get('delayed_model_loading', False)
+
+        self.validate_config(config_entry, delayed_model_loading=self._delayed_model_loading)
+        if not self._delayed_model_loading:
+            self.load_model()
+
+    @classmethod
+    def parameters(cls):
+        parameters = super().parameters()
+        parameters.update({
+            'model': PathField(description="Path to model.", file_or_directory=True),
+            'params': PathField(description='Path to model params', optional=True, file_or_directory=True),
+            'device': StringField(regex=DEVICE_REGEX, description="Device name.", optional=True, default='CPU')
+        })
+
+        return parameters
+
+    def load_model(self):
+        model, params = self.automatic_model_search()
+        config = AnalysisConfig(str(model), str(params))
+        config.disable_glog_info()
+        device = self.get_value_from_config('device')
+        if device.upper() == 'CPU':
+            config.disable_gpu()
+        self._predictor = create_paddle_predictor(config)
+
+    @property
+    def inputs(self):
+        return self._predictor.get_input_tensor_shape()
+
+    @property
+    def output_blob(self):
+        return next(iter(self._predictor.get_output_names()))
+
+    @property
+    def batch(self):
+        return 1
+
+    def automatic_model_search(self):
+        model = Path(self.get_value_from_config('model'))
+        if model.is_dir():
+            model_list = list(model.glob('model'))
+            if not model_list:
+                raise ConfigError('Model not found')
+            model = model_list[0]
+            print_info('Found model: {}'.format(model))
+        params = self.get_value_from_config('params')
+        if params is None or Path(params).is_dir():
+            params_dir = model.parent if params is None else Path(params)
+            params_list = list(params_dir.glob('params'))
+            if not params_list:
+                raise ConfigError('Params not found')
+            params = params_list[0]
+            print_info('Found params: {}'.format(params))
+        params = Path(params)
+
+        return model, params
+
+    def predict(self, inputs, metadata=None, **kwargs):
+        results = []
+        for infer_input in inputs:
+            input_list = [infer_input[name] for name in self._predictor.get_input_names()]
+            prediction_list = self._predictor.run(input_list)
+            res_outs = {}
+            for name, prediction in zip(self._predictor.get_output_names(), prediction_list):
+                res_outs[name] = prediction.as_ndarray()
+            results.append(res_outs)
+            if metadata is not None:
+                for meta_ in metadata:
+                    meta_['input_shape'] = self.inputs_info_for_meta()
+
+        return results
+
+    def fit_to_input(self, data, layer_name, layout, precision):
+        layer_shape = self.inputs[layer_name]
+        input_precision = np.float32 if not precision else precision
+        if len(np.shape(data)) == 4:
+            data = np.transpose(data, layout).astype(input_precision)
+            if len(layer_shape) == 3:
+                if np.shape(data)[0] != 1:
+                    raise ValueError('Only for batch size 1 first dimension can be omitted')
+                return PaddleTensor(data[0].astype(input_precision))
+            return PaddleTensor(data.astype(input_precision))
+        if len(np.shape(data)) == 5 and len(layout) == 5:
+            return PaddleTensor(np.transpose(data, layout).astype(input_precision))
+        if len(np.shape(data))-1 == len(layer_shape):
+            return PaddleTensor(np.array(data[0]).astype(input_precision))
+        return PaddleTensor(np.array(data).astype(input_precision))
+
+    def predict_async(self, *args, **kwargs):
+        raise ValueError('ONNX Runtime Launcher does not support async mode yet')
+
+    def release(self):
+        if hasattr(self, '_predictor'):
+            del self._predictor
