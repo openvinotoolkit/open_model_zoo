99b5659e93642dd68108d711c4bb605f7f4710c9
Vladimir Dudnik <vladimir.dudnik@intel.com>
2021-02-25 19:26:42 +0300

    fix yet another pre-commit checks

--- demos/face_detection_mtcnn_demo/python/face_detection_mtcnn_demo.py
+++ demos/face_detection_mtcnn_demo/python/face_detection_mtcnn_demo.py
@@ -190,4 +190,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
-
--- demos/face_detection_mtcnn_demo/python/tools_matrix.py
+++ demos/face_detection_mtcnn_demo/python/tools_matrix.py
@@ -34,22 +34,22 @@ def NMS(rectangles, threshold, type):
     y2 = boxes[:, 3]
     s  = boxes[:, 4]
     area = np.multiply(x2-x1+1, y2-y1+1)
-    I = np.array(s.argsort())
+    i = np.array(s.argsort())
     pick = []
-    while len(I)>0:
-        xx1 = np.maximum(x1[I[-1]], x1[I[0:-1]]) #I[-1] have hightest prob score, I[0:-1]->others
-        yy1 = np.maximum(y1[I[-1]], y1[I[0:-1]])
-        xx2 = np.minimum(x2[I[-1]], x2[I[0:-1]])
-        yy2 = np.minimum(y2[I[-1]], y2[I[0:-1]])
+    while len(i)>0:
+        xx1 = np.maximum(x1[i[-1]], x1[i[0:-1]]) #I[-1] have hightest prob score, I[0:-1]->others
+        yy1 = np.maximum(y1[i[-1]], y1[i[0:-1]])
+        xx2 = np.minimum(x2[i[-1]], x2[i[0:-1]])
+        yy2 = np.minimum(y2[i[-1]], y2[i[0:-1]])
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
         if type == 'iom':
-            o = inter / np.minimum(area[I[-1]], area[I[0:-1]])
+            o = inter / np.minimum(area[i[-1]], area[i[0:-1]])
         else:
-            o = inter / (area[I[-1]] + area[I[0:-1]] - inter)
-        pick.append(I[-1])
-        I = I[np.where(o<=threshold)[0]]
+            o = inter / (area[i[-1]] + area[i[0:-1]] - inter)
+        pick.append(i[-1])
+        i = i[np.where(o<=threshold)[0]]
     result_rectangle = boxes[pick].tolist()
     return result_rectangle
 '''
@@ -194,7 +194,7 @@ def filter_face_48net(cls_prob, roi, pts, rectangles, width, height, score_thres
 '''
 Function:
     calculate multi-scale and limit the maxinum side to 1000
-Input: 
+Input:
     img: original image
 Output:
     pr_scale: limit the maxinum side to 1000, < 1.0
