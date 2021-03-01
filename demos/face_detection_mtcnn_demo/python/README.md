4cad425e3af3987cba8b3c872160bbb3aae45ef7
Vladimir Dudnik <vladimir.dudnik@intel.com>
2021-03-01 12:58:43 +0300

    modifications according review comments

--- demos/face_detection_mtcnn_demo/python/README.md
+++ demos/face_detection_mtcnn_demo/python/README.md
@@ -16,41 +16,53 @@ The demo workflow is the following:
 
 ## Running
 
-Run the application with the `-h` option to see the following usage message:
+Run the application with the `-h` option to see available command line parameters:
 
 ```
-usage: face_detection_mtcnn_demo.py \
-    [-h] \
-    [-i INPUT] \
-    [-m_p MODEL_PNET] \
-    [-m_r MODEL_RNET] \
-    [-m_o MODEL_ONET] \
-    [-d DEVICE]
+python3 face_detection_mtcnn_demo.py -h
+```
+
+The command yields the following usage message:
+
+```
+usage: face_detection_mtcnn_demo.py [-h] -i INPUT -m_p "<path>" -m_r "<path>"
+                                    -m_o "<path>" [-th "<num>"]
+                                    [-d "<device>"] [--loop] [--no_show]
+                                    [-o OUTPUT] [-limit OUTPUT_LIMIT]
+                                    [-u UTILIZATION_MONITORS]
 
 Options:
   -h, --help            Show this help message and exit.
   -i INPUT, --input INPUT
                         Required. Path to a test image file.
-  -m_p MODEL_PNET, --model_pnet MODEL_PNET
+  -m_p "<path>", --model_pnet "<path>"
                         Required. Path to an .xml file with a pnet model.
-  -m_r MODEL_RNET, --model_rnet MODEL_RNET
+  -m_r "<path>", --model_rnet "<path>"
                         Required. Path to an .xml file with a rnet model.
-  -m_o MODEL_ONET, --model_onet MODEL_ONET
+  -m_o "<path>", --model_onet "<path>"
                         Required. Path to an .xml file with a onet model.
-  -th THRESHOLD, --threshold THRESHOLD
+  -th "<num>", --threshold "<num>"
                         Optional. The threshold to define the face is
                         recognized or not.
-  -d DEVICE, --device DEVICE
+  -d "<device>", --device "<device>"
                         Optional. Specify the target device to infer on; CPU,
                         GPU, FPGA, HDDL, MYRIAD or HETERO: is acceptable. The
                         sample will look for a suitable plugin for device
                         specified. Default value is CPU
-
+  --loop                Optional. Enable reading the input in a loop.
+  --no_show             Optional. Don't show output
+  -o OUTPUT, --output OUTPUT
+                        Optional. Name of output to save.
+  -limit OUTPUT_LIMIT, --output_limit OUTPUT_LIMIT
+                        Optional. Number of frames to store in output. If 0 is
+                        set, all frames are stored.
+  -u UTILIZATION_MONITORS, --utilization_monitors UTILIZATION_MONITORS
+                        Optional. List of monitors to show initially.
 ```
 
 Running the application with an empty list of options yields the short version of the usage message and an error message.
 
-To run the demo, you can use public or pre-trained models. To download the pre-trained models, use the OpenVINO [Model Downloader](../../../tools/downloader/README.md). The list of models supported by the demo is in [models.lst](./models.lst).
+To run the demo, you can use public pre-trained MTCNN models. To download the pre-trained models, use the OpenVINO [Model Downloader](../../../tools/downloader/README.md). The list of models supported by the demo is in [models.lst](./models.lst).
 
 > **NOTE**: Before running the demo with a trained model, make sure the model is converted to the Inference Engine format (`*.xml` + `*.bin`) using the [Model Optimizer tool](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html).
 To run the demo, please provide paths to the model in the IR format, and to an input video or image(s):
--- demos/face_detection_mtcnn_demo/python/face_detection_mtcnn_demo.py
+++ demos/face_detection_mtcnn_demo/python/face_detection_mtcnn_demo.py
@@ -1,11 +1,22 @@
 import sys
 import os
 from argparse import ArgumentParser, SUPPRESS
+from pathlib import Path
+from time import perf_counter
 import cv2
 import numpy as np
-import logging as log
-import tools_matrix as tools
+import logging
+import mtcnn_utils as utils
 from openvino.inference_engine import IECore
+
+sys.path.append(str(Path(__file__).resolve().parents[2] / 'common/python'))
+
+import monitors
+from images_capture import open_images_capture
+from performance_metrics import PerformanceMetrics
+
+logging.basicConfig(format='[ %(levelname)s ] %(message)s', level=logging.INFO, stream=sys.stdout)
+log = logging.getLogger()
 
 
 score_threshold = [0.6, 0.7, 0.7]
@@ -36,15 +47,23 @@ def build_argparser():
                            "acceptable. The sample will look for a suitable plugin for device specified. Default "
                            "value is CPU",
                       default="CPU", type=str, metavar='"<device>"')
+    args.add_argument('--loop', default=False, action='store_true',
+                         help='Optional. Enable reading the input in a loop.')
     args.add_argument("--no_show",
                       help="Optional. Don't show output",
                       action='store_true')
+    args.add_argument('-o', '--output', required=False,
+                         help='Optional. Name of output to save.')
+    args.add_argument('-limit', '--output_limit', required=False, default=1000, type=int,
+                         help='Optional. Number of frames to store in output. '
+                              'If 0 is set, all frames are stored.')
+    args.add_argument('-u', '--utilization_monitors', default='', type=str,
+                         help='Optional. List of monitors to show initially.')
 
     return parser
 
 
-def image_reader(image, w, h):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def preprocess_image(image, w, h):
     image = cv2.resize(image, (w, h))
     # Change input shape to [B,C,W,H] for MTCNN
     image = image.transpose((2, 1, 0))
@@ -52,35 +71,28 @@ def image_reader(image, w, h):
     return image
 
 def main():
-    log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+    metrics = PerformanceMetrics()
+
     args = build_argparser().parse_args()
-    PNET_model_xml = args.model_pnet
-    PNET_model_bin = os.path.splitext(PNET_model_xml)[0] + ".bin"
-    RNET_model_xml = args.model_rnet
-    RNET_model_bin = os.path.splitext(RNET_model_xml)[0] + ".bin"
-    ONET_model_xml = args.model_onet
-    ONET_model_bin = os.path.splitext(ONET_model_xml)[0] + ".bin"
 
     # Plugin initialization for specified device and load extensions library if specified
     log.info("Creating Inference Engine")
 
-    ie_p = IECore()
-    ie_r = IECore()
-    ie_o = IECore()
+    ie = IECore()
 
     # Read IR
-    log.info("Loading network files:\n\t{}\n\t{}".format(PNET_model_xml, PNET_model_bin))
-    p_net = ie_p.read_network(model=PNET_model_xml, weights=PNET_model_bin)
+    log.info("Loading network files:\n\t{}".format(args.model_pnet))
+    p_net = ie.read_network(args.model_pnet)
     assert len(p_net.input_info.keys()) == 1, "Pnet supports only single input topologies"
     assert len(p_net.outputs) == 2, "Pnet supports two output topologies"
 
-    log.info("Loading network files:\n\t{}\n\t{}".format(RNET_model_xml, RNET_model_bin))
-    r_net = ie_r.read_network(model=RNET_model_xml, weights=RNET_model_bin)
+    log.info("Loading network files:\n\t{}".format(args.model_rnet))
+    r_net = ie.read_network(args.model_rnet)
     assert len(r_net.input_info.keys()) == 1, "Rnet supports only single input topologies"
     assert len(r_net.outputs) == 2, "Rnet supports two output topologies"
 
-    log.info("Loading network files:\n\t{}\n\t{}".format(ONET_model_xml, ONET_model_bin))
-    o_net = ie_o.read_network(model=ONET_model_xml, weights=ONET_model_bin)
+    log.info("Loading network files:\n\t{}".format(args.model_onet))
+    o_net = ie.read_network(args.model_onet)
     assert len(o_net.input_info.keys()) == 1, "Onet supports only single input topologies"
     assert len(o_net.outputs) == 3, "Onet supports three output topologies"
 
@@ -90,94 +102,126 @@ def main():
     rnet_input_blob = next(iter(r_net.input_info))
     onet_input_blob = next(iter(o_net.input_info))
 
+    cap = open_images_capture(args.input, args.loop)
+
+    next_frame_id = 0
+
+    log.info('Starting inference...')
+    print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
+
+    presenter = None
+    video_writer = cv2.VideoWriter()
+
     # Read image
-    origin_image = cv2.imread(args.input)
-    oh, ow, _ = origin_image.shape
+    while True:
+        start_time = perf_counter()
+        origin_image = cap.read()
+        if origin_image is None:
+            if next_frame_id == 0:
+                raise ValueError("Can't read an image from the input")
+            break
+        if next_frame_id == 0:
+            presenter = monitors.Presenter(args.utilization_monitors, 55,
+                                           (round(origin_image.shape[1] / 4), round(origin_image.shape[0] / 8)))
+            if args.output and not video_writer.open(args.output, cv2.VideoWriter_fourcc(*'MJPG'),
+                                                     cap.fps(), (origin_image.shape[1], origin_image.shape[0])):
+                raise RuntimeError("Can't open video writer")
+        next_frame_id += 1
 
-    scales = tools.calculateScales(origin_image)
+        rgb_image = cv2.cvtColor(origin_image, cv2.COLOR_BGR2RGB)
+        oh, ow, _ = rgb_image.shape
 
-    # *************************************
-    # Start Pnet
-    # *************************************
-    # Loading Pnet model to the plugin
-    log.info("Loading Pnet model to the plugin")
+        scales = utils.calculateScales(rgb_image)
 
-    t0 = cv2.getTickCount()
-    pnet_res = []
-    for scale in scales:
-        hs = int(oh*scale)
-        ws = int(ow*scale)
-        image = image_reader(origin_image, ws, hs)
+        # *************************************
+        # Pnet stage
+        # *************************************
+        log.info("Loading Pnet model to the plugin")
 
-        p_net.reshape({pnet_input_blob : [1, 3, ws, hs]})  # Change weidth and height of input blob
-        exec_pnet = ie_p.load_network(network=p_net, device_name=args.device)
+        t0 = cv2.getTickCount()
+        pnet_res = []
+        for scale in scales:
+            hs = int(oh*scale)
+            ws = int(ow*scale)
+            image = preprocess_image(rgb_image, ws, hs)
 
-        p_res = exec_pnet.infer(inputs={pnet_input_blob: image})
-        pnet_res.append(p_res)
+            p_net.reshape({pnet_input_blob : [1, 3, ws, hs]})  # Change weidth and height of input blob
+            exec_pnet = ie.load_network(network=p_net, device_name=args.device)
 
-    image_num = len(scales)
-    rectangles = []
-    for i in range(image_num):
-        (layer_name_roi, roi), (layer_name_cls, cls_prob) = pnet_res[i].items()
-        _, _, out_h, out_w = cls_prob.shape
-        out_side = max(out_h, out_w)
-        rectangle = tools.detect_face_12net(cls_prob[0][1], roi[0], out_side, 1/scales[i], ow, oh, score_threshold[0], iou_threshold[0])
-        rectangles.extend(rectangle)
-    rectangles = tools.NMS(rectangles, iou_threshold[1], 'iou')
+            p_res = exec_pnet.infer(inputs={pnet_input_blob: image})
+            pnet_res.append(p_res)
 
-    # Start Rnet
-    if len(rectangles) > 0:
-        # Loading Rnet model to the plugin
-        log.info("Loading Rnet model to the plugin")
+        image_num = len(scales)
+        rectangles = []
+        for i in range(image_num):
+            (layer_name_roi, roi), (layer_name_cls, cls_prob) = pnet_res[i].items()
+            _, _, out_h, out_w = cls_prob.shape
+            out_side = max(out_h, out_w)
+            rectangle = utils.detect_face_12net(cls_prob[0][1], roi[0], out_side, 1/scales[i], ow, oh, score_threshold[0], iou_threshold[0])
+            rectangles.extend(rectangle)
+        rectangles = utils.NMS(rectangles, iou_threshold[1], 'iou')
 
-        r_net.reshape({rnet_input_blob : [len(rectangles), 3, 24, 24]})  # Change batch size of input blob
-        exec_rnet = ie_r.load_network(network = r_net, device_name = args.device)
+        # Rnet stage
+        if len(rectangles) > 0:
+            log.info("Loading Rnet model to the plugin")
 
-        rnet_input = []
+            r_net.reshape({rnet_input_blob : [len(rectangles), 3, 24, 24]})  # Change batch size of input blob
+            exec_rnet = ie.load_network(network = r_net, device_name = args.device)
+
+            rnet_input = []
+            for rectangle in rectangles:
+                crop_img = rgb_image[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
+                crop_img = preprocess_image(crop_img, 24, 24)
+                rnet_input.extend(crop_img)
+
+            rnet_res = exec_rnet.infer(inputs={rnet_input_blob: rnet_input})
+
+            (layer_name_roi, roi_prob), (layer_name_cls, cls_prob)  = rnet_res.items()
+            rectangles = utils.filter_face_24net(cls_prob, roi_prob, rectangles, ow, oh, score_threshold[1], iou_threshold[2])
+
+        # Onet stage
+        if len(rectangles) > 0:
+            log.info("Loading Onet model to the plugin")
+
+            o_net.reshape({onet_input_blob : [len(rectangles), 3, 48, 48]})  # Change batch size of input blob
+            exec_onet = ie.load_network(network = o_net, device_name = args.device)
+
+            onet_input = []
+            for rectangle in rectangles:
+                crop_img = rgb_image[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
+                crop_img = preprocess_image(crop_img, 48, 48)
+                onet_input.extend(crop_img)
+
+            onet_res = exec_onet.infer(inputs={onet_input_blob: onet_input})
+
+            (layer_name_roi, roi_prob), (layer_name_landmark, pts_prob), (layer_name_cls, cls_prob)  = onet_res.items()
+            rectangles = utils.filter_face_48net(cls_prob, roi_prob, pts_prob, rectangles, ow, oh, score_threshold[2], iou_threshold[3])
+
+        # display results
         for rectangle in rectangles:
-            crop_img = origin_image[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
-            crop_img = image_reader(crop_img, 24, 24)
-            rnet_input.extend(crop_img)
+            # Draw detected boxes
+            cv2.putText(origin_image, 'confidence: {:.2f}'.format(rectangle[4]), (int(rectangle[0]), int(rectangle[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0))
+            cv2.rectangle(origin_image, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])), (255, 0, 0), 1)
+            for i in range(5, 15, 2):
+                cv2.circle(origin_image, (int(rectangle[i+0]), int(rectangle[i+1])), 2, (0, 255 , 0))
 
-        rnet_res = exec_rnet.infer(inputs={rnet_input_blob: rnet_input})
+        infer_time = (cv2.getTickCount() - t0) / cv2.getTickFrequency()  # Record infer time
+        cv2.putText(origin_image, 'summary: {:.1f} FPS'.format(
+            1.0 / infer_time), (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 200))
 
-        (layer_name_roi, roi_prob), (layer_name_cls, cls_prob)  = rnet_res.items()
-        rectangles = tools.filter_face_24net(cls_prob, roi_prob, rectangles, ow, oh, score_threshold[1], iou_threshold[2])
+        if video_writer.isOpened() and (args.output_limit <= 0 or next_frame_id <= args.output_limit - 1):
+            video_writer.write(origin_image)
 
-    # Start Onet
-    if len(rectangles) > 0:
-        log.info("Loading Onet model to the plugin")
+        if not args.no_show:
+            cv2.imshow('MTCNN Results', origin_image)
+            key = cv2.waitKey(1)
+            if key == 27 or key == 'q' or key == 'Q':
+                break
+            presenter.handleKey(key)
 
-        o_net.reshape({onet_input_blob : [len(rectangles), 3, 48, 48]})  # Change batch size of input blob
-        exec_onet = ie_o.load_network(network = o_net, device_name = args.device)
+        metrics.update(start_time, origin_image)
 
-        onet_input = []
-        for rectangle in rectangles:
-            crop_img = origin_image[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
-            crop_img = image_reader(crop_img, 48, 48)
-            onet_input.extend(crop_img)
-
-        onet_res = exec_onet.infer(inputs={onet_input_blob: onet_input})
-
-        (layer_name_roi, roi_prob), (layer_name_landmark, pts_prob), (layer_name_cls, cls_prob)  = onet_res.items()
-        rectangles = tools.filter_face_48net(cls_prob, roi_prob, pts_prob, rectangles, ow, oh, score_threshold[2], iou_threshold[3])
-
-    for rectangle in rectangles:
-        # Draw detected boxes
-        cv2.putText(origin_image, 'confidence: {:.2f}'.format(rectangle[4]), (int(rectangle[0]), int(rectangle[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0))
-        cv2.rectangle(origin_image, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])), (255, 0, 0), 1)
-        for i in range(5, 15, 2):
-            cv2.circle(origin_image, (int(rectangle[i+0]), int(rectangle[i+1])), 2, (0, 255 , 0))
-
-    infer_time = (cv2.getTickCount() - t0) / cv2.getTickFrequency()  # Record infer time
-    cv2.putText(origin_image, 'summary: {:.1f} FPS'.format(
-        1.0 / infer_time), (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 200))
-
-    if not args.no_show:
-        # Show resulting image.
-        cv2.imshow('test', origin_image)
-        cv2.waitKey()
-
+    metrics.print_total()
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
