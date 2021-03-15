aa112165c9383acfcc93c33812a3ac28b60c663d
Zuev,MikhailX <mikhailx.zuev@intel.com>
2021-03-16 00:11:59 +0300

    Fix PyLint warnings

--- tools/accuracy_checker/accuracy_checker/annotation_converters/image_processing.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/image_processing.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from pathlib import Path
 from ..config import PathField, StringField, ConfigError, BoolField
 from ..representation import ImageProcessingAnnotation
 from ..representation.image_processing import GTLoader
