aa57d5e414121ed4f89427bd523ebe40f21b2fe9
Nikita Ignatovsky <73649020+nignatovsky@users.noreply.github.com>
2021-03-17 12:07:28 +0300

    Removed additional wrappers.

--- demos/tests/run_tests.py
+++ demos/tests/run_tests.py
@@ -29,7 +29,6 @@ For the tests to work, the test data directory must contain:
 import argparse
 import contextlib
 import csv
-import io
 import json
 import os
 import platform
@@ -131,8 +130,6 @@ def prepare_models(auto_tools_dir, downloader_cache_dir, mo_path, global_temp_di
 
 def main():
     args = parse_args()
-    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
     omz_dir = (Path(__file__).parent / '../..').resolve()
     demos_dir = omz_dir / 'demos'
