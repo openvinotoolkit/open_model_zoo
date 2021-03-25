b7e8aa1b2baf1dfd2ac91235f53105553dfb80c8
ivikhrev <ivan.vikhrev@intel.com>
2021-03-25 19:15:15 +0300

    fix sh script

--- demos/build_demos.sh
+++ demos/build_demos.sh
@@ -34,9 +34,7 @@ for opt in "$@"; do
         extra_cmake_opts+=("$opt")
         ;;
     --target=*)
-        #tmp="${opt%\"}"
-        #tmp="${tmp#\"}"
-        build_targets+=("${tmp//=/ }")
+        build_targets+=("${opt//=/ }")
         ;;
     *)
         printf "Unknown option: %q\n" "$opt"
@@ -88,6 +86,6 @@ fi
 mkdir -p "$build_dir"
 
 (cd "$build_dir" && cmake -DCMAKE_BUILD_TYPE=Release "${extra_cmake_opts[@]}" "$DEMOS_PATH")
-cmake --build "$build_dir" "$build_targets" -- "$NUM_THREADS"
+cmake --build "$build_dir" ${build_targets[@]} -- "$NUM_THREADS"
 
 printf "\nBuild completed, you can find binaries for all demos in the %s subfolder.\n\n" "$build_dir/$OS_PATH/Release"
