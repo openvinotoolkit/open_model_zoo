fb9bc6218095843c4541ea002e5b9643b6e1bdf4
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-04-14 21:11:32 +0300

    handl ark files

--- tools/accuracy_checker/accuracy_checker/annotation_converters/kaldi_speech_recognition_pipeline.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/kaldi_speech_recognition_pipeline.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 from ..data_readers import KaldiMatrixIdentifier, KaldiARKReader
 from ..representation import CharacterRecognitionAnnotation
-from ..config import PathField
+from ..config import PathField, BoolField
 from ..utils import read_txt
 from .format_converter import BaseFormatConverter, ConverterReturn
 
@@ -29,7 +29,8 @@ class KaldiSpeechRecognitionDataConverter(BaseFormatConverter):
         params.update({
             'annotation_file': PathField(description='file with gt transcription'),
             'data_dir': PathField(description='directory with ark files', is_directory=True),
-            'features_subset_file': PathField(description='file with list testing ark files', optional=True)
+            'features_subset_file': PathField(description='file with list testing ark files', optional=True),
+            'ivectors': BoolField(optional=True, default=False, description='include ivectors features')
         })
         return params
 
@@ -37,26 +38,48 @@ class KaldiSpeechRecognitionDataConverter(BaseFormatConverter):
         self.annotation_file = self.get_value_from_config('annotation_file')
         self.data_dir = self.get_value_from_config('data_dir')
         self.feat_list_file = self.get_value_from_config('features_subset_file')
+        self.ivectors = self.get_value_from_config('ivectors')
 
     def convert(self, check_content=False, progress_callback=None, progress_interval=100, **kwargs):
         ark_list = self.select_subset()
         transcripts = self.read_annotation()
         annotations = []
         for ark in ark_list:
+            ivect = None
+            if isinstance(ark, tuple):
+                ark, ivect = ark
             utterances = KaldiARKReader.read_frames(ark)
             for utt in utterances:
                 if utt not in transcripts:
                     continue
-                identifier = KaldiMatrixIdentifier(ark.name, utt)
+
+                identifier = (
+                    KaldiMatrixIdentifier(ark.name, utt)
+                    if not ivect else [KaldiMatrixIdentifier(ark.name, utt), KaldiMatrixIdentifier(ivect.name, utt)]
+                )
+
                 gt = transcripts[utt]
                 annotations.append(CharacterRecognitionAnnotation(identifier, gt))
 
         return ConverterReturn(annotations, None, None)
 
     def select_subset(self):
+        if not self.ivectors:
+            if self.feat_list_file:
+                return [self.data_dir / ark for ark in read_txt(self.feat_list_file)]
+            return list(self.data_dir.glob('*.ark'))
         if self.feat_list_file:
-            return [self.data_dir / ark for ark in read_txt(self.feat_list_file)]
-        return list(self.data_dir.glob('*.ark'))
+            return [
+                (self.data_dir / ark.split(' ')[0], self.data_dir / ark.split(' ')[1])
+                for ark in read_txt(self.feat_list_file)
+            ]
+        pairs = []
+        for ivector_file in self.data_dir.glob("*_ivector.ark"):
+            feats_file = self.data_dir / ivector_file.name.replace('_ivector', '')
+            if not feats_file.exists():
+                continue
+            pairs.append((feats_file, ivector_file))
+        return pairs
 
     def read_annotation(self):
         trascript_dict = {}
