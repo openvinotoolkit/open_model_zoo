a71b432d7fb870ee9723cf257e13cdf1b0e765df
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-04-19 13:26:43 +0300

    replace pykaldi decoding with kaldi utilities

--- tools/accuracy_checker/accuracy_checker/adapters/wfst_decoder.py
+++ tools/accuracy_checker/accuracy_checker/adapters/wfst_decoder.py
@@ -14,24 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import os
+import struct
+import subprocess
+import tempfile
+from pathlib import Path
 import numpy as np
 from .adapter import Adapter
-from ..config import PathField, BoolField, NumberField
-from ..utils import read_txt, UnsupportedPackage
+from ..config import PathField, BoolField, NumberField, ConfigError
 from ..representation import CharacterRecognitionPrediction
-
-try:
-    from kaldi.asr import MappedLatticeFasterRecognizer
-    from kaldi.matrix import Matrix
-    from kaldi.decoder import LatticeFasterDecoderOptions
-except ImportError as err:
-    MappedLatticeFasterRecognizer = UnsupportedPackage('pykaldi', err.msg)
-    Matrix = UnsupportedPackage('pykaldi', err.msg)
-    LatticeFasterDecoderOptions = UnsupportedPackage('pykaldi', err.msg)
+from ..utils import read_txt
 
 
-class WFSTDecodingAdapter(Adapter):
-    __provider__ = 'kaldi_lattice_decoder'
+class KaldiLatGenDecoder(Adapter):
+    __provider__ = 'kaldi_latgen_faster_mapped'
 
     @classmethod
     def parameters(cls):
@@ -46,45 +42,26 @@ class WFSTDecodingAdapter(Adapter):
             'acoustic_scale': NumberField(optional=True, default=0.1, value_type=float),
             'min_active': NumberField(optional=True, value_type=int, min_value=9),
             'max_active': NumberField(optional=True, value_type=int, min_value=0),
+            'inverse_acoustic_scale': NumberField(optional=True, value_type=float, default=0),
+            'word_insertion_penalty': NumberField(optional=True, default=0, value_type=float),
+            'kaldi_bin_dir': PathField(is_directory=True)
         })
         return params
 
     def configure(self):
-        if isinstance(MappedLatticeFasterRecognizer, UnsupportedPackage):
-            MappedLatticeFasterRecognizer.raise_error(self.__provider__)
         self.fst_file = self.get_value_from_config('fst_file')
         self.words_file = self.get_value_from_config('words_file')
         self.transition_model = self.get_value_from_config('transition_model_file')
         self.words_table = self.read_words_table()
-        decoder_opts = LatticeFasterDecoderOptions()
-        beam = self.get_value_from_config('beam')
-        if beam:
-            decoder_opts.beam = beam
-        lattice_beam = self.get_value_from_config('lattice_beam')
-        if lattice_beam:
-            decoder_opts.lattice_beam = lattice_beam
-        min_active = self.get_value_from_config('min_active')
-        max_active = self.get_value_from_config('max_active')
-        if min_active:
-            decoder_opts.min_active = min_active
-        if max_active:
-            decoder_opts.max_active = max_active
-
-        self._decoder = MappedLatticeFasterRecognizer.from_files(
-            str(self.transition_model), str(self.fst_file), str(self.words_file),
-            allow_partial=self.get_value_from_config('allow_partial'),
-            acoustic_scale=self.get_value_from_config('acoustic_scale'),
-            decoder_opts=decoder_opts,
-        )
-
-    def process(self, raw, identifiers, frame_meta):
-        results = []
-        preds = self._extract_predictions(raw, frame_meta)
-        for identifier, log_scores in zip(identifiers, preds[self.output_blob]):
-            seq = self._decoder.decode(Matrix(log_scores))
-            trans = seq['text']
-            results.append(CharacterRecognitionPrediction(identifier, trans))
-        return results
+        self.beam = self.get_value_from_config('beam')
+        self.lattice_beam = self.get_value_from_config('lattice_beam')
+        self.min_active = self.get_value_from_config('min_active')
+        self.max_active = self.get_value_from_config('max_active')
+        self.acoustic_scale = self.get_value_from_config('acoustic_scale')
+        self.inv_acoustic_scale = self.get_value_from_config('inverse_acoustic_scale')
+        self.word_insertion_penalty = self.get_value_from_config('word_insertion_penalty')
+        self.allow_partial = self.get_value_from_config('allow_partial')
+        self.create_cmd()
 
     def read_words_table(self):
         words_table = {}
@@ -92,6 +69,60 @@ class WFSTDecodingAdapter(Adapter):
             word, idx = line.split()
             words_table[int(idx)] = word
         return words_table
+
+    def create_cmd(self):
+        self.kaldi_bin_dir = self.get_value_from_config('kaldi_bin_dir')
+        error_msg = 'Path to Kaldi executable {} is not found'
+        executable = '{}' if os.name != 'nt' else '{}.exe'
+        latgen_path = self.kaldi_bin_dir / executable.format('latgen-faster-mapped')
+        if not latgen_path.exists():
+            raise ConfigError(error_msg.format(latgen_path))
+        latgen_cmd = ' '.join([str(latgen_path),
+                      "--min-active={}".format(self.min_active),
+                      "--max-active={}".format(self.max_active),
+                      "--max-mem=50000000",
+                      "--beam={}".format(str(self.beam)),
+                      "--lattice-beam={}".format(str(self.lattice_beam)),
+                      "--acoustic-scale={}".format(str(self.acoustic_scale)),
+                      "--allow-partial={}".format(self.allow_partial),
+                      "--word-symbol-table={}".format(self.words_file),
+                      str(self.transition_model), str(self.fst_file),
+                      "ark:{}", "ark:-"])
+
+        lattice_scale_path = self.kaldi_bin_dir / executable.format('lattice-scale')
+        if not lattice_scale_path.exists():
+            raise ConfigError(error_msg.format(lattice_scale_path))
+        scale_cmd = '{} --inv-acoustic-scale={} ark:- ark:-'.format(lattice_scale_path, self.inv_acoustic_scale)
+        lattice_best_path = self.kaldi_bin_dir / executable.format('lattice-best-path')
+        if not lattice_best_path.exists():
+            raise ConfigError(error_msg.format(lattice_best_path))
+        best_path_cmd = '{} --word-symbol-table={} ark:- ark,t:-'.format(lattice_best_path, self.words_file)
+
+        lattice_add_penalty_path = self.kaldi_bin_dir / executable.format('lattice-add-penalty')
+        if not lattice_add_penalty_path.exists():
+            raise ConfigError(error_msg.format(lattice_add_penalty_path))
+        add_penalty_cmd = '{} --word-ins-penalty={} ark:- ark:-'.format(
+            lattice_add_penalty_path, self.word_insertion_penalty
+        )
+        self.decoder_cmd = ' | '.join([latgen_cmd, scale_cmd, add_penalty_cmd, best_path_cmd])
+        self._temp_dir = tempfile.TemporaryDirectory(suffix=self.__provider__, dir=Path.cwd())
+
+    def reset(self):
+        self._temp_dir.cleanup()
+
+    def release(self):
+        if Path(self._temp_dir.name).exists():
+            self._temp_dir.cleanup()
+
+    def process(self, raw, identifiers, frame_meta):
+        results = []
+        preds = self._extract_predictions(raw, frame_meta)
+        for identifier, log_scores in zip(identifiers, preds[self.output_blob]):
+            utt_name = identifier.key if not isinstance(identifier, list) else identifier[0].key
+            scores_file = self.dump_scores(utt_name, log_scores)
+            trans = self.run_decoder(scores_file)
+            results.append(CharacterRecognitionPrediction(identifier, trans[utt_name]))
+        return results
 
     def _extract_predictions(self, outputs_list, meta):
         is_multi_infer = meta[-1].get('multi_infer', False) if meta else False
@@ -103,3 +134,46 @@ class WFSTDecodingAdapter(Adapter):
         }
 
         return output_map
+
+    def dump_scores(self, utterance_key, mat):
+        out_file = Path(self._temp_dir.name) / '{}_scores.ark'.format(utterance_key)
+        with out_file.open('wb') as fd:
+            fd.write(str.encode(utterance_key + " "))
+            fd.write(str.encode('\0B'))
+            if mat.dtype not in [np.float32, np.float64]:
+                raise RuntimeError("Unsupported numpy dtype: {}".format(mat.dtype))
+            mat_type = 'FM' if mat.dtype == np.float32 else 'DM'
+            fd.write(str.encode(mat_type + " "))
+            num_rows, num_cols = mat.shape
+            fd.write(str.encode('\04'))
+            int_pack = struct.pack('i', num_rows)
+            fd.write(int_pack)
+            fd.write(str.encode('\04'))
+            int_pack = struct.pack('i', num_cols)
+            fd.write(int_pack)
+            fd.write(mat.tobytes())
+        return out_file
+
+    def run_decoder(self, scores_file):
+
+        def get_cmd_result(process):
+            _, stderr = process.communicate()
+            stderr = stderr.decode('utf-8') if stderr else None
+            if process.returncode != 0:
+                raise RuntimeError("\nAn error occurred!\n Return code: {}\n Error output:\n{}"
+                                   .format(str(process.returncode), stderr))
+
+        outfile = scores_file.with_suffix('.txt')
+        with outfile.open('w') as f:
+            p = subprocess.Popen(self.decoder_cmd.format(scores_file), stdout=f, stderr=subprocess.PIPE, shell=True)
+            get_cmd_result(p)
+        return self.get_transcript(outfile)
+
+    def get_transcript(self, lattice_file):
+        transcripts = {}
+        for line in read_txt(lattice_file):
+            result = line.split(' ')
+            utt = result[0]
+            decoded = ' '.join([self.words_table[int(idx)] for idx in result[1:]])
+            transcripts[utt] = decoded
+        return transcripts
