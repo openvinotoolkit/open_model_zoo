fcbd80af121492fd41d0ffef51038b4b3ec20548
Vladimir <10669582+Wovchena@users.noreply.github.com>
2021-05-27 23:43:43 +0300

    add missing comma

--- tools/downloader/src/open_model_zoo/model_tools/_common.py
+++ tools/downloader/src/open_model_zoo/model_tools/_common.py
@@ -59,7 +59,7 @@ KNOWN_TASK_TYPES = {
     'machine_translation',
     'monocular_depth_estimation',
     'named_entity_recognition',
-    'noise_suppression'
+    'noise_suppression',
     'object_attributes',
     'optical_character_recognition',
     'place_recognition',
