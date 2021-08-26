42ef93e69310398a3bc02827d0779b516b01f313
n_ignatovsky <nikita.ignatovsky@intel.com>
2021-08-26 23:15:44 +0300

    Fixed --m_reid argument.

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -821,13 +821,13 @@ PYTHON_DEMOS = [
     )),
 
     PythonDemo(name='multi_camera_multi_target_tracking_demo', device_keys=['-d'],
-               model_keys=['-m', '-m_reid'], test_cases=combine_cases(
+               model_keys=['-m', '--m_reid'], test_cases=combine_cases(
         TestCase(options={'--no_show': None,
             **MONITORS,
             '-i': [DataPatternArg('multi-camera-multi-target-tracking'),
                 DataPatternArg('multi-camera-multi-target-tracking/repeated')],
             '-m': ModelArg('person-detection-retail-0013')}),
-        single_option_cases('-m_reid',
+        single_option_cases('--m_reid',
             ModelArg('person-reidentification-retail-0277'),
             ModelArg('person-reidentification-retail-0286'),
             ModelArg('person-reidentification-retail-0287'),
