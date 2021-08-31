fc080f652d96ff0603edf0126933ce6752a9d020
legendary111 <86969009+legendary111@users.noreply.github.com>
2021-08-31 20:22:51 +0800

    Add files via upload

--- tools/accuracy_checker/accuracy_checker/annotation_converters/__init__.py
+++ tools/accuracy_checker/accuracy_checker/annotation_converters/__init__.py
@@ -19,7 +19,7 @@ from .convert import make_subset, save_annotation, analyze_dataset, DatasetConve
 from .market1501 import Market1501Converter
 from .veri776 import VeRi776Converter
 from .mars import MARSConverter
-from .pascal_voc import PascalVOCDetectionConverter,SYGDetectionConverter
+from .pascal_voc import PascalVOCDetectionConverter, SYGDetectionConverter
 from .sample_converter import SampleConverter
 from .wider import WiderFormatConverter
 from .detection_opencv_storage import DetectionOpenCVStorageFormatConverter
@@ -123,7 +123,7 @@ __all__ = [
     'VeRi776Converter',
     'SampleConverter',
     'PascalVOCDetectionConverter',
-    'SYGDetectionConverter'
+    'SYGDetectionConverter',
     'WiderFormatConverter',
     'MARSConverter',
     'DetectionOpenCVStorageFormatConverter',
