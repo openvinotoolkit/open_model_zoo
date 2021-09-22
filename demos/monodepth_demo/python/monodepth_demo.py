d65dbc15163400a33da33674b9b8616a198a1f7c
Pankratova <anzhella.pankratova@intel.com>
2021-09-22 15:14:15 +0300

    Move monodepth.py to openvino/model_zoo/model_api/

--- demos/monodepth_demo/python/monodepth_demo.py
+++ demos/monodepth_demo/python/monodepth_demo.py
@@ -26,12 +26,14 @@ import logging as log
 from openvino.inference_engine import IECore, get_version
 
 sys.path.append(str(Path(__file__).resolve().parents[2] / 'common/python'))
+sys.path.append(str(Path(__file__).resolve().parents[2] / 'common/python/openvino/model_zoo'))
+
+from model_api.models import MonoDepthModel, OutputTransform
+from model_api.pipelines import get_user_config, parse_devices, AsyncPipeline
+from model_api.performance_metrics import PerformanceMetrics
 
 import monitors
-from models import MonoDepthModel, OutputTransform
-from pipelines import get_user_config, parse_devices, AsyncPipeline
 from images_capture import open_images_capture
-from performance_metrics import PerformanceMetrics
 from helpers import resolution, log_blobs_info, log_runtime_settings
 
 
