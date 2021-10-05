756265ef54a53345b7695f46f6e72ca4896a6471
ivikhrev <ivan.vikhrev@intel.com>
2021-10-05 11:53:32 +0300

    added softmax to networks where needed, added topk layer to all

--- demos/common/python/openvino/model_zoo/model_api/models/classification.py
+++ demos/common/python/openvino/model_zoo/model_api/models/classification.py
@@ -14,7 +14,8 @@
  limitations under the License.
 """
 
-import numpy as np
+import ngraph as ng
+from openvino.inference_engine import IENetwork
 
 from .image_model import ImageModel
 
@@ -26,9 +27,30 @@ class Classification(ImageModel):
         if self.h != self.w:
             raise RuntimeError('Model input has incorrect image shape. Must be NxN square. '
                                 'Got {} x {}.'.format(self.h, self.w))
+
         self.ntop = ntop
         self.labels = self._load_labels(labels)
-        self.output_blob_name = self._get_outputs()
+        self._check_outputs()
+
+        function = ng.function_from_cnn(self.net)
+        if 'Softmax' not in [node.get_type_name() for node in function.get_ops()]:
+            logits = function.get_output_op(0)
+            logits = logits.inputs()[0].get_source_output().get_node()
+            logits.set_friendly_name('logits')
+            probs_node = ng.softmax(logits, 1)
+        else:
+            probs_node = function.get_output_op(0)
+            probs_node = probs_node.inputs()[0].get_source_output().get_node()
+            probs_node.set_friendly_name('probs')
+
+        topk_node = ng.topk(probs_node, ntop, 1, "max", "value")
+        f = ng.Function(
+            [ng.result(topk_node.outputs()[0], name='TopK_scores'),
+             ng.result(topk_node.outputs()[1], name='TopK_indices')],
+            function.get_parameters(), 'classification_res')
+        self.net = IENetwork(ng.impl.Function.to_capsule(f))
+        self.out_blob_names = list(self.net.outputs.keys())
+        self.out_blob_names.sort() # sort names to make sure that out_blob_names[0] - scores, out_blob_names[1] - indices
 
     @staticmethod
     def _load_labels(labels_file):
@@ -42,7 +64,7 @@ class Classification(ImageModel):
                 labels.append(s[(begin_idx + 1):end_idx])
         return labels
 
-    def _get_outputs(self):
+    def _check_outputs(self):
         out_blob_name = next(iter(self.net.outputs))
         out_blob = self.net.outputs[out_blob_name]
         out_size = out_blob.shape
@@ -51,19 +73,14 @@ class Classification(ImageModel):
         if len(out_size) == 4 and (out_size[2] != 1 or out_size[3] != 1):
             raise RuntimeError('The Classification model wrapper supports topologies only with 4D '
                                'output which has last two dimensions of size 1')
-
         if (out_size[1] == len(self.labels) + 1):
             self.labels.insert(0, 'other')
             self.logger.warning("\tInserted 'other' label as first.")
         if out_size[1] != len(self.labels):
             raise RuntimeError("Model's number of classes and parsed "
-                               'labels must match ({} != {})'.format(out_size[1], len(self.labels)))
-
-        return out_blob_name
+                              'labels must match ({} != {})'.format(out_size[1], len(self.labels)))
 
     def postprocess(self, outputs, meta):
-        probs = outputs[self.output_blob_name].squeeze()
-        indices = np.argsort(probs)
-        max_indices = indices[-self.ntop:][::-1] # indices sorted by probs in descended order
-        result = [(i, probs[i]) for i in max_indices]
-        return result
+        scores = outputs[self.out_blob_names[0]].squeeze()
+        indices = outputs[self.out_blob_names[1]].squeeze()
+        return list(zip(indices, scores))
