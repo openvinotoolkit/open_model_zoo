5c0ea6caca57e6f9fc23940be61668cf857a985d
Pankratova <anzhella.pankratova@intel.com>
2021-10-11 12:03:05 +0300

    Add performance scope

---
+++ demos/tests/performance_cases.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+from args import DataPatternArg
+
+from cases import BASE, single_option_cases
+
+THREADS_NUM = os.cpu_count()
+
+
+class PerformanceParser:
+    def __init__(self, demo):
+        self.filename = demo._exec_name.replace('/', '_') + '.csv'
+        self.model_keys = demo.model_keys
+
+    def __call__(self, output, test_case, device):
+        result = self.parse_metrics(output)
+        self.write_to_csv(result, test_case, device)
+
+    def parse_metrics(self, output):
+
+        def get_metric(pattern):
+            metric = pattern.search(output)
+            return metric.group(1) if metric else 'N/A'
+
+        latency_re = re.compile(r'Latency: (([0-9]+)\.[0-9]+)')
+        fps_re = re.compile(r'FPS: (([0-9]+)\.[0-9]+)')
+
+        return {'Latency': get_metric(latency_re), 'FPS': get_metric(fps_re)}
+
+    def write_to_csv(self, result, test_case, device):
+        result['Nireq'] = test_case.options.get('-nireq', '-')
+        result['Nstreams'] = test_case.options.get('-nstreams', '-')
+        result['Nthreads'] = test_case.options.get('-nthreads', '-')
+
+        if not os.path.isfile(self.filename):
+            models_col = [f"Model {key}" for key in self.model_keys]
+            columns = ','.join(['Device', *models_col, *result.keys()])
+            with open(self.filename, 'w') as f:
+                print(columns, file=f)
+
+        models_names = [test_case.options[key].name if key in test_case.options else '-'
+                        for key in self.model_keys]
+        data = ','.join([device, *models_names, *result.values()])
+        with open(self.filename, 'a') as f:
+            print(data, file=f)
+
+
+DEMOS = [
+    BASE['interactive_face_detection_demo/cpp'],
+
+    BASE['object_detection_demo/python']
+        .only_models(['person-detection-0200', 'yolo-v2-tf'])
+        .update_option({'-i': DataPatternArg('action-recognition')})
+        .add_test_cases(single_option_cases('-nireq', '3', '5'),
+                        single_option_cases('-nstreams', '3', '4'),
+                        single_option_cases('-nthreads', str(THREADS_NUM), str(THREADS_NUM - 2)))
+]
+
+
+DEMOS = [demo.add_parser(PerformanceParser) for demo in DEMOS]
