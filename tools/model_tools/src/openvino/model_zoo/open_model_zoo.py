3ee6316bf43740e35b4fa4233d65202e5eaf5253
jkamelin <julia.kamelina@intel.com>
2021-10-26 11:37:57 +0300

    add ac config and model config properties

---
+++ tools/model_tools/src/openvino/model_zoo/open_model_zoo.py
@@ -0,0 +1,152 @@
+import cv2
+import os
+import argparse
+import yaml
+
+from pathlib import Path
+
+from  openvino.model_zoo import (
+    _configuration, _common, omz_downloader, omz_converter
+)
+from openvino.model_zoo.download_engine import validation
+
+
+class Model:
+    def __init__(self, name=None, model_path=None, description=None, task_type=None, subdirectory=None):
+        self.model_path = model_path
+        self.description = description
+        self.task_type = task_type
+        self.name = name
+        self.subdirectory = subdirectory
+
+        self.net = None
+        self.exec_net = None
+        self._ie = None
+        self._accuracy_config = None
+        self._model_config = None
+
+    @classmethod
+    def download_model(cls, model_name, *, precision='FP32', download_dir='models', cache_dir=None):
+        '''
+        Downloads target model. If the model has already been downloaded,
+        retrieves the model from the cache instead of downloading it again.
+        Then creates OpenVINO Intermediate Representation (IR) network model.
+
+        Creates an object which stores information about the downloaded and IR model.
+
+        Parameters
+        ----------
+            model_name
+                Target model name.
+            precision
+                Target model precisions.
+            download_dir
+                Model download directory. By default creates a folder 'models'
+                in current directory and downloads to it.
+            cache_dir
+                Cache directory.
+                The script will place a copy of each downloaded file in the cache, or,
+                if it is already there, retrieve it from the cache.
+                By default creates a folder '.cache' in current download directory.
+        '''
+        download_dir = Path(download_dir)
+        cache_dir = cache_dir or download_dir / '.cache'
+
+        parser = argparse.ArgumentParser()
+        args = argparse.Namespace(all=False, list=None, name=model_name, print_all=False)
+        model = _configuration.load_models_from_args(parser, args, _common.MODEL_ROOT)[0]
+
+        model_dir = download_dir / model.subdirectory
+
+        flags = ['--name=' + model_name,
+                 '--output_dir=' + str(download_dir),
+                 '--cache_dir=' + str(cache_dir),
+                 '--precisions=' + str(precision)]
+
+        omz_downloader.main(flags)
+
+        description = '{}\n\n    License: {}'.format(model.description, model.license_url)
+        task_type = model.task_type
+
+        if precision not in model.precisions:
+            raise ValueError('Incorrect precision value!\n    Current precision value is: \''
+                                        + str(precision) + '\'.\n    Allowed precision values for model are: '
+                                        + str(model.precisions))
+
+        prefix = Path(model_dir) / precision / model_name
+
+        model_path = str(prefix) + '.xml'
+        bin_path = str(prefix) + '.bin'
+        name = model.name
+        subdirectory = model.subdirectory
+
+        if not os.path.exists(model_path) or not os.path.exists(bin_path):
+            omz_converter.main(['--name=' + model_name, '--precisions=' + precision,
+                        '--download_dir=' + str(download_dir)])
+        return cls(name, model_path, description, task_type, subdirectory)
+
+    @classmethod
+    def from_pretrained(cls, model_path, *, task_type=None):
+        '''
+        Loads model from existing .xml, .onnx files.
+        Parameters
+        ----------
+            model_path
+                Path to .xml or .onnx file.
+            task_type
+                Type of task that the model performs.
+        '''
+        model_path = Path(model_path).resolve()
+
+        if not model_path.exists():
+            raise ValueError('Path {} to model file does not exist.'.format(model_path))
+
+        if model_path.suffix not in ['.xml', '.onnx']:
+            raise ValueError('Unsupported model format {}. Only .xml or .onnx supported.'.format(model_path.suffix))
+
+        description = 'Pretrained model {}'.format(model_path.name)
+        name = model_path.name
+        task_type = task_type
+        subdirectory = model_path.parent
+        model_path = str(model_path)
+
+        return cls(name, model_path, description, task_type, subdirectory)
+
+    def load(self, ie, device='CPU'):
+        self.net = ie.read_network(self.model_path)
+        self.exec_net = ie.load_network(self.net, device)
+
+    def __call__(self, inputs, ie=None, device='CPU'):
+        if self.exec_net is None:
+            self.load(ie, device)
+
+        input_names = next(iter(self.net.input_info))
+        for input_name, value in inputs:
+            if input_name not in input_names:
+                raise ValueError('Unknown input name {}'.format(input_name))
+
+            input_shape = self.net.input_info[input_name].input_data.shape
+            if input_shape != value.shape:
+                value = cv2.resize(value, input_shape)
+        res = self.exec_net.infer(inputs=inputs)
+        return res
+
+    @property
+    def accuracy_checker_config(self):
+        if self._accuracy_config is None:
+            config_path = _common.MODEL_ROOT / self.subdirectory / 'accuracy-check.yml'
+            with config_path.open('rb') as config_file, \
+                    validation.deserialization_context('Loading config "{}"'.format(config_path)):
+                self._accuracy_config = yaml.safe_load(config_file)
+
+        return self._accuracy_config
+
+    @property
+    def model_config(self):
+        if self._model_config is None:
+            config_path = _common.MODEL_ROOT / self.subdirectory / 'model.yml'
+            with config_path.open('rb') as config_file, \
+                    validation.deserialization_context('Loading config "{}"'.format(config_path)):
+                self._model_config = yaml.safe_load(config_file)
+
+        return self._model_config
--- tools/model_tools/tests/open_model_zoo_test.py
+++ tools/model_tools/tests/open_model_zoo_test.py
@@ -2,20 +2,19 @@ import numpy as np
 import unittest
 import os
 
-import openvino.open_model_zoo as omz
+import openvino.model_zoo.open_model_zoo as omz
 
 from openvino.inference_engine import IECore
 
 class TestTopologies(unittest.TestCase):
     def test_load_intel(self):
-        face_detection = omz.Model('face-detection-0200', precision='FP16-INT8')
-        xml_path, bin_path = face_detection.model_path, face_detection.bin_path
+        face_detection = omz.Model.download_model('face-detection-0200', precision='FP16-INT8')
+        xml_path = face_detection.model_path
 
         self.assertTrue(os.path.exists(xml_path))
-        self.assertTrue(os.path.exists(bin_path))
 
         ie = IECore()
-        net = ie.read_network(xml_path, bin_path)
+        net = ie.read_network(xml_path)
         exec_net = ie.load_network(net, 'CPU')
         infer_request = exec_net.requests[0]
 
@@ -26,14 +25,13 @@ class TestTopologies(unittest.TestCase):
         self.assertEqual(output_blob.tensor_desc.dims, [1, 1, 200, 7])
 
     def test_load_public(self):
-        model = omz.Model('colorization-v2', precision='FP32')
-        xml_path, bin_path = model.model_path, model.bin_path
+        model = omz.Model.download_model('colorization-v2', precision='FP32')
+        xml_path = model.model_path
 
         self.assertTrue(os.path.exists(xml_path))
-        self.assertTrue(os.path.exists(bin_path))
 
         ie = IECore()
-        net = ie.read_network(xml_path, bin_path)
+        net = ie.read_network(xml_path)
         exec_net = ie.load_network(net, 'CPU')
         infer_request = exec_net.requests[0]
 
@@ -45,13 +43,12 @@ class TestTopologies(unittest.TestCase):
 
     def test_load_from_prtained(self):
         model = omz.Model.from_pretrained('models/public/colorization-v2/FP32/colorization-v2.xml')
-        xml_path, bin_path = model.model_path, model.bin_path
+        xml_path = model.model_path
 
         self.assertTrue(os.path.exists(xml_path))
-        self.assertTrue(os.path.exists(bin_path))
 
         ie = IECore()
-        net = ie.read_network(xml_path, bin_path)
+        net = ie.read_network(xml_path)
         exec_net = ie.load_network(net, 'CPU')
         infer_request = exec_net.requests[0]
 
@@ -62,18 +59,25 @@ class TestTopologies(unittest.TestCase):
         self.assertEqual(output_blob.tensor_desc.dims, [1, 2, 256, 256])
 
     def test_load_model(self):
-        model = omz.Model('colorization-v2', cache_dir='models/public/colorization-v2/')
+        model = omz.Model.download_model('colorization-v2', cache_dir='models/public/colorization-v2/')
         ie = IECore()
         model.load(ie)
         self.assertIsNotNone(model.net)
         self.assertIsNotNone(model.exec_net)
 
+    def test_get_accuracy_checker_config(self):
+        model = omz.Model.download_model('colorization-v2', cache_dir='models/public/colorization-v2/')
+        self.assertIsInstance(model.accuracy_checker_config, dict)
+
+    def test_get_model_config(self):
+        model = omz.Model.download_model('colorization-v2', cache_dir='models/public/colorization-v2/')
+        self.assertIsInstance(model.model_config, dict)
+
     def test_infer_model(self):
-        model = omz.Model('colorization-v2', cache_dir='models/public/colorization-v2/')
+        model = omz.Model('colorization-v2', 'models/public/colorization-v2/FP32/colorization-v2.xml')
         # dummy_inputs = np.zeros(input_shape)
         # ie = IECore()
         
-
 
 if __name__ == '__main__':
     unittest.main()
