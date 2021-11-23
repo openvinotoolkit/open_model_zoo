2004bf9d599784ee4ec4bf347e3b22bd26261c97
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-11-23 14:44:12 +0300

    Update tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/pascal_voc.py

--- tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/pascal_voc.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/pascal_voc.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from pathlib import Path
 from ..config import PathField, BoolField
 from ..representation import DetectionAnnotation, SegmentationAnnotation
 from ..representation.segmentation_representation import GTMaskLoader
