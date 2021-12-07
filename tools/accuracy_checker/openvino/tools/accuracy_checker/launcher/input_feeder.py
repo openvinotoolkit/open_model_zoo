030a25702f192ea7a6dac20d0f2ef14d9b2e1470
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-12-07 13:59:29 +0300

    Update tools/accuracy_checker/openvino/tools/accuracy_checker/launcher/input_feeder.py

--- tools/accuracy_checker/openvino/tools/accuracy_checker/launcher/input_feeder.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/launcher/input_feeder.py
@@ -68,7 +68,7 @@ PRECISION_TO_DTYPE = {
     'i32': np.int32,
     'i64': np.int64,
     'fp16': np.float16,
-    'f16': np.float16
+    'f16': np.float16,
     'i16': np.int16,
     'u16': np.uint16,
     'i8': np.int8,
