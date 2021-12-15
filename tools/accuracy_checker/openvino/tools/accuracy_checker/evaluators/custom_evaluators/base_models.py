5684f72a48bddfc7e729e525abcc0aac88bdfeb3
Ekaterina Aidova <ekaterina.aidova@intel.com>
2021-12-15 13:34:43 +0300

    fix cascade model for single model evaluators

--- tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/custom_evaluators/base_models.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/custom_evaluators/base_models.py
@@ -55,10 +55,16 @@ class BaseCascadeModel:
                 model.release()
 
     def load_network(self, network_list, launcher):
+        if len(self._part_by_name) == 1 and 'name' not in network_list[0]:
+            next(iter(self._part_by_name.values())).load_model(network_list[0]['model'], launcher)
+            return
         for network_dict in network_list:
             self._part_by_name[network_dict['name']].load_network(network_dict['model'], launcher)
 
     def load_model(self, network_list, launcher):
+        if len(self._part_by_name) == 1 and 'name' not in network_list[0]:
+            next(iter(self._part_by_name.values())).load_model(network_list[0], launcher)
+            return
         for network_dict in network_list:
             self._part_by_name[network_dict['name']].load_model(network_dict, launcher)
 
