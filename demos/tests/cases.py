2bf99555b59d35e2c9148bb70142da6bdbfc4931
Dmitry Sidnev <dmitry.sidnev@intel.com>
2021-12-15 16:31:34 +0300

    Add test

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -705,6 +705,15 @@ PYTHON_DEMOS = [
         ],
     )),
 
+    PythonDemo(name='background_subtraction_demo', device_keys=['-d'], test_cases=combine_cases(
+        TestCase(options={'--no_show': None,
+            **MONITORS,
+            '-i': DataPatternArg('instance-segmentation'),
+            '--delay': '1'}),
+        single_option_cases('-m',
+            ModelArg('instance-segmentation-person-0007')),
+    )),
+
     PythonDemo(name='bert_question_answering_demo', device_keys=['-d'], test_cases=combine_cases(
         TestCase(options={'-i': 'https://en.wikipedia.org/wiki/OpenVINO',
                           '--questions': ['What frameworks does OpenVINO support?', 'Who are developers?']}),
