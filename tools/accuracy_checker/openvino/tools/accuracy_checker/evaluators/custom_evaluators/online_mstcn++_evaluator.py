ad510c134ae4c55d55524f0d381c84dab599073b
yufeiwu <yufei.wu@intel.com>
2022-01-04 16:33:21 +0800

    [AC] Add new model: smartlab-sequence-modelling-0001

--- tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/custom_evaluators/online_mstcn++_evaluator.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/custom_evaluators/online_mstcn++_evaluator.py
@@ -1,48 +1,70 @@
 from .base_custom_evaluator import BaseCustomEvaluator
+from .base_models import BaseOpenVINOModel
+from ...data_readers import create_reader
+from collections import OrderedDict
+import numpy as np
 
-def create_model(model_config, launcher, data_source, launcher_model_mapping, suffix=None, delayed_model_loading=False):
-    framework = launcher.config['framework']
-    model_class = launcher_model_mapping.get(framework)
-    if not model_class:
-        raise ValueError('model for framework {} is not supported'.format(framework))
-    return model_class(model_config, launcher, data_source, suffix, delayed_model_loading)
+
+class online_MSTCN_plus(BaseOpenVINOModel):
+    def __init__(self, network_info, launcher):
+        super(online_MSTCN_plus, self).__init__(network_info, launcher)
+        reader_config = network_info.get('reader', {})
+        self.reader = create_reader(reader_config)
+
+    def infer(self, feed_dict):
+        return self.exec_network.infer(feed_dict)
+
+    def prepare_data(self, data):
+        return self.reader(data)
+
+    def predict(self, identifiers, input_data):
+        input_dict = {'input': input_data[0].data, 'fhis_in_0': input_data[1].data[0],
+                      'fhis_in_1': input_data[1].data[1], 'fhis_in_2': input_data[1].data[2],
+                      'fhis_in_3': input_data[1].data[3]}
+        return self.infer(input_dict)
+
 
 class MSTCNEvaluator(BaseCustomEvaluator):
     def __init__(self, dataset_config, launcher, model, orig_config):
-            super().__init__(dataset_config, launcher, orig_config)
-            self.model = model      
-    
+        super().__init__(dataset_config, launcher, orig_config)
+        self.model = model
+        self.metric_result = []
+
     @classmethod
     def from_configs(cls, config, delayed_model_loading=False, orig_config=None):
         dataset_config, launcher, launcher_config = cls.get_dataset_and_launcher_info(config)
-        data_source = dataset_config[0].get('data_source', None)
+        model = online_MSTCN_plus(config.get('network_info', {}), launcher)
 
-        model = create_model()
+        return cls(dataset_config, launcher, model, orig_config)
 
-        adapter.output_blob = model.output_blob
-        return cls(dataset_config, launcher, adapter, model, orig_config)
+    @staticmethod
+    def get_dataset_info(dataset):
+        identifiers = dataset.identifiers
+        annotation_reader = create_reader(
+            {'type': 'numpy_reader', 'data_source': dataset.dataset_config["data_source"]})
+        annotation = annotation_reader(identifiers)
+        return annotation, identifiers
 
     def _process(self, output_callback, calculate_metrics, progress_reporter, metric_config, csv_file):
         annotation, identifiers = self.get_dataset_info(self.dataset)
-        for batch_id, (batch_annotation, batch_identifiers) in enumerate(zip(annotation, identifiers)):
-            batch_inputs_images = self.model.rgb_model.prepare_data(batch_identifiers)
-            batch_inputs_flow = self.model.flow_model.prepare_data(batch_identifiers)
+        for batch_id in range(len(identifiers) // 2):
+            batch_annotation = [item for item in annotation.data[batch_id + 2]] + [annotation.data[batch_id]]
+            batch_inputs_1 = self.model.prepare_data("input_1_%d.npy" % batch_id)
+            batch_inputs_2 = self.model.prepare_data("input_2_%d.npz" % batch_id)
+            batch_prediction = list(self.model.predict(batch_id, [batch_inputs_1, batch_inputs_2]).values())
 
-            extr_batch_inputs_images, _ = extract_image_representations([batch_inputs_images])
-            extr_batch_inputs_flow, _ = extract_image_representations([batch_inputs_flow])
+            metrics_result = OrderedDict()
+            result = np.mean(
+                [np.linalg.norm(batch_prediction[i] - batch_annotation[i]) for i in range(len(batch_prediction))])
+            metrics_result[batch_id] = result
+            self.metric_result.append(result)
+            if output_callback:
+                output_callback(batch_prediction, metrics_result=metrics_result,
+                                element_identifiers=batch_id, dataset_indices=batch_id)
+            self._update_progress(progress_reporter, metric_config, batch_id, 1, csv_file)
 
-            batch_raw_prediction_rgb = self.model.rgb_model.predict(batch_identifiers, extr_batch_inputs_images)
-            batch_raw_prediction_flow = self.model.flow_model.predict(batch_identifiers, extr_batch_inputs_flow)
-            batch_raw_out = self.combine_predictions(batch_raw_prediction_rgb, batch_raw_prediction_flow)
+    def extract_metrics_results(self, print_results=True, ignore_results_formatting=False,
+                                ignore_metric_reference=False):
 
-            batch_prediction = self.adapter.process([batch_raw_out], identifiers, [{}])
-
-            if self.metric_executor.need_store_predictions:
-                self._annotations.extend([batch_annotation])
-                self._predictions.extend(batch_prediction)
-
-            if self.metric_executor:
-                self.metric_executor.update_metrics_on_batch(
-                    [batch_id], [batch_annotation], batch_prediction
-                )
-            self._update_progress(progress_reporter, metric_config, batch_id, len(batch_prediction), csv_file)
+        extracted_meta = []
+        return self.metric_result, extracted_meta
