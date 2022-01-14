8edaf1b1f670972b68831e3802373660df961d1b
ieliz <ilya.elizarov@intel.com>
2022-01-14 17:45:21 +0300

    fixing PR comments

--- demos/noise_suppression_demo/python/noise_suppression_demo.py
+++ demos/noise_suppression_demo/python/noise_suppression_demo.py
@@ -15,6 +15,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
+from json import encoder
 import logging as log
 import sys
 import copy
@@ -34,7 +35,7 @@ def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument("-m", "--model_path", help="Required. Path to an .xml file with a trained model",
+    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model",
                       required=True, type=Path)
     args.add_argument("-i", "--input", help="Required. Path to a 16kHz wav file with speech+noise",
                       required=True, type=str)
@@ -77,35 +78,35 @@ def main():
     core = Core()
 
     # read IR
-    log.info("Reading model {}".format(args.model_path))
-    model = core.read_model(args.model_path)
+    log.info("Reading model {}".format(args.model))
+    ov_encoder = core.read_model(args.model)
 
     # check input and output names
-    input_shapes = {}
-    input_names = []
-    output_names = []
-    for i in range(len(model.get_parameters())):
-        input_shapes[model.inputs[i].get_any_name()] = model.inputs[i].shape
-        input_names.append(model.inputs[i].get_any_name())
-        output_names.append(model.outputs[i].get_any_name())
+    if len(ov_encoder.inputs) == len(ov_encoder.outputs):
+        input_shapes = {}
+        output_names = []
 
-    # sorting by names because then we get data from model it is disordered
-    input_names.sort()
-    output_names.sort()
-    input_shapes = dict(sorted(input_shapes.items()))
+        for const_obj in ov_encoder.inputs:
+            input_shapes[const_obj.get_names().pop()] = const_obj.shape
 
-    assert "input" in input_names, "'input' is not presented in model"
+        for const_obj in ov_encoder.outputs:
+            output_names.append(const_obj.get_names().pop())
+    else:
+        log.error("Number of inputs of the model ({}) is not equal to number of outputs({})".format(len(ov_encoder.inputs), len(ov_encoder.outputs)))
+
+
+    assert "input" in input_shapes.keys(), "'input' is not presented in model"
     assert "output" in output_names, "'output' is not presented in model"
-    state_inp_names = [n for n in input_names if "state" in n]
+    state_inp_names = [n for n in input_shapes.keys() if "state" in n]
     state_param_num = sum(np.prod(input_shapes[n]) for n in state_inp_names)
     log.debug("State_param_num = {} ({:.1f}Mb)".format(state_param_num, state_param_num*4e-6))
 
     # load model to the device
-    compiled_model = core.compile_model(model, args.device)
+    compiled_model = core.compile_model(ov_encoder, args.device)
 
     infer_request = compiled_model.create_infer_request()
 
-    log.info('The model {} is loaded to {}'.format(args.model_path, args.device))
+    log.info('The model {} is loaded to {}'.format(args.model, args.device))
 
     start_time = perf_counter()
     sample_inp = wav_read(args.input)
