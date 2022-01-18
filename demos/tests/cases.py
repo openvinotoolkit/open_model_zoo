2c0e14a46b5d135f80b78252f3f6b269bff822c0
eaidova <ekaterina.aidova@intel.com>
2022-01-18 22:02:06 +0300

    add model to model lists and demos testing

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -541,6 +541,7 @@ NATIVE_DEMOS = [
                     ModelArg('fastseg-small'),
                     ModelArg('hrnet-v2-c1-segmentation'),
                     ModelArg('deeplabv3'),
+                    ModelArg('ocrnet-hrnet-w48-paddle'),
                     ModelArg('pspnet-pytorch'),
                     ModelArg('drn-d-38'))),
         ],
@@ -1216,6 +1217,7 @@ PYTHON_DEMOS = [
                     ModelArg('icnet-camvid-ava-sparse-60-0001'),
                     ModelArg('unet-camvid-onnx-0001'),
                     ModelArg('deeplabv3'),
+                    ModelArg('ocrnet-hrnet-w48-paddle'),
                     ModelArg('pspnet-pytorch'),
                     ModelArg('drn-d-38'))),
             TestCase(options={
