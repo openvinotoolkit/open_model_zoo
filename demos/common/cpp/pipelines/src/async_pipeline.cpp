1a6c8c62746f0f91f880010a34fc5e1ccde2340f
ivikhrev <ivan.vikhrev@intel.com>
2022-01-26 17:40:13 +0300

    upd config api

--- demos/common/cpp/pipelines/src/async_pipeline.cpp
+++ demos/common/cpp/pipelines/src/async_pipeline.cpp
@@ -28,7 +28,7 @@ AsyncPipeline::AsyncPipeline(std::unique_ptr<ModelBase>&& modelInstance, const C
     if (nireq == 0) {
         try {
             // +1 to use it as a buffer of the pipeline
-            nireq = compiledModel.get_metric(METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)).as<unsigned int>() + 1;
+            nireq = compiledModel.get_property(METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)).as<unsigned int>() + 1;
         } catch (const ov::Exception& ex) {
             throw std::runtime_error(std::string("Every device used with the demo should support "
                 "OPTIMAL_NUMBER_OF_INFER_REQUESTS ExecutableNetwork metric. Failed to query the metric with error: ") + ex.what());
