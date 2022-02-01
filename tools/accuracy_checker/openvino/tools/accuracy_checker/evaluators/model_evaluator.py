0814bc37e2064fe977cee689d2cce2d9d34d58b6
Ekaterina Aidova <ekaterina.aidova@intel.com>
2022-02-01 09:28:19 +0300

    AC: fix lpcnet and dien dynamic shapes (#3145)
    * AC: fix lpcnet and dien dynamic shapes
    
    * fix for motion blur inputs order

--- tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/model_evaluator.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/evaluators/model_evaluator.py
@@ -721,14 +721,18 @@ class ModelEvaluator(BaseEvaluator):
         per_input_tamplates = []
         for stat_shape in shapes_statistic:
             shape_template = [-1] * len(stat_shape[0])
+            if isinstance(stat_shape[0], tuple):
+                stat_shape = np.array([np.array(st) for st in stat_shape])
 
-            undefined_shapes = np.sum(stat_shape == -1, axis=0).astype(int)
+            undefined_shapes = np.sum(np.array(stat_shape) == -1, axis=0).astype(int)
             if undefined_shapes.ndim >= 2:
                 undefined_shapes = np.squeeze(undefined_shapes, 0)
+            if undefined_shapes.ndim == 0:
+                undefined_shapes = np.expand_dims(undefined_shapes, 0)
             for i, ds in enumerate(undefined_shapes):
                 if ds > 0:
                     continue
-                axis_sizes = stat_shape[:, i]
+                axis_sizes = np.array(stat_shape)[:, i]
                 min_size = np.min(axis_sizes)
                 max_size = np.max(axis_sizes)
                 shape_template[i] = min_size if min_size == max_size else (min_size, max_size)
