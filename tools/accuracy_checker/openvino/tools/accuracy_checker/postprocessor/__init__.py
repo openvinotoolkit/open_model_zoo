d512b11e8fcf86045aed0f32561a979f98dde448
Zuev,MikhailX <mikhailx.zuev@intel.com>
2022-02-04 18:46:38 +0300

    Remove debugging preprocessor

--- tools/accuracy_checker/openvino/tools/accuracy_checker/postprocessor/__init__.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/postprocessor/__init__.py
@@ -67,7 +67,6 @@ from .invert_mask import InvertMask
 from .rescale_mask import RescaleMask
 from .facial_landmarks_98_postprocess import Heatmap2Keypoints
 from .text_similarity_pooling import SentenceSimilarityPooling
-from .hand_landmarks_postprocess import HandLandmarksPostprocessor
 
 __all__ = [
     'Postprocessor',
@@ -157,7 +156,5 @@ __all__ = [
 
     'SentenceSimilarityPooling',
 
-    'WeightedNMS',
-
-    'HandLandmarksPostprocessor'
+    'WeightedNMS'
 ]
