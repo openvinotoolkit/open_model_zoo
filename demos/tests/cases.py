bf7dcba14a91723d41d1ab1c1a74528f6dc0216e
kazuki minemura <kazuki.minemura@intel.com>
2022-02-10 23:47:39 +0800

    fixed format

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -1246,7 +1246,7 @@ PYTHON_DEMOS = [
     #)),
 
     PythonDemo(name='smartlab_demo', device_keys=['-d'],
-        model_keys=['-m_ta', '-m_tm', '-m_fa', '-m_fm', '-m_en', '-m_de']
+        model_keys=['-m_ta', '-m_tm', '-m_fa', '-m_fm', '-m_en', '-m_de'],
         test_cases=combine_cases(
         TestCase(options={'-tv': TestDataArg('data/test_data/videos/smartlab/stream_8_top.mp4'),
             '-fv': TestDataArg('data/test_data/videos/smartlab/stream_8_front.mp4'),
