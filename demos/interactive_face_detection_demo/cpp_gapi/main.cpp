90dc81c99fb99dec50487e90513aa28d640f7ae6
Maxim Pashchenkov <maxim.pashchenkov@intel.com>
2022-02-17 15:49:05 +0300

    G-API: Align limit flag behavior of G-API demos with OMZ (#3198)
    * Align gapi demos with omz
    
    * Extended error messages. Changed name of capture for GestRec demo
    
    * Added check for sizes
    
    * Removed check. Will be added in next PR

--- demos/interactive_face_detection_demo/cpp_gapi/main.cpp
+++ demos/interactive_face_detection_demo/cpp_gapi/main.cpp
@@ -424,8 +424,7 @@ int main(int argc, char *argv[]) {
         std::unique_ptr<Presenter> presenter;
 
          /** Get information about frame **/
-        std::shared_ptr<ImagesCapture> cap = openImagesCapture(FLAGS_i, FLAGS_loop, 0,
-            FLAGS_limit);
+        std::shared_ptr<ImagesCapture> cap = openImagesCapture(FLAGS_i, FLAGS_loop);
         const auto tmp = cap->read();
         cap.reset();
         if (!tmp.data) {
