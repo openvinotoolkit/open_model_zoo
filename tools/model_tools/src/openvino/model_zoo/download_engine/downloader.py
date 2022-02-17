4f00f3c1a22770e989feab7d1be3afbcecd98288
jkamelin <julia.kamelina@intel.com>
2022-02-17 18:58:19 +0300

    print message only if no matching precisions found

--- tools/model_tools/src/openvino/model_zoo/download_engine/downloader.py
+++ tools/model_tools/src/openvino/model_zoo/download_engine/downloader.py
@@ -214,10 +214,12 @@ class Downloader:
         reporter.print_group_heading('Downloading {}', model.name)
 
         model_unsupported_precisions = self.requested_precisions - model.precisions
-        if model_unsupported_precisions:
-            reporter.print_section_heading('Model {} is not supported in {} precisions. '
-                + 'Skipping downloading for these precisions.', model.name, model_unsupported_precisions)
+        if model_unsupported_precisions and not self.requested_precisions & model.precisions:
+            reporter.print_section_heading('Skipping {} (model is unsupported in {} precisions)',
+                model.name, model_unsupported_precisions)
             reporter.print()
+
+            return True
 
         reporter.emit_event('model_download_begin', model=model.name, num_files=len(model.files))
 
