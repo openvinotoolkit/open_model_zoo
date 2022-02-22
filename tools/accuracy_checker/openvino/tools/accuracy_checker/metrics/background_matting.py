f58615a284109be02e724e1dcd73ef09008bbe55
eaidova <ekaterina.aidova@intel.com>
2022-02-22 19:08:06 +0300

    AC: make scipy optional

--- tools/accuracy_checker/openvino/tools/accuracy_checker/metrics/background_matting.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/metrics/background_matting.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 import cv2
 import numpy as np
-from scipy.ndimage.filters import convolve
+
 
 from ..representation import (
     BackgroundMattingAnnotation,
@@ -25,6 +25,13 @@ from ..representation import (
 
 from ..config import BoolField, StringField
 from .metric import PerImageEvaluationMetric
+from ..utils import UnsupportedPackage
+
+try:
+    from scipy.ndimage.filters import convolve
+except ImportError as err:
+    convolve = UnsupportedPackage('scipy', err.msg)
+
 
 
 class BaseBackgroundMattingMetrics(PerImageEvaluationMetric):
@@ -133,6 +140,8 @@ class SpatialGradient(BaseBackgroundMattingMetrics):
         return -x * self.gaussian(x, sigma) / sigma ** 2
 
     def configure(self, sigma=1.4):
+        if isinstance(convolve, UnsupportedPackage):
+            convolve.raise_error(self.__provider__)
         super().configure()
         self.filter_x, self.filter_y = self.gauss_filter(sigma)
         self.reset()
