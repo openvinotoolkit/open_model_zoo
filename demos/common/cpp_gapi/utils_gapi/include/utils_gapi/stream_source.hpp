0d65526c33e7f052b6a3aca878a0649eeaeb91ab
TolyaTalamanov <anatoliy.talamanov@intel.com>
2022-08-20 11:43:04 +0100

    Refactor MediaCommonCapSrc

--- demos/common/cpp_gapi/utils_gapi/include/utils_gapi/stream_source.hpp
+++ demos/common/cpp_gapi/utils_gapi/include/utils_gapi/stream_source.hpp
@@ -40,46 +40,21 @@ class MediaBGRAdapter final: public cv::MediaFrame::IAdapter {
 public:
     using Cb = cv::MediaFrame::View::Callback;
 
-    explicit MediaBGRAdapter(cv::Mat m, Cb cb = [](){})
-        : m_mat(m), m_cb(cb) {
-    }
+    explicit MediaBGRAdapter(cv::Mat m, Cb cb = [](){});
 
-    cv::GFrameDesc meta() const override {
-        return cv::GFrameDesc{cv::MediaFormat::BGR, cv::Size(m_mat.cols, m_mat.rows)};
-    }
-
-    cv::MediaFrame::View access(cv::MediaFrame::Access) override {
-        cv::MediaFrame::View::Ptrs pp = { m_mat.ptr(), nullptr, nullptr, nullptr };
-        cv::MediaFrame::View::Strides ss = { m_mat.step, 0u, 0u, 0u };
-        return cv::MediaFrame::View(std::move(pp), std::move(ss), Cb{m_cb});
-    }
+    cv::GFrameDesc meta() const override;
+    cv::MediaFrame::View access(cv::MediaFrame::Access) override;
 
 private:
     cv::Mat m_mat;
     Cb m_cb;
 };
 
-class MediaCommonCapSrc : public cv::gapi::wip::IStreamSource {
-public:
-    explicit MediaCommonCapSrc(std::shared_ptr<ImagesCapture>& img_cap)
-        : cap(std::make_shared<CommonCapSrc>(img_cap)) {
-    }
+class MediaCommonCapSrc : public CommonCapSrc {
+    using CommonCapSrc::CommonCapSrc;
 
-    bool pull(cv::gapi::wip::Data& data) {
-        if (cap->pull(data)) {
-            data = cv::MediaFrame::Create<MediaBGRAdapter>(cv::util::get<cv::Mat>(data));
-            return true;
-        }
-        return false;
-    }
-
-    cv::GMetaArg descr_of() const override {
-        return cv::GMetaArg{cv::GFrameDesc{cv::MediaFormat::BGR,
-                            cv::util::get<cv::GMatDesc>(cap->descr_of()).size}};
-    }
-
-private:
-    std::shared_ptr<CommonCapSrc> cap;
+    bool pull(cv::gapi::wip::Data& data);
+    cv::GMetaArg descr_of() const override;
 };
 
 }  // namespace custom
