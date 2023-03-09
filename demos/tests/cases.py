da4201b73f904c29171b70ab5aed445a6221af37
Wovchena <vladimir.zlobin@intel.com>
2023-03-09 11:26:57 +0400

    Temp

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
+import collections  # asdf
 import itertools
 import sys
 from copy import deepcopy
