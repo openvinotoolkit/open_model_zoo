a82ab24761219d58be7ad10c25f7ba2b3cde6ea5
Wovchena <vladimir.zlobin@intel.com>
2023-08-09 13:18:31 +0400

    Fix spelling

--- demos/3d_segmentation_demo/python/3d_segmentation_demo.py
+++ demos/3d_segmentation_demo/python/3d_segmentation_demo.py
@@ -368,7 +368,7 @@ def main():
         for seg_res in list_seg_result:
             nii_filename = os.path.join(args.path_to_output, 'output_{}.nii.gz'.format(list_seg_result.index(seg_res)))
             # Nifti1Image expects uit8, but brain-tumor-segmentation-0001 gives int64.
-            # brain-tumor-segmentation-0002 gives uit8
+            # brain-tumor-segmentation-0002 gives uint8
             nib.save(nib.Nifti1Image(seg_res.astype(np.uint8), affine=affine), nii_filename)
             log.debug("Result nifti file was saved to {}".format(nii_filename))
 
