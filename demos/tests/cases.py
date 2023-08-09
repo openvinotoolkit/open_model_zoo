c462147c23a3905ac74433f541a067b599d2f431
Wovchena <vladimir.zlobin@intel.com>
2023-08-09 21:16:22 +0400

    demos/tests: suppress ocrnet-hrnet-w48-paddle back

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -617,7 +617,7 @@ DEMOS = [
                     ModelArg('fastseg-small'),
                     ModelArg('hrnet-v2-c1-segmentation'),
                     ModelArg('deeplabv3'),
-                    ModelArg('ocrnet-hrnet-w48-paddle'),
+                    # ModelArg('ocrnet-hrnet-w48-paddle'),  # TODO: enable after paddle pip conflicts resolution
                     ModelArg('pspnet-pytorch'),
                     ModelArg('drn-d-38'),
                     ModelArg('erfnet'),
@@ -1286,7 +1286,7 @@ DEMOS = [
                     ModelArg('icnet-camvid-ava-sparse-60-0001'),
                     ModelArg('unet-camvid-onnx-0001'),
                     ModelArg('deeplabv3'),
-                    ModelArg('ocrnet-hrnet-w48-paddle'),
+                    # ModelArg('ocrnet-hrnet-w48-paddle'),  # TODO: enable after paddle pip conflicts resolution
                     ModelArg('pspnet-pytorch'),
                     ModelArg('drn-d-38'),
                     ModelArg('erfnet'),
