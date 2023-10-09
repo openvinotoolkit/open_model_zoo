60fce19bbc9ed2407c5b62dd4dd0ffbec34ef67c
sivanov-work <sergey.ivanov@intel.com>
2023-10-09 09:56:41 +0000

    Decrease IE and ONNX in opencv support versions requirements

--- demos/common/cpp_gapi/utils_gapi/include/utils_gapi/gapi_features.hpp
+++ demos/common/cpp_gapi/utils_gapi/include/utils_gapi/gapi_features.hpp
@@ -3,11 +3,11 @@
 #include <opencv2/core/version.hpp>
 #include <opencv2/gapi/core.hpp>
 
-#if CV_VERSION_MAJOR > 4 || (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 5)
+#if CV_VERSION_MAJOR > 4 || (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 2)
     #define GAPI_IE_BACKEND
 #endif
 
-#if CV_VERSION_MAJOR > 4 || (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 7)
+#if CV_VERSION_MAJOR > 4 || (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR > 5)
     #define GAPI_ONNX_BACKEND
 #endif
 
