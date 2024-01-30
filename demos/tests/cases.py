66779691fc1491eff72a9b194adbe4652f5deb61
DariaMityagina <daria.mityagina@intel.com>
2024-01-30 01:47:00 +0000

    Background subtraction - video case

--- demos/tests/cases.py
+++ demos/tests/cases.py
@@ -173,11 +173,14 @@ DEMOS = [
     CppDemo(name='background_subtraction_demo', device_keys=['-d'], implementation='cpp_gapi', test_cases=combine_cases(
         TestCase(options={'--no_show': None, '-at': 'maskrcnn',
             **MONITORS,
-            '-i': DataPatternArg('coco128-subset-480x640x3'),
         }),
         single_option_cases('-m',
             ModelArg('instance-segmentation-person-0007'),
             ModelArg('instance-segmentation-security-0091')),
+        single_option_cases(
+            '-i',
+            str('video.mp4'),
+            DataPatternArg('coco128-every-480x640x3')),
     )),
 
     CppDemo('classification_benchmark_demo', 'cpp_gapi', test_cases=combine_cases(
