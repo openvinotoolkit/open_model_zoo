99d1176e6f6ea650052723c44f9e836b211a9130
Kadian <ujjayant.kadian@intel.com>
2024-02-26 13:33:29 +0000

    Corrected error

--- demos/common/cpp_gapi/utils_gapi/include/utils_gapi/backend_description.hpp
+++ demos/common/cpp_gapi/utils_gapi/include/utils_gapi/backend_description.hpp
@@ -26,8 +26,8 @@ struct BackendDescription {
 
 struct BackendsConfig: ModelConfig {
     BackendsConfig(const ModelConfig &src,
-                   const std::vector<float> &mean_values = "",
-                   const std::vector<float> &scale_values = "");
+                   const std::vector<float> &mean_values,
+                   const std::vector<float> &scale_values);
     std::string mean_values;
     std::string scale_values;
 };
