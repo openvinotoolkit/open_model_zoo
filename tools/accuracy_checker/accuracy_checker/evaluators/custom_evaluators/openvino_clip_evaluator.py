88d605f2aea78880a098397caad64744b42f0941
Piotr Wolnowski <piotr.wolnowski@intel.com>
2024-04-16 09:45:32 +0200

    Add custom evaluator openvino clip models

---
+++ tools/accuracy_checker/accuracy_checker/evaluators/custom_evaluators/openvino_clip_evaluator.py
@@ -0,0 +1,190 @@
+"""
+Copyright (c) 2024 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import os
+import numpy as np
+import open_clip
+import PIL
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from .base_custom_evaluator import BaseCustomEvaluator
+from .base_models import BaseCascadeModel
+from ...config import ConfigError
+from ...utils import contains_all, extract_image_representations, read_json
+from ...representation import ClassificationPrediction
+from ...logging import print_info
+
+
+class OpenVinoClipEvaluator(BaseCustomEvaluator):
+    def __init__(self, dataset_config, launcher, model, orig_config):
+        super().__init__(dataset_config, launcher, orig_config)
+        self.model = model
+
+    @classmethod
+    def from_configs(cls, config, delayed_model_loading=False, orig_config=None):
+        dataset_config, launcher, _ = cls.get_dataset_and_launcher_info(config)
+
+        model = OpenVinoClipModel(
+            config.get('network_info', {}), launcher, config.get('_models', []),
+            config.get('_model_is_blob'),
+            delayed_model_loading, config
+        )
+        return cls(dataset_config, launcher, model, orig_config)
+
+    def _process(self, output_callback, calculate_metrics, progress_reporter, metric_config, csv_file):
+
+        zeroshot_weights = self.model.zero_shot_classifier(self.dataset.data_reader.data_source)
+        for batch_id, (batch_input_ids, batch_annotation, batch_inputs, batch_identifiers) in enumerate(self.dataset):
+            batch_data, _ = extract_image_representations(batch_inputs)
+
+            batch_raw_prediction, batch_prediction = self.model.predict(
+                batch_identifiers, batch_data, zeroshot_weights
+            )
+
+            metrics_result = self._get_metrics_result(batch_input_ids, batch_annotation, batch_prediction,
+                                                      calculate_metrics)
+            if output_callback:
+                output_callback(batch_raw_prediction, metrics_result=metrics_result,
+                                element_identifiers=batch_identifiers, dataset_indices=batch_input_ids)
+            self._update_progress(progress_reporter, metric_config, batch_id, len(batch_prediction), csv_file)
+
+
+class OpenVinoClipModel(BaseCascadeModel):
+    def __init__(self, network_info, launcher, models_args, is_blob, delayed_model_loading=False, config=None):
+        super().__init__(network_info, launcher, delayed_model_loading)
+        self.network_info = network_info
+        self.launcher = launcher
+        self.config = config or {}
+        parts = ['text_encoder', 'image_encoder']
+        network_info = self.fill_part_with_model(network_info, parts, models_args, False, delayed_model_loading)
+        if not contains_all(network_info, parts) and not delayed_model_loading:
+            raise ConfigError('configuration for text_encoder/image_encoder does not exist')
+        if not delayed_model_loading:
+            self.create_pipeline(launcher, network_info)
+
+    def create_pipeline(self, launcher, network_info):
+        orig_model_name = self.config.get("orig_model_name", "ViT-B-16-plus-240")
+        pretrained = self.config.get("pretrained", "laion400m_e32")
+
+        self.load_models(network_info, launcher, True)
+
+        self.text_encoder = launcher.ie_core.compile_model(self.text_encoder_model, launcher.device)
+        self.image_encoder = launcher.ie_core.compile_model(self.image_encoder_model, launcher.device)
+
+        unet_shapes = [inp.get_partial_shape() for inp in self.text_encoder_model.inputs]
+        if unet_shapes[0][0].is_dynamic:
+            self.templates_file = self.config.get("templates", "zeroshot_classification_templates.json")
+        else:
+            self.templates_file = None
+
+        self.classnames_file = self.config.get("classnames", "classnames.json")
+
+        self.torch_model, _, self.transform = open_clip.create_model_and_transforms(orig_model_name,
+                                                                                    pretrained=pretrained,
+                                                                                    cache_dir=None)
+        self.tokenizer = open_clip.get_tokenizer(orig_model_name)
+
+    def predict(self, identifiers, input_data, zeroshot_weights):
+        preds = []
+        for idx, image_data in zip(identifiers, input_data):
+            image = self.transform(PIL.Image.fromarray(image_data))
+            image_features = self.encode_image(image.unsqueeze(0))
+            image_features = F.normalize(image_features, dim=-1)
+            logits = 100. * image_features @ zeroshot_weights
+            preds.append(ClassificationPrediction(idx, logits.squeeze(0)))
+        return None, preds
+
+    def get_network(self):
+        models = self.pipe.get_models()
+        model_list = []
+        for model_part_name, model in models.items():
+            model_list.append({"name": model_part_name, "model": model})
+        return model_list
+
+    def encode_image(self, image, normalize: bool = False):
+        '''
+        Code source https://github.com/AlexKoff88/CLIP_benchmark/blob/openvino/clip_benchmark/models/openvino_clip.py
+        '''
+        features = self.image_encoder(image)
+        features = torch.from_numpy(features[self.image_encoder.output()])
+        return F.normalize(features, dim=-1) if normalize else features
+
+    def encode_text(self, text, normalize: bool = False):
+        # Code source https://github.com/AlexKoff88/CLIP_benchmark/blob/openvino/clip_benchmark/models/openvino_clip.py
+        x = self.torch_model.token_embedding(text)
+        x = x + self.torch_model.positional_embedding
+        x = x.permute(1, 0, 2)
+        x = torch.asarray(x).contiguous()
+        x = self.text_encoder((x, self.torch_model.attn_mask))
+        x = torch.from_numpy(x[self.text_encoder.output()])
+        x = x.permute(1, 0, 2)
+        x = self.torch_model.ln_final(x)
+        x = x[np.arange(x.shape[0]), text.argmax(dim=-1)] @ self.torch_model.text_projection
+        return F.normalize(x, dim=-1) if normalize else x
+
+    def zero_shot_classifier(self, data_source):
+        classnames = read_json(os.path.join(data_source, self.classnames_file))
+        if self.templates_file:
+            templates = read_json(os.path.join(data_source, self.templates_file))
+        else:
+            templates = ["a photo of a {c}"]
+
+        device = 'cpu'
+        print_info('Encoding zeroshot weights for {} imagenet classes'.format(len(classnames)))
+
+        # Code source https://github.com/mlfoundations/open_clip/blob/main/src/training/zero_shot.py
+        autocast = torch.cuda.amp.autocast
+        with torch.no_grad(), autocast():
+            zeroshot_weights = []
+            for classname in tqdm(classnames, mininterval=2):
+                texts = [template.format(c=classname) for template in templates]
+                tokenized_texts = self.tokenizer(texts).to(device)  # tokenize
+                class_embeddings = self.encode_text(tokenized_texts, self.torch_model)
+                class_embedding = F.normalize(class_embeddings, dim=-1).mean(dim=0)
+                class_embedding /= class_embedding.norm()
+                zeroshot_weights.append(class_embedding)
+            zeroshot_weights = torch.stack(zeroshot_weights, dim=1).to(device)
+        return zeroshot_weights
+
+    def load_models(self, network_info, launcher, log=False):
+        if isinstance(network_info, dict):
+            for model_name, model_dict in network_info.items():
+                model_dict["name"] = model_name
+                self.load_model(model_dict, launcher)
+        else:
+            for model_dict in network_info:
+                self.load_model(model_dict, launcher)
+
+        if log:
+            self.print_input_output_info()
+
+    def load_model(self, network_list, launcher):
+        model, weights = self.automatic_model_search(network_list)
+        if weights:
+            network = launcher.read_network(str(model), str(weights))
+        else:
+            network = launcher.read_network(str(model), None)
+        self.set_model(network, )
+        setattr(self, "{}_model".format(network_list["name"]), network)
+
+    def print_input_output_info(self):
+        model_parts = ("text_encoder", "image_encoder")
+        for part in model_parts:
+            part_model_id = "{}_model".format(part)
+            model = getattr(self, part_model_id, None)
+            if model is not None:
+                self.launcher.print_input_output_info(model, part)
