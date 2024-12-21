f870aed66a727c9ea7bdaf6f04e81f2ee9c70484
Piotr Wolnowski <piotr.wolnowski@intel.com>
2024-12-21 07:29:19 +0100

    Cleanup of test_whisper_evaluator.py

--- tools/accuracy_checker/tests/test_whisper_evaluator.py
+++ tools/accuracy_checker/tests/test_whisper_evaluator.py
@@ -15,80 +15,71 @@ limitations under the License.
 """
 import os
 from pathlib import Path
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 from accuracy_checker.evaluators.custom_evaluators.whisper_evaluator import (
-    GenAIWhisperPipeline, OptimumWhisperPipeline, HFWhisperPipeline,
+    GenAIWhisperPipeline, HFWhisperPipeline, OptimumWhisperPipeline,
     WhisperEvaluator, normalize_transcription)
 from datasets import load_dataset
 from optimum.exporters.openvino.convert import export_tokenizer
 from optimum.intel.openvino import OVModelForSpeechSeq2Seq
-from transformers import AutoTokenizer, AutoProcessor
-
-def export_model(model_id, output_dir):
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    processor = AutoProcessor.from_pretrained(model_id)
-    base_model = OVModelForSpeechSeq2Seq.from_pretrained(model_id)
-
-    base_model.save_pretrained(output_dir)
-    tokenizer.save_pretrained(output_dir)
-    processor.save_pretrained(output_dir)
-    export_tokenizer(tokenizer, output_dir)
+from transformers import AutoProcessor, AutoTokenizer
 
 model_id = "openai/whisper-tiny"
 model_dir = Path("/tmp/whisper-tiny")
 
-# Export the model
-export_model(model_id, model_dir)
+def setup_module(module):
+    global input_data, input_meta, identifiers
+
+    dataset = load_dataset("openslr/librispeech_asr", "clean", split="validation", streaming=True, trust_remote_code=True)
+    sample = next(iter(dataset))
+    input_data = [sample["audio"]["array"]]
+    input_meta = [{"sample_rate": sample["audio"]["sampling_rate"]}]
+    identifiers = [sample["id"]]
+
+def teardown_module(module):
+    if model_dir.exists():
+        for item in model_dir.iterdir():
+            if item.is_file():
+                item.unlink()
+        model_dir.rmdir()
+
+def test_optimum_convert_model_to_ir():
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    processor = AutoProcessor.from_pretrained(model_id)
+    base_model = OVModelForSpeechSeq2Seq.from_pretrained(model_id)
+
+    model_dir.mkdir(parents=True, exist_ok=True)
+    base_model.save_pretrained(model_dir)
+    tokenizer.save_pretrained(model_dir)
+    processor.save_pretrained(model_dir)
+    export_tokenizer(tokenizer, model_dir)
+    assert base_model.__class__.__module__.startswith('optimum.intel.openvino')
 
 class TestWhisperEvaluator:
-    @classmethod
-    def setup_class(cls):
-        # Load a single sample from the dataset
-        dataset = load_dataset("openslr/librispeech_asr", "clean", split="validation", streaming=True, trust_remote_code=True)
-        sample = next(iter(dataset))
-        cls.input_data = [sample["audio"]["array"]]
-        cls.input_meta = [{"sample_rate": sample["audio"]["sampling_rate"]}]
-        cls.identifiers = [sample["id"]]
-
-    @classmethod
-    def teardown_class(cls):
-        if model_dir.exists():
-            for item in model_dir.iterdir():
-                if item.is_file():
-                    item.unlink()
-            model_dir.rmdir()
-
-
     def test_hf_whisper_pipeline(self):
         config = {"model_id": model_id}
         pipeline = HFWhisperPipeline(config)
         evaluator = WhisperEvaluator(None, pipeline, None)
 
-        result = evaluator.pipe._get_predictions(self.input_data, self.identifiers, self.input_meta)
+        result = evaluator.pipe._get_predictions(input_data, identifiers, input_meta)
         assert isinstance(result, str)
 
+    @pytest.mark.dependency(depends=["test_base_model"])
     def test_genai_whisper_pipeline(self):
         config = {"_models": [model_dir], "_device": "CPU"}
         pipeline = GenAIWhisperPipeline(config)
         evaluator = WhisperEvaluator(None, pipeline, None)
 
-        result = evaluator.pipe._get_predictions(self.input_data, self.identifiers, self.input_meta)
+        result = evaluator.pipe._get_predictions(input_data, identifiers, input_meta)
         assert isinstance(result, str)
 
+    @pytest.mark.dependency(depends=["test_base_model"])
     def test_optimum_whisper_pipeline(self):
         config = {"_models": [model_dir], "_device": "CPU"}
         pipeline = OptimumWhisperPipeline(config)
         evaluator = WhisperEvaluator(None, pipeline, None)
 
-        result = evaluator.pipe._get_predictions(self.input_data, self.identifiers, self.input_meta)
+        result = evaluator.pipe._get_predictions(input_data, identifiers, input_meta)
         assert isinstance(result, str)
-
-def test_normalize_transcription():
-    engine = MagicMock()
-    engine.number_to_words.side_effect = lambda x: "one" if x == "1" else x
-    text = "This is a test 1"
-    result = normalize_transcription(engine, text)
-    assert result == "THIS IS A TEST ONE"
-
